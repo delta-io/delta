04fd2c7e8a650738de30822219cd7adde666fb7f
Burak Yavuz <brkyvz@gmail.com>
2019-04-22 10:41:19 -0700

    [SC-17152] Open source some Time Travel tests
    ## What changes were proposed in this pull request?
    
    This PR open sources some time travel tests.
    ## How was this patch tested?
    Copybara'd to open source repo and ran tests
    
    Closes #4909 from brkyvz/ttTests.
    
    Lead-authored-by: Burak Yavuz <brkyvz@gmail.com>
    Co-authored-by: liwensun <liwen.sun@databricks.com>
    Signed-off-by: Tathagata Das <tathagata.das1565@gmail.com>
    GitOrigin-RevId: 853989251f00d43ac71932fc64e25904ff51ba6c

--- src/main/scala/org/apache/spark/sql/delta/MetadataCleanup.scala
+++ src/main/scala/org/apache/spark/sql/delta/MetadataCleanup.scala
@@ -16,10 +16,12 @@
 
 package org.apache.spark.sql.delta
 
-import java.util.Date
+import java.util.{Calendar, Date, TimeZone}
 
+import org.apache.spark.sql.delta.DeltaHistoryManager.BufferingLogDeletionIterator
 import org.apache.spark.sql.delta.metering.DeltaLogging
-import org.apache.hadoop.fs.FileStatus
+import org.apache.commons.lang3.time.DateUtils
+import org.apache.hadoop.fs.{FileStatus, Path}
 
 /** Cleans up expired Delta table metadata. */
 trait MetadataCleanup extends DeltaLogging {
@@ -45,12 +47,12 @@ trait MetadataCleanup extends DeltaLogging {
   /** Clean up expired delta and checkpoint logs. Exposed for testing. */
   private[delta] def cleanUpExpiredLogs(): Unit = {
     recordDeltaOperation(this, "delta.log.cleanup") {
-      val fileCutOffTime = clock.getTimeMillis() - deltaRetentionMillis
-      val formattedDate = new Date(fileCutOffTime).toGMTString
+      val fileCutOffTime = truncateDay(clock.getTimeMillis() - deltaRetentionMillis).getTime
+      val formattedDate = fileCutOffTime.toGMTString
       logInfo(s"Starting the deletion of log files older than $formattedDate")
 
       var numDeleted = 0
-      listExpiredDeltaLogs(fileCutOffTime).map(_.getPath).foreach { path =>
+      listExpiredDeltaLogs(fileCutOffTime.getTime).map(_.getPath).foreach { path =>
         // recursive = false
         if (fs.delete(path, false)) numDeleted += 1
       }
@@ -62,7 +64,7 @@ trait MetadataCleanup extends DeltaLogging {
    * Returns an iterator of expired delta logs that can be cleaned up. For a delta log to be
    * considered as expired, it must:
    *  - have a checkpoint file after it
-   *  - be older than `deltaRetentionMillis`
+   *  - be older than `fileCutOffTime`
    */
   private def listExpiredDeltaLogs(fileCutOffTime: Long): Iterator[FileStatus] = {
     import org.apache.spark.sql.delta.util.FileNames._
@@ -70,20 +72,25 @@ trait MetadataCleanup extends DeltaLogging {
     val latestCheckpoint = lastCheckpoint
     if (latestCheckpoint.isEmpty) return Iterator.empty
     val threshold = latestCheckpoint.get.version - 1L
-    store.listFrom(deltaFile(logPath, 0))
+    val files = store.listFrom(deltaFile(logPath, 0))
       .filter(f => isCheckpointFile(f.getPath) || isDeltaFile(f.getPath))
-      .takeWhile { fileStatus =>
-        val filePath = fileStatus.getPath
-        if (fileStatus.getModificationTime <= fileCutOffTime) {
-          val fileVersion = if (isCheckpointFile(filePath)) {
-            checkpointVersion(filePath)
-          } else {
-            deltaVersion(filePath)
-          }
-          fileVersion <= threshold
-        } else {
-          false
-        }
+    def getVersion(filePath: Path): Long = {
+      if (isCheckpointFile(filePath)) {
+        checkpointVersion(filePath)
+      } else {
+        deltaVersion(filePath)
       }
+    }
+
+    new BufferingLogDeletionIterator(files, fileCutOffTime, threshold, getVersion)
+  }
+
+  /** Truncates a timestamp down to the previous midnight and returns the time and a log string */
+  private[delta] def truncateDay(timeMillis: Long): Calendar = {
+    val date = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    date.setTimeInMillis(timeMillis)
+    DateUtils.truncate(
+      date,
+      Calendar.DAY_OF_MONTH)
   }
 }
