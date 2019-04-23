d6e5f67a60b5c83a436bd92482c10a329ca1e0e7
Wesley Hsiao <wesley.hsiao@databricks.com>
2019-04-22 22:41:37 -0700

    [SC-17067] Update scala files with new copyright header
    ## What changes were proposed in this pull request?
    
    jira: https://databricks.atlassian.net/browse/SC-17067
    
    This pr is the batch copy right header update for scala files.  Make sure scala styles pass the lint check and do not change any of the file permissions.
    
    ## How was this patch tested?
    
    Command to update files:
    
    `python dev/update_databricks_copyright_header.py`
    
    Please review http://spark.apache.org/contributing.html before opening a pull request.
    
    Authored-by: Wesley Hsiao <wesley.hsiao@databricks.com>
    Signed-off-by: Wesley Hsiao <wesley.hsiao@databricks.com>
    GitOrigin-RevId: 9b8148676c22aa3df29e9f579069ea2fb6cee1cd

--- src/main/scala/org/apache/spark/sql/delta/DeltaFileFormat.scala
+++ src/main/scala/org/apache/spark/sql/delta/DeltaFileFormat.scala
@@ -1,12 +1,22 @@
 package org.apache.spark.sql.delta
 
 /*
- * Copyright (C) 2019 Databricks, Inc.
+ * DATABRICKS CONFIDENTIAL & PROPRIETARY
+ * __________________
  *
- * Portions of this software incorporate or are derived from software contained within Apache Spark,
- * and this modified software differs from the Apache Spark software provided under the Apache
- * License, Version 2.0, a copy of which you may obtain at
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2019 Databricks, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of Databricks, Inc.
+ * and its suppliers, if any.  The intellectual and technical concepts contained herein are
+ * proprietary to Databricks, Inc. and its suppliers and may be covered by U.S. and foreign Patents,
+ * patents in process, and are protected by trade secret and/or copyright law. Dissemination, use,
+ * or reproduction of this information is strictly forbidden unless prior written permission is
+ * obtained from Databricks, Inc.
+ *
+ * If you view or obtain a copy of this information and believe Databricks, Inc. may not have
+ * intended it to be made available, please promptly report it to Databricks Legal Department
+ * @ legal@databricks.com.
  */
 
 import org.apache.spark.sql.execution.datasources.FileFormat
