73f4a64284f7e971be4a5238aa4b3f310360df85
Rahul Mahadev <rahul.mahadev@databricks.com>
2019-07-25 03:39:31 +0000

    [SC-19632][DELTA] - VACUUM Scala API
    ## What changes were proposed in this pull request?
    
    Users can Vacuum a given DeltaTable with a given retention period. Vacuum would Recursively delete files and directories in the table that are not needed by the table for maintaining older versions up to the given retention threshold.
    Note: Vacuum would disable the ability to time travel beyond the retention period.
    
    Sample usage :
    deltaTable = new DeltaTable(spark.table(tableName))
    deltaTable.vacuum(retentionHours = 13)
    deltaTable.vacuum()
    
    ## How was this patch tested?
    - DeltaVacuumSuite to test the functionality of Vacuum.
    
    Closes delta-io/delta#95
    ORIGINAL_AUTHOR=rahulsmahadev rahul.mahadev@databricks.com
    
    Author: Rahul Mahadev <rahul.mahadev@databricks.com>
    
    #5741 is resolved by rahulsmahadev/vacuum.
    
    GitOrigin-RevId: 1fe58f3f8877fd82a3770e7d12e282ab0241b89a

---
+++ src/main/scala/org/apache/spark/sql/delta/util/SerializableFileStatus.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2019 Databricks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.delta.util
+
+import java.util.Objects
+
+import org.apache.hadoop.fs.{FileStatus, LocatedFileStatus, Path}
+
+/** A serializable variant of HDFS's FileStatus. */
+case class SerializableFileStatus(
+    path: String,
+    length: Long,
+    isDir: Boolean,
+    modificationTime: Long) {
+
+  // Important note! This is very expensive to compute, but we don't want to cache it
+  // as a `val` because Paths internally contain URIs and therefore consume lots of memory.
+  def getPath: Path = new Path(path)
+  def getLen: Long = length
+  def getModificationTime: Long = modificationTime
+  def isDirectory: Boolean = isDir
+
+  def toFileStatus: FileStatus = {
+    new LocatedFileStatus(
+      new FileStatus(length, isDir, 0, 0, modificationTime, new Path(path)),
+      null)
+  }
+
+  override def equals(obj: Any): Boolean = obj match {
+    case other: SerializableFileStatus =>
+      // We only compare the paths to stay consistent with FileStatus.equals.
+      Objects.equals(path, other.path)
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    // We only use the path to stay consistent with FileStatus.hashCode.
+    Objects.hashCode(path)
+  }
+}
+
+object SerializableFileStatus {
+  def fromStatus(status: FileStatus): SerializableFileStatus = {
+    SerializableFileStatus(
+      Option(status.getPath).map(_.toString).orNull,
+      status.getLen,
+      status.isDirectory,
+      status.getModificationTime)
+  }
+
+  val EMPTY: SerializableFileStatus = fromStatus(new FileStatus())
+}
