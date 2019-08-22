2673efad443993e883ba49fd7b39a72af19b1784
Yishuang Lu <luystu@gmail.com>
2019-08-22 14:35:31 -0700

    Remove all unused imports in the code
    Remove all unused imports in the code
    
    Signed-off-by: Yishuang Lu <luystu@gmail.com>

--- src/test/scala/org/apache/spark/sql/delta/DescribeDeltaHistorySuite.scala
+++ src/test/scala/org/apache/spark/sql/delta/DescribeDeltaHistorySuite.scala
@@ -20,7 +20,7 @@ import org.apache.spark.sql.delta.actions.CommitInfo
 import org.apache.spark.sql.delta.sources.DeltaSQLConf
 import org.scalatest.Tag
 
-import org.apache.spark.sql.{AnalysisException, Column, DataFrame, QueryTest, Row}
+import org.apache.spark.sql.{Column, DataFrame, QueryTest, Row}
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.test.SharedSQLContext
 import org.apache.spark.util.Utils
