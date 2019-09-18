99ad0d130f2833f2c8042a5ec90615c4e148009c
Rahul Mahadev <rahul.mahadev@databricks.com>
2019-09-18 12:59:33 -0700

    [SC-20936] Python API for Delta Utility Commands(Vacuum, History, ConvertToDelta)
    * Added Python APIs for Vacuum, ConvertToDelta and Describe History
    * Added unit tests for the above
    * Added fix to not swallow Python tests Exceptions
    
    closes delta.io/delta  #173
    
    * Unit tests( `test_deltatable.py`)
    
    Closes #6423 from rahulsmahadev/pyUtilCommands.
    
    Lead-authored-by: Rahul Mahadev <rahul.mahadev@databricks.com>
    Co-authored-by: Tathagata Das <tathagata.das1565@gmail.com>
    Co-authored-by: Zhitong Yan <zhitong.yan@databricks.com>
    Signed-off-by: Rahul Mahadev <rahul.mahadev@databricks.com>
    GitOrigin-RevId: 6576d4457f19583cc62a88350a9d83c990703109

--- python/delta/tables.py
+++ python/delta/tables.py
@@ -53,6 +53,62 @@ class DeltaTable(object):
         jdt = self._jdt.alias(aliasName)
         return DeltaTable(self._spark, jdt)
 
+    def vacuum(self, retentionHours=None):
+        """
+        Recursively delete files and directories in the table that are not needed by the table for
+        maintaining older versions up to the given retention threshold. This method will return an
+        empty DataFrame on successful completion. This uses the default retention period of 7
+        days.
+        :param retentionHours:
+        :return: DataFrame of the history
+
+        .. note:: Evolving
+        """
+        jdt = self._jdt
+        if retentionHours is None:
+            return DataFrame(jdt.vacuum(), self._spark._wrapped)
+        else:
+            return DataFrame(jdt.vacuum(retentionHours), self._spark._wrapped)
+
+    def history(self, limit=None):
+        """
+        Get the information of the latest `limit` commits on this table as a Spark DataFrame.
+        The information is in reverse chronological order.
+        :param limit:
+        :return: DataFrame of the history
+
+        .. note:: Evolving
+        """
+        jdt = self._jdt
+        if limit is None:
+            return DataFrame(jdt.history(), self._spark._wrapped)
+        else:
+            return DataFrame(jdt.history(limit), self._spark._wrapped)
+
+    @classmethod
+    def convertToDelta(cls, sparkSession, identifier, partitionSchema=None):
+        """
+        Create a DeltaTable from the given parquet table. Takes an existing parquet table and
+        constructs a delta transaction log in the base path of the table.
+        Note: Any changes to the table during the conversion process may not result in a consistent
+        state at the end of the conversion. Users should stop any changes to the table before the
+        conversion is started.
+
+        :param sparkSession:
+        :param identifier:
+        :param partitionSchema:
+        :return: DeltaTable
+        """
+        assert sparkSession is not None
+        if partitionSchema is None:
+            jdt = sparkSession._sc._jvm.io.delta.tables.DeltaTable.convertToDelta(
+                sparkSession._jsparkSession, identifier)
+        else:
+            jdt = sparkSession._sc._jvm.io.delta.tables.DeltaTable.convertToDelta(
+                sparkSession._jsparkSession, identifier,
+                sparkSession._jsparkSession.parseDataType(partitionSchema.json()))
+        return jdt
+
     @classmethod
     def forPath(cls, sparkSession, path):
         """
--- src/main/scala/io/delta/tables/DeltaTable.scala
+++ src/main/scala/io/delta/tables/DeltaTable.scala
@@ -98,7 +98,7 @@ class DeltaTable private[tables](df: Dataset[Row], deltaLog: DeltaLog)
    * maintaining older versions up to the given retention threshold. This method will return an
    * empty DataFrame on successful completion.
    *
-   * note: This will use the default retention period of 7 hours.
+   * note: This will use the default retention period of 7 days.
    *
    * @since 0.3.0
    */
