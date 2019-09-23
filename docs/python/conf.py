15d3c6483690d6ac0dad81bfdc0384d355d89e80
Rahul Mahadev <rahul.mahadev@databricks.com>
2019-09-23 13:50:36 -0700

    Making a change to allow building on older versions of sphinx

--- docs/python/conf.py
+++ docs/python/conf.py
@@ -22,9 +22,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(root_dir,'python')))
 
 # -- Project information -----------------------------------------------------
 
-project = 'delta'
-copyright = '2019, io.delta'
-author = 'io.delta'
+project = 'delta-core'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.0'
@@ -59,4 +57,8 @@ html_theme = 'nature'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# Fix for doc generation to work on older version of sphinx as well.
+master_doc = 'index'
 
--- docs/python/index.rst
+++ docs/python/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to delta's documentation!
-=================================
+Welcome to Delta Lake's Python documentation page
+==========================================
 
 .. toctree::
    :maxdepth: 2
