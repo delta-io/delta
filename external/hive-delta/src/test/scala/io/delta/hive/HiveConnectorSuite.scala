a9af8cc136e266b37fc384e29fec9192b265f445
Shixiong Zhu <zsxwing@gmail.com>
2019-10-21 10:54:55 -0700

    add a test for partition pruning

--- external/hive-delta/src/test/scala/io/delta/hive/HiveConnectorSuite.scala
+++ external/hive-delta/src/test/scala/io/delta/hive/HiveConnectorSuite.scala
@@ -1,5 +1,6 @@
 package io.delta.hive
 
+import java.io.File
 import java.nio.file.Files
 
 import io.delta.hive.test.HiveTest
@@ -28,7 +29,7 @@ class HiveConnectorSuite extends HiveTest with BeforeAndAfterEach {
         .appName("HiveConnectorSuite")
         .master("local[2]")
         .getOrCreate()
-      val testData = (0 until 10).map(x => (x, s"foo$x")).toSeq
+      val testData = (0 until 10).map(x => (x, s"foo${x % 2}")).toSeq
       import spark.implicits._
       testData.toDS.toDF("c1", "c2").write.format("delta").save(tempPath.getCanonicalPath)
       // Clean up resources so that we can use new DeltaLog and SparkSession
@@ -59,7 +60,7 @@ class HiveConnectorSuite extends HiveTest with BeforeAndAfterEach {
         .appName("HiveConnectorSuite")
         .master("local[2]")
         .getOrCreate()
-      val testData = (0 until 10).map(x => (x, s"foo$x")).toSeq
+      val testData = (0 until 10).map(x => (x, s"foo${x % 2}")).toSeq
       import spark.implicits._
       testData.toDS.toDF("c1", "c2").write.format("delta")
         .partitionBy("c2").save(tempPath.getCanonicalPath)
@@ -79,6 +80,45 @@ class HiveConnectorSuite extends HiveTest with BeforeAndAfterEach {
       runQuery("msck repair table deltatesttable")
       assert(runQuery(
         "select * from deltatesttable").sorted === testData.map(r => s"${r._1}\t${r._2}").sorted)
+      runQuery("drop table deltatesttable")
+    } finally {
+      JavaUtils.deleteRecursively(tempPath)
+    }
+  }
+
+  test("read a partitioned table with a partition filter") {
+    // Create a Delta table
+    val tempPath = Files.createTempDirectory("testdata").toFile
+    try {
+      val conf = new SparkConf()
+      val spark = SparkSession.builder()
+        .appName("HiveConnectorSuite")
+        .master("local[2]")
+        .getOrCreate()
+      val testData = (0 until 10).map(x => (x, s"foo${x % 2}")).toSeq
+      import spark.implicits._
+      testData.toDS.toDF("c1", "c2").write.format("delta")
+        .partitionBy("c2").save(tempPath.getCanonicalPath)
+      // Clean up resources so that we can use new DeltaLog and SparkSession
+      spark.stop()
+      DeltaLog.clearCache()
+      runQuery(
+        s"""
+           |create external table deltatesttable(c1 INT)
+           |partitioned by(c2 STRING) -- TODO Remove this. This should be read from Delta's metadata
+           |row format serde 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+           |stored as inputformat 'io.delta.hive.DeltaInputFormat'
+           |outputformat 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+           |location '${tempPath.getCanonicalPath}'
+      """.stripMargin)
+      // TODO Remove this. We should discover partitions automatically
+      runQuery("msck repair table deltatesttable")
+      // Delete the partition not needed in the below query to verify the partition pruning works
+      JavaUtils.deleteRecursively(new File(tempPath, "c2=foo1"))
+      assert(tempPath.listFiles.map(_.getName).sorted === Seq("_delta_log", "c2=foo0").sorted)
+      assert(runQuery(
+        "select * from deltatesttable where c2 = 'foo0'").sorted ===
+        testData.filter(_._2 == "foo0").map(r => s"${r._1}\t${r._2}").sorted)
       runQuery("drop table deltatesttable")
     } finally {
       JavaUtils.deleteRecursively(tempPath)
