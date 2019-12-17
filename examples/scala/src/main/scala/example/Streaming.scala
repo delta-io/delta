81d92ae4edb468b7480e223c79d35a045d14d9fe
csdingbin <bin.ding@intel.com>
2019-12-18 00:28:27 +0800

    Refine example streaming code of scala

--- examples/scala/src/main/scala/example/Streaming.scala
+++ examples/scala/src/main/scala/example/Streaming.scala
@@ -18,98 +18,100 @@ package example
 
 import java.io.File
 
-import io.delta.tables.DeltaTable
+import io.delta.tables.DeltaTable                                                                              
 import org.apache.commons.io.FileUtils
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}                                                          
 import org.apache.spark.sql.functions.col
 
-object Streaming {
+object Streaming extends App {
 
-  def main(args: Array[String]): Unit = {
-    // Create a Spark Session
-    val spark = SparkSession
-      .builder()
-      .appName("Streaming")
-      .master("local[*]")
-      .getOrCreate()
+  // Create a Spark Session                                                                                    
+  val spark = SparkSession                                                                                     
+    .builder()                                                                                                 
+    .appName("Streaming")                                                                                      
+    .master("local[*]")                                                                                        
+    .getOrCreate()
 
-    import spark.implicits._
+  spark.sparkContext.setLogLevel("Error")
 
-    // Create a table
-    val data = spark.range(0, 5)
-    val path = new File("/tmp/delta-table").getAbsolutePath
-    data.write.format("delta").save(path)
+  import spark.implicits._
 
-    // Read table
-    val df = spark.read.format("delta").load(path)
-    df.show()
+  //==== Section One                                                                                           
+  println("=== Section One: write and read delta *Table*")                                                     
+  // Create a table                                                                                            
+  val data = spark.range(0, 5)                                                                                 
+  val path = new File("/tmp/delta-table").getAbsolutePath                                                      
+  data.write.format("delta").save(path)
 
-    println("Streaming write")
-    val streamingDf = spark.readStream.format("rate").load()
-    val tablePath2 = new File("/tmp/delta-table2").getCanonicalPath
-    val stream = streamingDf
-      .select($"value" as "id")
-      .writeStream
-      .format("delta")
-      .option("checkpointLocation", new File("/tmp/checkpoint").getCanonicalPath)
-      .start(tablePath2)
+  // Read table                                                                                                
+  val df = spark.read.format("delta").load(path)                                                               
+  df.show()
 
-    stream.awaitTermination(10000)
-    stream.stop()
+                                                                                                               
+  //==== Section Two                                                                                           
+  println("=== Section Two: write and read delta *Stream*")                                                    
+  println("Streaming write")                                                                                   
+  val tablePath2 = new File("/tmp/delta-table2").getCanonicalPath                                              
+  val checkpointPath = new File("/tmp/checkpoint").getCanonicalPath                                            
+  val stream = spark                                                                                           
+    .readStream.format("rate").load()                                                                          
+    .select($"value" as "id")                                                                                  
+    .writeStream.format("delta")                                                                               
+    .option("checkpointLocation", checkpointPath)                                                              
+    .start(tablePath2)
 
-    println("Reading from stream")
-    val stream2 = spark
-      .readStream
-      .format("delta")
-      .load(tablePath2)
-      .writeStream
-      .format("console")
-      .start()
+  stream.awaitTermination(10000)                                                                               
+  stream.stop()
 
-    stream2.awaitTermination(10000)
-    stream2.stop()
+  println("Reading from stream")                                                                               
+  val stream2 = spark                                                                                          
+    .readStream.format("delta").load(tablePath2)                                                               
+    .writeStream.format("console")                                                                             
+    .start()
 
-    // Function to upsert microBatchOutputDF into Delta Lake table using merge
-    println("Streaming upgrades in update mode")
-    def upsertToDelta(microBatchOutputDF: DataFrame, batchId: Long) {
-      val deltaTable = DeltaTable.forPath(path)
-      deltaTable.as("t")
-        .merge(
-          microBatchOutputDF.select($"value" as "id").as("s"),
-          "s.id = t.id")
-        .whenMatched().updateAll()
-        .whenNotMatched().insertAll()
-        .execute()
-    }
+  stream2.awaitTermination(10000)                                                                              
+  stream2.stop()
 
-    val streamingAggregatesDf = spark
-      .readStream
-      .format("rate")
-      .load()
-      .withColumn("key", col("value") % 10)
-      .drop("timestamp")
-
-    // Write the output of a streaming aggregation query into Delta Lake table
-    println("Original Delta Table")
-    val deltaTable = DeltaTable.forPath(path)
-    deltaTable.toDF.show()
-
-    val stream3 = streamingAggregatesDf.writeStream
-      .format("delta")
-      .foreachBatch(upsertToDelta _)
-      .outputMode("update")
-      .start()
-
-    stream3.awaitTermination(10000)
-    stream3.stop()
-
-    println("Delta Table after streaming upsert")
-    deltaTable.toDF.show()
-
-    // Cleanup
-    FileUtils.deleteDirectory(new File(path))
-    FileUtils.deleteDirectory(new File(tablePath2))
-    spark.stop()
+                                                                                                               
+  //==== Section Three                                                                                         
+  println("=== Section Three: Streaming upgrades in update mode")                                              
+  // Function to upsert microBatchOutputDF into Delta Lake table using merge                                   
+  def upsertToDelta(microBatchOutputDF: DataFrame, batchId: Long) {                                            
+    val deltaTable = DeltaTable.forPath(path)                                                                  
+    deltaTable.as("t")                                                                                         
+      .merge(                                                                                                  
+        microBatchOutputDF.select($"value" as "id").as("s"),                                                   
+        "s.id = t.id")                                                                                         
+      .whenMatched().updateAll()                                                                               
+      .whenNotMatched().insertAll()                                                                            
+      .execute()                                                                                               
   }
+
+  // Write the output of a streaming aggregation query into Delta Lake table                                   
+  println("Original Delta Table")                                                                              
+  val deltaTable = DeltaTable.forPath(path)                                                                    
+  deltaTable.toDF.show()
+
+  val stream3 = spark                                                                                          
+    .readStream.format("rate").load()                                                                          
+    .withColumn("key", col("value") % 10)                                                                      
+    .drop("timestamp")                                                                                         
+    .writeStream.format("delta")                                                                               
+    .foreachBatch(upsertToDelta _)                                                                             
+    .outputMode("update")                                                                                      
+    .start()
+
+  stream3.awaitTermination(30000)                                                                              
+  stream3.stop()
+
+  println("Delta Table after streaming upsert")                                                                
+  deltaTable.toDF.show()
+
+  // Cleanup                                                                                                   
+  FileUtils.deleteDirectory(new File(path))                                                                    
+  FileUtils.deleteDirectory(new File(tablePath2))                                                              
+  FileUtils.deleteDirectory(new File(checkpointPath))
+
+  spark.stop()                                                                                                 
 }
