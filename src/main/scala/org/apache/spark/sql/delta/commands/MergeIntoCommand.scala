6713da506b1f9259ad6df3883984336fbf60a0f8
Wesley Hoffman <wesleyhoffman109@gmail.com>
2020-03-11 11:41:26 -0700

    add conditional statement to source/target join

--- src/main/scala/org/apache/spark/sql/delta/commands/MergeIntoCommand.scala
+++ src/main/scala/org/apache/spark/sql/delta/commands/MergeIntoCommand.scala
@@ -199,31 +199,34 @@ case class MergeIntoCommand(
     }}.asNondeterministic()
 
     // Skip data based on the merge condition
-    val targetOnlyPredicates =
+    val targetOnlyPredicates: Seq[Expression] =
       splitConjunctivePredicates(condition).filter(_.references.subsetOf(target.outputSet))
     val dataSkippedFiles = deltaTxn.filterFiles(targetOnlyPredicates)
 
-    // Apply inner join to between source and target using the merge condition to find matches
-    // In addition, we attach two columns
-    // - a monotonically increasing row id for target rows to later identify whether the same
-    //     target row is modified by multiple user or not
-    // - the target file name the row is from to later identify the files touched by matched rows
-    val joinToFindTouchedFiles = {
-      val sourceDF = Dataset.ofRows(spark, source)
-      val targetDF = Dataset.ofRows(spark, buildTargetPlanWithFiles(deltaTxn, dataSkippedFiles))
-        .withColumn(ROW_ID_COL, monotonically_increasing_id())
-        .withColumn(FILE_NAME_COL, input_file_name())
-      sourceDF.join(targetDF, new Column(condition), "inner")
-    }
+    // if no target files are skipped, then skip a possibly expensive join and collect
+    if (dataSkippedFiles.nonEmpty) {
+      // Apply inner join to between source and target using the merge condition to find matches
+      // In addition, we attach two columns
+      // - a monotonically increasing row id for target rows to later identify whether the same
+      //     target row is modified by multiple user or not
+      // - the target file name the row is from to later identify the files touched by matched rows
+      val joinToFindTouchedFiles = {
+        val sourceDF = Dataset.ofRows(spark, source)
+        val targetDF = Dataset.ofRows(spark, buildTargetPlanWithFiles(deltaTxn, dataSkippedFiles))
+          .withColumn(ROW_ID_COL, monotonically_increasing_id())
+          .withColumn(FILE_NAME_COL, input_file_name())
+        sourceDF.join(targetDF, new Column(condition), "inner")
+      }
 
-    // Process the matches from the inner join to record touched files and find multiple matches
-    val collectTouchedFiles = joinToFindTouchedFiles
-      .select(col(ROW_ID_COL), recordTouchedFileName(col(FILE_NAME_COL)).as("one"))
+      // Process the matches from the inner join to record touched files and find multiple matches
+      val collectTouchedFiles = joinToFindTouchedFiles
+        .select(col(ROW_ID_COL), recordTouchedFileName(col(FILE_NAME_COL)).as("one"))
 
-    // Calculate frequency of matches per source row
-    val matchedRowCounts = collectTouchedFiles.groupBy(ROW_ID_COL).agg(sum("one").as("count"))
-    if (matchedRowCounts.filter("count > 1").count() != 0) {
-      throw DeltaErrors.multipleSourceRowMatchingTargetRowInMergeException(spark)
+      // Calculate frequency of matches per source row
+      val matchedRowCounts = collectTouchedFiles.groupBy(ROW_ID_COL).agg(sum("one").as("count"))
+      if (matchedRowCounts.filter("count > 1").count() != 0) {
+        throw DeltaErrors.multipleSourceRowMatchingTargetRowInMergeException(spark)
+      }
     }
 
     // Get the AddFiles using the touched file names.
