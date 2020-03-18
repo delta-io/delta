dc20a2739c19744e91d9047228af584c7ce73993
ekoifman <ekoifman@apache.org>
2020-03-18 18:17:46 +0000

    [DELTA-OSS-EXTERNAL] Optimize merge command when there is no whenNotMatched clause #342
    When Merge command doesn't have a whenNotMatched clause, the Full Outer Join in MergeIntoCommand.writeAllChanges can be changed to Right Outer Join. Since left/source side is usually small, this can enable Broadcast join - closes #342
    
    Closes delta-io/delta#343
    
    Author: Tathagata Das <tathagata.das1565@gmail.com>
    Author: ekoifman <ekoifman@apache.org>
    
    GitOrigin-RevId: 0bf21ea6eaa30df303a9b1ac926833c38f294e0e

--- src/test/scala/org/apache/spark/sql/delta/MergeIntoSuiteBase.scala
+++ src/test/scala/org/apache/spark/sql/delta/MergeIntoSuiteBase.scala
@@ -1769,4 +1769,113 @@ abstract class MergeIntoSuiteBase
       iter.close()
     }
   }
+
+  protected def testMatchedOnlyOptimization(
+      name: String)(
+      source: Seq[(Int, Int)],
+      target: Seq[(Int, Int)],
+      mergeOn: String,
+      mergeClauses: MergeClause*) (
+      result: Seq[(Int, Int)]): Unit = {
+    Seq(true, false).foreach { matchedOnlyEnabled =>
+      Seq(true, false).foreach { isPartitioned =>
+        val s = if (matchedOnlyEnabled) "enabled" else "disabled"
+        test(s"matched only merge - $s - $name - isPartitioned: $isPartitioned ") {
+          withKeyValueData(source, target, isPartitioned) { case (sourceName, targetName) =>
+            withSQLConf(DeltaSQLConf.MERGE_MATCHED_ONLY_ENABLED.key -> s"$matchedOnlyEnabled") {
+              executeMerge(s"$targetName t", s"$sourceName s", mergeOn, mergeClauses: _*)
+            }
+            val deltaPath = if (targetName.startsWith("delta.`")) {
+              targetName.stripPrefix("delta.`").stripSuffix("`")
+            } else targetName
+            checkAnswer(
+              readDeltaTable(deltaPath),
+              result.map { case (k, v) => Row(k, v) })
+          }
+        }
+      }
+    }
+  }
+
+  testMatchedOnlyOptimization("with update") (
+    source = Seq((1, 100), (3, 300), (5, 500)),
+    target = Seq((1, 10), (2, 20), (3, 30)),
+    mergeOn = "s.key = t.key",
+    update("t.key = s.key, t.value = s.value")) (
+    result = Seq(
+      (1, 100), // updated
+      (2, 20), // existed previously
+      (3, 300) // updated
+    )
+  )
+
+  testMatchedOnlyOptimization("with delete") (
+    source = Seq((1, 100), (3, 300), (5, 500)),
+    target = Seq((1, 10), (2, 20), (3, 30)),
+    mergeOn = "s.key = t.key",
+    delete()) (
+    result = Seq(
+      (2, 20) // existed previously
+    )
+  )
+
+  testMatchedOnlyOptimization("with update and delete")(
+    source = Seq((1, 100), (3, 300), (5, 500)),
+    target = Seq((1, 10), (3, 30), (5, 30)),
+    mergeOn = "s.key = t.key",
+    update("t.value = s.value", "t.key < 3"), delete("t.key > 3")) (
+    result = Seq(
+      (1, 100), // updated
+      (3, 30)   // existed previously
+    )
+  )
+
+  protected def testNullCaseMatchedOnly(name: String) (
+      source: Seq[(JInt, JInt)],
+      target: Seq[(JInt, JInt)],
+      mergeOn: String,
+      result: Seq[(JInt, JInt)]) = {
+    Seq(true, false).foreach { isPartitioned =>
+      withSQLConf(DeltaSQLConf.MERGE_MATCHED_ONLY_ENABLED.key -> "true") {
+        test(s"matched only merge - null handling - $name, isPartitioned: $isPartitioned") {
+          withView("sourceView") {
+            append(target.toDF("key", "value"), "key" :: Nil)
+            source.toDF("key", "value").createOrReplaceTempView("sourceView")
+
+            executeMerge(
+              tgt = s"delta.`$tempPath` as t",
+              src = "sourceView s",
+              cond = mergeOn,
+              update("t.value = s.value"))
+
+            checkAnswer(
+              readDeltaTable(tempPath),
+              result.map { r => Row(r._1, r._2) }
+            )
+
+            Utils.deleteRecursively(new File(tempPath))
+          }
+        }
+      }
+    }
+  }
+
+  testNullCaseMatchedOnly("null in source") (
+    source = Seq((1, 10), (2, 20), (null, null)),
+    target = Seq((1, 1)),
+    mergeOn = "s.key = t.key",
+    result = Seq(
+      (1, 10) // update
+    )
+  )
+
+  testNullCaseMatchedOnly("null value in both source and target") (
+    source = Seq((1, 10), (2, 20), (null, 0)),
+    target = Seq((1, 1), (null, null)),
+    mergeOn = "s.key = t.key",
+    result = Seq(
+      (null, null), // No change as null in source does not match null in target
+      (1, 10) // update
+    )
+  )
 }
