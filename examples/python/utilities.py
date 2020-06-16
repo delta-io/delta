5a320599baf816115f6d407fc51f96c357fd020a
Tathagata Das <tdas@databricks.com>
2020-06-16 04:22:34 -0400

    Update utilities.py

--- examples/python/utilities.py
+++ examples/python/utilities.py
@@ -22,7 +22,7 @@ import shutil
 import threading
 
 spark = SparkSession.builder \
-    .appName("quickstart") \
+    .appName("utilities") \
     .master("local[*]") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
