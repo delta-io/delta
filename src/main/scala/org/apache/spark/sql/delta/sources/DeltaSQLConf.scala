5c137b1f4aaef96ab1f02e20a591094416557be2
Scott Sandre <scott.sandre@databricks.com>
2020-09-15 13:17:35 +0000

    [SC-48966] Add a flag to disable writing Delta checksum file
    - added new `DeltaSQLConf` value `DELTA_WRITE_CHECKSUM_ENABLED`
    - if that conf is set to false when `Checksum.scala::writeChecksumFile` is called, then we return right away
    
    GitOrigin-RevId: 5849a60ebf3a5ddbf2a145b9f070e86713be950e

--- src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
+++ src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
@@ -342,4 +342,10 @@ object DeltaSQLConf {
       .booleanConf
       .createWithDefault(true)
 
+  val DELTA_WRITE_CHECKSUM_ENABLED =
+    buildConf("writeChecksumFile.enabled")
+      .doc("Whether the checksum file can be written.")
+      .booleanConf
+      .createWithDefault(true)
+
 }
