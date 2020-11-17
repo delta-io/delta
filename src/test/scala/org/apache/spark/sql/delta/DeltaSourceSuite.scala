5ff4f2bce402b8cde66251e34d31e003769e3bae
mahmoud MEHDI <mahmoudmahdi24@gmail.com>
2020-11-17 20:19:34 +0100

    remove unused import

--- src/test/scala/org/apache/spark/sql/delta/DeltaSourceSuite.scala
+++ src/test/scala/org/apache/spark/sql/delta/DeltaSourceSuite.scala
@@ -35,7 +35,6 @@ import org.apache.spark.sql.streaming.util.StreamManualClock
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.{ManualClock, Utils}
-import io.delta.sources._
 
 class DeltaSourceSuite extends DeltaSourceSuiteBase with DeltaSQLCommandTest {
 
