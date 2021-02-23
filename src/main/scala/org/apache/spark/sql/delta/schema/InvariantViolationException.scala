2fbfe7ef206dad411ba65c202b2b74170a17343e
Yijia Cui <yijia.cui@databricks.com>
2021-02-23 19:18:42 +0000

    [SC-70376][Delta] Minor refactor of InvariantViolationException
    Minor refactor of InvariantViolationException to extend RuntimeException instead of IllegalArgumentException
    
    Author: Yijia Cui <yijia.cui@databricks.com>
    
    GitOrigin-RevId: 99ac06bc894ab0659bf0a9b47477b1e64dcfbb0b

--- src/main/scala/org/apache/spark/sql/delta/schema/InvariantViolationException.scala
+++ src/main/scala/org/apache/spark/sql/delta/schema/InvariantViolationException.scala
@@ -16,15 +16,15 @@
 
 package org.apache.spark.sql.delta.schema
 
+// scalastyle:off import.ordering.noEmptyLine
 import scala.collection.JavaConverters._
 
 import org.apache.spark.sql.delta.constraints.Constraints
 
-import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
 
 /** Thrown when the given data doesn't match the rules defined on the table. */
-case class InvariantViolationException(msg: String) extends IllegalArgumentException(msg)
+case class InvariantViolationException(message: String) extends RuntimeException(message)
 
 object InvariantViolationException {
   def apply(constraint: Constraints.NotNull): InvariantViolationException = {
