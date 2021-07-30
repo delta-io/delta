74c801c4a4f498d1ec0793f6a1f3c582a239a669
Scott Sandre <scott.sandre@databricks.com>
2021-07-30 09:05:25 -0700

    remove backslash

--- python/run-tests.py
+++ python/run-tests.py
@@ -130,9 +130,9 @@ def run_pypi_packaging_tests(root_dir):
     delete_if_exists(wheel_dist_dir)
 
     # generate artifacts
-    run_cmd( \
-        ["python3", "setup.py", "bdist_wheel"], \
-        stream_output=True, \
+    run_cmd(
+        ["python3", "setup.py", "bdist_wheel"],
+        stream_output=True,
         stderr=open('/dev/null', 'w'))
 
     run_cmd(["python3", "setup.py", "sdist"], stream_output=True)
