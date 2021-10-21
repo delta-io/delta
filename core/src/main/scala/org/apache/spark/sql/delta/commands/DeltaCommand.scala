481021a588ab91c775c5912ed0629e95957ff1f0
Scott Sandre <scott.sandre@databricks.com>
2021-10-21 15:10:31 -0700

    [SC-87924] Remove try and catches for deltaLog.checkpoint
    Removes 3 occurrences of `deltaLog.checkpoint` that was wrapped by a `try { ... } catch { case e: IllegalStateException } `
    
    GitOrigin-RevId: 4821acd730f3ee7faeb54a7b02e16a84386aa497

--- core/src/main/scala/org/apache/spark/sql/delta/commands/DeltaCommand.scala
+++ core/src/main/scala/org/apache/spark/sql/delta/commands/DeltaCommand.scala
@@ -232,12 +232,7 @@ trait DeltaCommand extends DeltaLogging {
 
     logInfo(s"Committed delta #$attemptVersion to ${deltaLog.logPath}. Wrote $commitSize actions.")
 
-    try {
-      deltaLog.checkpoint(currentSnapshot)
-    } catch {
-      case e: IllegalStateException =>
-        logWarning("Failed to checkpoint table state.", e)
-    }
+    deltaLog.checkpoint(currentSnapshot)
   }
 
   /**
