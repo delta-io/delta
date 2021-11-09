b2452850689b916d9432ce979c0b938155945034
Wenchen Fan <wenchen@databricks.com>
2021-11-09 18:18:18 +0000

    Minor Refactoring GeneratedColumnSuite
    Author: Wenchen Fan <wenchen@databricks.com>
    
    GitOrigin-RevId: 3ccb4391361c414368d56df5ac150022fc01a087

--- core/src/test/scala/org/apache/spark/sql/delta/GeneratedColumnSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/delta/GeneratedColumnSuite.scala
@@ -533,8 +533,8 @@ trait GeneratedColumnSuiteBase extends GeneratedColumnTest {
     spark.udf.register("myudf", (s: Array[Int]) => s)
     for ((exprString, error) <- Seq(
       "myudf(foo)" -> "Found myudf(foo). A generated column cannot use a user-defined function",
-      "first(foo)" ->
-        "Found first(foo). A generated column cannot use a non deterministic expression",
+      "rand()" ->
+        "Found rand(). A generated column cannot use a non deterministic expression",
       "max(foo)" -> "Found max(foo). A generated column cannot use an aggregate expression",
       "explode(foo)" -> "explode(foo) cannot be used in a generated column",
       "current_timestamp" -> "current_timestamp() cannot be used in a generated column"
