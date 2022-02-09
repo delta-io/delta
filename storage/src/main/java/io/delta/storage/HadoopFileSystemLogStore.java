c4fe04ffb036bf2fcd887d8f659da8a166601c6b
Scott Sandre <scott.sandre@databricks.com>
2022-02-09 10:21:50 -0800

    Fix indentation in HadoopFileSystemLogStore

--- storage/src/main/java/io/delta/storage/HadoopFileSystemLogStore.java
+++ storage/src/main/java/io/delta/storage/HadoopFileSystemLogStore.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (2021) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package io.delta.storage;
 
 import java.io.*;
@@ -34,14 +51,14 @@ public abstract class HadoopFileSystemLogStore extends LogStore {
         FileSystem fs = path.getFileSystem(hadoopConf);
         if (!fs.exists(path.getParent())) {
             throw new FileNotFoundException(
-                    String.format("No such file or directory: %s", path.getParent())
+                String.format("No such file or directory: %s", path.getParent())
             );
         }
         FileStatus[] files = fs.listStatus(path.getParent());
         return Arrays.stream(files)
-                .filter(f -> f.getPath().getName().compareTo(path.getName()) >= 0)
-                .sorted(Comparator.comparing(o -> o.getPath().getName()))
-                .iterator();
+            .filter(f -> f.getPath().getName().compareTo(path.getName()) >= 0)
+            .sorted(Comparator.comparing(o -> o.getPath().getName()))
+            .iterator();
     }
 
     @Override
