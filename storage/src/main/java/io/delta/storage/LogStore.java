3eed2e81282ef9c2100a7a0ad18e61db2945eff0
Scott Sandre <scott.sandre@databricks.com>
2022-02-09 09:50:02 -0800

    Update LogStore.java

--- storage/src/main/java/io/delta/storage/LogStore.java
+++ storage/src/main/java/io/delta/storage/LogStore.java
@@ -20,29 +20,35 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.Iterator;
 
 /**
  * :: DeveloperApi ::
  *
+ * <p></p>
+ *
  * General interface for all critical file system operations required to read and write the
  * Delta logs. The correctness is predicated on the atomicity and durability guarantees of
  * the implementation of this interface. Specifically,
  *
- * 1. Atomic visibility of files: If isPartialWriteVisible is false, any file written through
- * this store must be made visible atomically. In other words, this should not generate partial
- * files.
+ * <ol>
+ *     <li> Atomic visibility of files: If isPartialWriteVisible is false, any file written through
+ *          this store must be made visible atomically. In other words, this should not generate
+ *          partial files. </li>
  *
- * 2. Mutual exclusion: Only one writer must be able to create (or rename) a file at the final
- *    destination.
+ *    <li> Mutual exclusion: Only one writer must be able to create (or rename) a file at the final
+ *         destination. </li>
  *
- * 3. Consistent listing: Once a file has been written in a directory, all future listings for
- *    that directory must return that file.
+ *    <li> Consistent listing: Once a file has been written in a directory, all future listings for
+ *    that directory must return that file. </li>
+ * </ol>
  *
  * All subclasses of this interface is required to have a constructor that takes Configuration
  * as a single parameter. This constructor is used to dynamically create the LogStore.
+ *
+ * <p></p>
  *
  * LogStore and its implementations are not meant for direct access but for configuration based
  * on storage system. See [[https://docs.delta.io/latest/delta-storage.html]] for details.
@@ -72,9 +78,10 @@ public abstract class LogStore {
    * Load the given file and return an `Iterator` of lines, with line breaks removed from each line.
    * Callers of this function are responsible to close the iterator if they are done with it.
    *
+   * @throws IOException if there's an issue resolving the FileSystem
    * @since 1.0.0
    */
-  public abstract CloseableIterator<String> read(Path path, Configuration hadoopConf);
+  public abstract CloseableIterator<String> read(Path path, Configuration hadoopConf) throws IOException;
 
   /**
    * :: DeveloperApi ::
@@ -85,6 +92,7 @@ public abstract class LogStore {
    * implementation must ensure that the entire file is made visible atomically, that is,
    * it should not generate partial files.
    *
+   * @throws IOException if there's an issue resolving the FileSystem
    * @since 1.0.0
    */
   public abstract void write(
@@ -99,20 +107,23 @@ public abstract class LogStore {
    * List the paths in the same directory that are lexicographically greater or equal to
    * (UTF-8 sorting) the given `path`. The result should also be sorted by the file name.
    *
+   * @throws IOException if there's an issue resolving the FileSystem
+   * @throws FileAlreadyExistsException if {@code path} directory can't be found
    * @since 1.0.0
    */
   public abstract Iterator<FileStatus> listFrom(
       Path path,
-      Configuration hadoopConf) throws FileNotFoundException;
+      Configuration hadoopConf) throws IOException;
 
   /**
    * :: DeveloperApi ::
    *
    * Resolve the fully qualified path for the given `path`.
    *
+   * @throws IOException if there's an issue resolving the FileSystem
    * @since 1.0.0
    */
-  public abstract Path resolvePathOnPhysicalStorage(Path path, Configuration hadoopConf);
+  public abstract Path resolvePathOnPhysicalStorage(Path path, Configuration hadoopConf) throws IOException;
 
   /**
    * :: DeveloperApi ::
