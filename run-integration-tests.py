cf68635f830b961fd8a6b70a4b0f26552e601a92
allisonport-db <allison.portis@databricks.com>
2022-03-10 14:08:10 -0800

    switch to publishM2

--- run-integration-tests.py
+++ run-integration-tests.py
@@ -36,7 +36,7 @@ def run_scala_integration_tests(root_dir, version, test_name, extra_maven_repo, 
           % (str(version), scala_version))
     clear_artifact_cache()
     if use_local:
-        run_cmd(["build/sbt", "publishLocal"])
+        run_cmd(["build/sbt", "publishM2"])
 
     test_dir = path.join(root_dir, "examples", "scala")
     test_src_dir = path.join(test_dir, "src", "main", "scala", "example")
@@ -65,7 +65,7 @@ def run_python_integration_tests(root_dir, version, test_name, extra_maven_repo,
     print("\n\n##### Running Python tests on version %s #####" % str(version))
     clear_artifact_cache()
     if use_local:
-        run_cmd(["build/sbt", "publishLocal"])
+        run_cmd(["build/sbt", "publishM2"])
 
     test_dir = path.join(root_dir, path.join("examples", "python"))
     files_to_skip = {"using_with_pip.py"}
