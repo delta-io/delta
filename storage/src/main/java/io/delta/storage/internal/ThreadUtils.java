ed291cbe39b6d0e1872ddec7a7f61a13c94453ad
Kapil Sreedharan <kapilsreed@gmail.com>
2022-03-27 03:12:29 +0200

    Fix for ThreadUnitls, removed Java9 TakeWhile & DropWhile

--- storage/src/main/java/io/delta/storage/internal/ThreadUtils.java
+++ storage/src/main/java/io/delta/storage/internal/ThreadUtils.java
@@ -16,13 +16,11 @@
 
 package io.delta.storage.internal;
 
-import io.delta.storage.GCSLogStore;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.stream.Stream;
 
 public class ThreadUtils {
 
@@ -31,12 +29,12 @@ public class ThreadUtils {
      * Run a piece of code in a new thread and return the result. Exception in the new thread is
      * thrown in the caller thread with an adjusted stack trace that removes references to this
      * method for clarity. The exception stack traces will be like the following
-     *
+     * <p>
      * SomeException: exception-message
-     *   at CallerClass.body-method (sourcefile.java)
-     *   at ... run in separate thread using org.apache.spark.util.ThreadUtils ... ()
-     *   at CallerClass.caller-method (sourcefile.java)
-     *   ...
+     * at CallerClass.body-method (sourcefile.java)
+     * at ... run in separate thread using org.apache.spark.util.ThreadUtils ... ()
+     * at CallerClass.caller-method (sourcefile.java)
+     * ...
      */
     public static <T> T runInNewThread(
             String threadName,
@@ -58,35 +56,52 @@ public class ThreadUtils {
         };
         thread.setDaemon(isDaemon);
         thread.start();
+        thread.join();
         if (!exceptionHolder.isEmpty()) {
             Exception realException = exceptionHolder.get(0);
             // Remove the part of the stack that shows method calls into this helper method
             // This means drop everything from the top until the stack element
-            // ThreadUtils.runInNewThread(), and then drop that as well (hence the `drop(1)`).
-            Stream<StackTraceElement> baseStackTrace = Arrays.stream(
-                    Thread.currentThread().getStackTrace()).dropWhile(t ->
-                    !t.getClassName().contains(GCSLogStore.class.getSimpleName())).skip(1);
+            // ThreadUtils.runInNewThread(), and then drop that as well (hence the + 1 to start index).
+            List<StackTraceElement> currentThreadStackTrace =
+                    Arrays.asList(Thread.currentThread().getStackTrace());
+            int startIndex = currentThreadStackTrace.indexOf(ThreadUtils.class.getSimpleName()) + 1;
+            int endIndex = currentThreadStackTrace.size();
+            List<StackTraceElement> baseStackTrace;
+            if (startIndex <= 0 || startIndex >= endIndex) {
+                baseStackTrace = Collections.emptyList();
+            } else {
+                baseStackTrace =
+                        currentThreadStackTrace.subList(startIndex, currentThreadStackTrace.size());
+            }
             // Remove the part of the new thread stack that shows methods call from this helper method
-            Stream<StackTraceElement> extraStackTrace = Arrays.stream(
-                    realException.getStackTrace()).takeWhile(e ->
-                    !e.getClassName().contains(GCSLogStore.class.getSimpleName()));
+            // This means take everything from the top until the stack element
+            StackTraceElement[] realExceptionStackTrace = realException.getStackTrace();
+            List<StackTraceElement> extraStackTrace = new ArrayList<>();
+            for (StackTraceElement st : realExceptionStackTrace) {
+                if (st.getClassName().contains(ThreadUtils.class.getSimpleName())) {
+                    break;
+                }
+                extraStackTrace.add(st);
+            }
             // Combine the two stack traces, with a place holder just specifying that there
             // was a helper method used, without any further details of the helper
-            Stream<StackTraceElement> placeHolderStackElem = Stream.of(
+            List<StackTraceElement> placeHolderStackElem = Arrays.asList(
                     new StackTraceElement(
                             String.format(
                                     "... run in separate thread using $s",
-                                    GCSLogStore.class.getSimpleName(),
+                                    ThreadUtils.class.getSimpleName(),
                                     " static method runInNewThread"),
                             "",
                             "",
                             -1)
             );
-            StackTraceElement[] finalStackTrace = Stream.concat(
-                    Stream.concat(baseStackTrace,
-                            placeHolderStackElem),
-                    baseStackTrace).toArray(StackTraceElement[]::new);
-            realException.setStackTrace(finalStackTrace);
+
+            List<StackTraceElement> finalStackTrace = new ArrayList<>();
+            finalStackTrace.addAll(extraStackTrace);
+            finalStackTrace.addAll(placeHolderStackElem);
+            finalStackTrace.addAll(baseStackTrace);
+
+            realException.setStackTrace(finalStackTrace.toArray(StackTraceElement[]::new));
             throw realException;
         } else {
             return resultHolder.get(0);
