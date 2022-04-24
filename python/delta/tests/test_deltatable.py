3f993152c7880a2bac3041bb8562dfc502adba07
Burak Yilmaz <burakyilmaz321@gmail.com>
2022-04-25 00:01:29 +0200

    Add unit tests for python api optimize
    Signed-off-by: Burak Yilmaz <burakyilmaz321@gmail.com>

--- python/delta/tests/test_deltatable.py
+++ python/delta/tests/test_deltatable.py
@@ -24,7 +24,7 @@ from pyspark.sql.functions import col, lit, expr
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, DataType
 from pyspark.sql.utils import AnalysisException, ParseException
 
-from delta.tables import DeltaTable, DeltaTableBuilder
+from delta.tables import DeltaTable, DeltaTableBuilder, DeltaOptimizeBuilder
 from delta.testing.utils import DeltaTestCase
 
 
@@ -849,6 +849,54 @@ class DeltaTableTests(DeltaTestCase):
             dt.restoreToVersion("0")  # type: ignore[arg-type]
         self.__intercept(runRestoreToVersion,
                          "version needs to be an int but got '<class 'str'>'")
+
+    def test_optimize(self) -> None:
+        # write an unoptimized delta table
+        df = self.spark.createDataFrame([("a", 1), ("a", 2)], ["key", "value"]).repartition(1)
+        df.write.format("delta").save(self.tempFile)
+        df = self.spark.createDataFrame([("a", 3), ("a", 4)], ["key", "value"]).repartition(1)
+        df.write.format("delta").save(self.tempFile, mode="append")
+        df = self.spark.createDataFrame([("b", 1), ("b", 2)], ["key", "value"]).repartition(1)
+        df.write.format("delta").save(self.tempFile, mode="append")
+
+        # create DeltaTable
+        dt = DeltaTable.forPath(self.spark, self.tempFile)
+
+        # execute bin compaction
+        optimizer = dt.optimize()
+        res = optimizer.executeCompaction()
+        op_params = dt.history().first().operationParameters
+
+        # assertions
+        self.assertTrue(isinstance(optimizer, DeltaOptimizeBuilder))
+        self.assertTrue(isinstance(res, DataFrame))
+        self.assertEqual(1, res.first().metrics.numFilesAdded)
+        self.assertEqual(3, res.first().metrics.numFilesRemoved)
+        self.assertDictEqual({'predicate': '["true"]'}, op_params)
+
+    def test_optimize_w_partition_filter(self) -> None:
+        # write an unoptimized delta table
+        df = self.spark.createDataFrame([("a", 1), ("a", 2)], ["key", "value"]).repartition(1)
+        df.write.partitionBy("key").format("delta").save(self.tempFile)
+        df = self.spark.createDataFrame([("a", 3), ("a", 4)], ["key", "value"]).repartition(1)
+        df.write.partitionBy("key").format("delta").save(self.tempFile, mode="append")
+        df = self.spark.createDataFrame([("b", 1), ("b", 2)], ["key", "value"]).repartition(1)
+        df.write.partitionBy("key").format("delta").save(self.tempFile, mode="append")
+
+        # create DeltaTable
+        dt = DeltaTable.forPath(self.spark, self.tempFile)
+
+        # execute bin compaction
+        optimizer = dt.optimize().partitionFilter("key = 'a'")
+        res = optimizer.executeCompaction()
+        op_params = dt.history().first().operationParameters
+
+        # assertions
+        self.assertTrue(isinstance(optimizer, DeltaOptimizeBuilder))
+        self.assertTrue(isinstance(res, DataFrame))
+        self.assertEqual(1, res.first().metrics.numFilesAdded)
+        self.assertEqual(2, res.first().metrics.numFilesRemoved)
+        self.assertDictEqual({'predicate': '["(key = \'a\')"]'}, op_params)
 
     def __checkAnswer(self, df: DataFrame,
                       expectedAnswer: List[Any],
