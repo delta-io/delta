2f2242918e93bf38893ce739b00b2b254403e321
Shixiong Zhu <zsxwing@gmail.com>
2022-05-20 08:12:35 -0700

    Revert "Remove unused DeltaConfigs"
    This reverts commit f50ecb6e0a3c18d7e2d0ac0600aa6a2d1d7fbdc7
    
    We realized removing DeltaConfigs could break old Delta versions. So just revert the change. This is a clean revert.
    
    Existing tests
    
    GitOrigin-RevId: 8eca43edefb83fce64fbb3f6d76020970ee01abb

--- core/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
+++ core/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
@@ -180,6 +180,13 @@ trait DeltaSQLConfBase {
       .intConf
       .createWithDefault(1000000)
 
+  val DELTA_SAMPLE_ESTIMATOR_ENABLED =
+    buildConf("sampling.enabled")
+      .internal()
+      .doc("Enable sample based estimation.")
+      .booleanConf
+      .createWithDefault(false)
+
   val DELTA_CONVERT_METADATA_CHECK_ENABLED =
     buildConf("convert.metadataCheck.enabled")
       .doc(
