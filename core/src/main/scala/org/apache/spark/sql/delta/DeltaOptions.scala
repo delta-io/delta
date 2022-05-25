1a1429a4cd73a14b606a730ba6f86364b2ef71e5
Tathagata Das <tathagata.das1565@gmail.com>
2022-05-25 14:27:15 -0400

    Idempotent writes using Dataframe write options
    This PR makes changes to support idempotent delta writes within foreachBatch() command. Prior to this change, streaming delta writes within foreachBatch command were not idempotent -- that is, rerunning a failed batch could result in duplicate data writes.
    
    With this change, we expose two options to the users:
    - txnAppId: A unique string that customers can pass on each dataframe write. For example, customers can use the StreamingQuery's id as appId.
    - txnVersion: A monotonically increasing number that acts as transaction version. The streaming batch id can be used as the txnVersion.
    
    Combination of txnAppId and txnVersion are used to identify duplicate writes and ignore duplicate writes by the runtime. Specifically, WriteIntoDelta class is modified to persist the transaction state (which includes the txnAppId and txnVersion) and consult on new writes to the same location.
    
    Example:
    ```
    df.write.format("delta").option("txnAppId", "myapp").option("txnVersion", 1).save(tablePath)
    df.write.format("delta").option("txnAppId", "myapp").option("txnVersion", 1).save(tablePath) // this will be ignored
    ```
    
    Closes delta-io/delta#1141
    
    New unit tests
    
    GitOrigin-RevId: aa9836a2498a65477584d8b60681246b972e4378

--- core/src/main/scala/org/apache/spark/sql/delta/DeltaOptions.scala
+++ core/src/main/scala/org/apache/spark/sql/delta/DeltaOptions.scala
@@ -93,6 +93,26 @@ trait DeltaWriteOptionsImpl extends DeltaOptionParser {
     options.get(DATA_CHANGE_OPTION).exists(!toBoolean(_, DATA_CHANGE_OPTION))
   }
 
+  val txnVersion = options.get(TXN_VERSION).map { str =>
+    Try(str.toLong).toOption.filter(_ >= 0).getOrElse {
+      throw DeltaErrors.illegalDeltaOptionException(
+        TXN_VERSION, str, "must be a non-negative integer")
+    }
+  }
+
+  val txnAppId = options.get(TXN_APP_ID)
+
+  private def validateIdempotentWriteOptions(): Unit = {
+    // Either both txnVersion and txnAppId must be specified to get idempotent writes or
+    // neither must be given. In all other cases, throw an exception.
+    val numOptions = txnVersion.size + txnAppId.size
+    if (numOptions != 0 && numOptions != 2) {
+      throw DeltaErrors.invalidIdempotentWritesOptionsException("Both txnVersion and txnAppId " +
+      "must be specified for idempotent data frame writes")
+    }
+  }
+
+  validateIdempotentWriteOptions()
 }
 
 trait DeltaReadOptions extends DeltaOptionParser {
@@ -195,6 +215,8 @@ object DeltaOptions extends DeltaLogging {
   val CDC_READ_OPTION_LEGACY = "readChangeData"
   val COMPRESSION = "compression"
   val MAX_RECORDS_PER_FILE = "maxRecordsPerFile"
+  val TXN_APP_ID = "txnAppId"
+  val TXN_VERSION = "txnVersion"
 
   val validOptionKeys : Set[String] = Set(
     REPLACE_WHERE_OPTION,
@@ -219,6 +241,8 @@ object DeltaOptions extends DeltaLogging {
     CDC_END_VERSION,
     COMPRESSION,
     MAX_RECORDS_PER_FILE,
+    TXN_APP_ID,
+    TXN_VERSION,
     "queryName",
     "checkpointLocation",
     "path",
