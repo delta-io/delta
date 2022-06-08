fa3a8737467cfb30249582cd7388404ee2ae6e1f
Wenchen Fan <wenchen@databricks.com>
2022-06-08 09:49:25 +0800

    Minor refactoring in DeltaTable
    GitOrigin-RevId: f0580e2cccca2479a803907b797afca8be696a05

--- core/src/main/scala/org/apache/spark/sql/delta/DeltaTable.scala
+++ core/src/main/scala/org/apache/spark/sql/delta/DeltaTable.scala
@@ -31,8 +31,7 @@ import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.analysis.NoSuchTableException
 import org.apache.spark.sql.catalyst.catalog.{CatalogTable, SessionCatalog}
 import org.apache.spark.sql.catalyst.expressions.{And, AttributeReference, Expression, NamedExpression, Or, PredicateHelper, SubqueryExpression}
-import org.apache.spark.sql.catalyst.planning.PhysicalOperation
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.catalyst.plans.logical.{Filter, LogicalPlan, Project}
 import org.apache.spark.sql.connector.expressions.{FieldReference, IdentityTransform}
 import org.apache.spark.sql.execution.datasources.{FileFormat, FileIndex, HadoopFsRelation, LogicalRelation}
 import org.apache.spark.sql.internal.SQLConf
@@ -56,9 +55,13 @@ object DeltaTable {
  */
 object DeltaFullTable {
   def unapply(a: LogicalPlan): Option[TahoeLogFileIndex] = a match {
-    case PhysicalOperation(_, filters, lr @ DeltaTable(index: TahoeLogFileIndex)) =>
+    // `DeltaFullTable` is not only used to match a certain query pattern, but also does
+    // some validations to throw errors. We need to match both Project and Filter here,
+    // so that we can check if Filter is present or not during validations.
+    case NodeWithOnlyDeterministicProjectAndFilter(DeltaTable(index: TahoeLogFileIndex)) =>
       if (!index.deltaLog.tableExists) return None
-      if (index.partitionFilters.isEmpty && index.versionToUse.isEmpty && filters.isEmpty) {
+      val hasFilter = a.find(_.isInstanceOf[Filter]).isDefined
+      if (index.partitionFilters.isEmpty && index.versionToUse.isEmpty && !hasFilter) {
         Some(index)
       } else if (index.versionToUse.nonEmpty) {
         throw DeltaErrors.failedScanWithHistoricalVersion(index.versionToUse.get)
@@ -69,6 +72,15 @@ object DeltaFullTable {
     case DeltaRelation(lr) => unapply(lr)
     case _ =>
       None
+  }
+}
+
+// TODO: remove this after Spark 3.4 is released.
+object NodeWithOnlyDeterministicProjectAndFilter {
+  def unapply(plan: LogicalPlan): Option[LogicalPlan] = plan match {
+    case Project(projectList, child) if projectList.forall(_.deterministic) => unapply(child)
+    case Filter(cond, child) if cond.deterministic => unapply(child)
+    case _ => Some(plan)
   }
 }
 
