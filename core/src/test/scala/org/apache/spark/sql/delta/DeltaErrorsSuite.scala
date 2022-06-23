7e39161e1af30f4cb5b720c98d927bad709b23b1
Scott Sandre <scott.sandre@databricks.com>
2022-06-23 09:48:10 -0700

    add new error and test

--- core/src/test/scala/org/apache/spark/sql/delta/DeltaErrorsSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/delta/DeltaErrorsSuite.scala
@@ -2176,6 +2176,15 @@ trait DeltaErrorsSuiteBase
       assert(e.getSqlState == "42000")
       assert(e.getMessage == s"Can't set location multiple times. Found ${locations}")
     }
+    {
+      val e = intercept[DeltaUnsupportedOperationException] {
+        throw DeltaErrors.blockCdfAndColumnMappingReads()
+      }
+      assert(e.getErrorClass == "DELTA_BLOCK_CDF_COLUMN_MAPPING_READS")
+      assert(e.getSqlState == "0A000")
+      assert(e.getMessage == "Change data feed (CDF) reads are currently not supported on tables " +
+        "with column mapping enabled.")
+    }
   }
 }
 
