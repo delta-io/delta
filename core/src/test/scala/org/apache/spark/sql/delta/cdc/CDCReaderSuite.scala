ca74733669165c6878ac6c5e1d6b6a6772931444
Lars Kroll <lars.kroll@databricks.com>
2022-07-20 10:14:44 +0200

    Minor refactor to CDCReaderSuite
    GitOrigin-RevId: 9f678845d439d10a9c525f2d21e82cafb1ed8eff

--- core/src/test/scala/org/apache/spark/sql/delta/cdc/CDCReaderSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/delta/cdc/CDCReaderSuite.scala
@@ -18,10 +18,12 @@ package org.apache.spark.sql.delta.cdc
 
 // scalastyle:off import.ordering.noEmptyLine
 import org.apache.spark.sql.delta._
-import org.apache.spark.sql.delta.actions.{Action, AddCDCFile}
+import org.apache.spark.sql.delta.DeltaOperations.Delete
+import org.apache.spark.sql.delta.actions.{Action, AddCDCFile, AddFile}
 import org.apache.spark.sql.delta.commands.cdc.CDCReader
 import org.apache.spark.sql.delta.commands.cdc.CDCReader._
 import org.apache.spark.sql.delta.files.DelayedCommitProtocol
+import org.apache.spark.sql.delta.sources.DeltaSQLConf
 import org.apache.spark.sql.delta.test.DeltaSQLCommandTest
 import org.apache.hadoop.fs.Path
 
