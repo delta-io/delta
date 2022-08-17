f768a0f839e5fd5a3dc9c41bdc881ee08dbf299a
Joe Harris <joe.harris@databricks.com>
2022-08-16 22:48:34 -0400

    Minor update to benchmarks.py
    GitOrigin-RevId: 1335f6f32c79ba40ce01e7052522f23d9580b3c6

--- benchmarks/scripts/benchmarks.py
+++ benchmarks/scripts/benchmarks.py
@@ -116,6 +116,8 @@ class TPCDSBenchmarkSpec(BenchmarkSpec):
             "--scale-in-gb", str(scale_in_gb)
         ])
 
+
+
 # ============== Delta benchmark specifications ==============
 
 
@@ -178,7 +180,6 @@ class ParquetBenchmarkSpec(BenchmarkSpec):
             main_class_args=main_class_args,
             **kwargs
         )
-
 
 class ParquetTPCDSDataLoadSpec(TPCDSDataLoadSpec, ParquetBenchmarkSpec):
     def __init__(self, scale_in_gb=1):
@@ -244,10 +245,10 @@ set -e
         shell_cmd = self.benchmark_spec.get_sparkshell_cmd(jar_path, shell_init_file_name)
         return f"""
 #!/bin/bash
-jps | grep "Spark" | cut -f 1 -d ' ' |  xargs kill -9 
-echo '{shell_init_file_content}' > {shell_init_file_name} 
+jps | grep "Spark" | cut -f 1 -d ' ' |  xargs kill -9
+echo '{shell_init_file_content}' > {shell_init_file_name}
 {shell_cmd} 2>&1 | tee {self.output_file}
-touch {self.completed_file} 
+touch {self.completed_file}
 """.strip()
 
     def upload_jar_to_cluster(self, cluster_hostname, ssh_id_file, ssh_user, delta_version_to_use=None):
@@ -277,10 +278,10 @@ package='screen'
 if [ -x "$(command -v yum)" ]; then
     if rpm -q $package; then
         echo "$package has already been installed"
-    else	    
+    else
         sudo yum -y install $package
     fi
-elif [ -x "$(command -v apt)" ]; then 
+elif [ -x "$(command -v apt)" ]; then
     if dpkg -s $package; then
         echo "$package has already been installed"
     else
