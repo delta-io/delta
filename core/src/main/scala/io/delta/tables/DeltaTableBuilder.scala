2589f480e9ecb3baeecc7392781aa3cff7399e65
Helge Bruegner <helge@bruegner.de>
2022-11-08 22:06:04 +0100

    revert import changes

--- core/src/main/scala/io/delta/tables/DeltaTableBuilder.scala
+++ core/src/main/scala/io/delta/tables/DeltaTableBuilder.scala
@@ -16,19 +16,20 @@
 
 package io.delta.tables
 
+import scala.collection.mutable
+
+import org.apache.spark.sql.delta.{DeltaErrors, DeltaTableUtils}
+import org.apache.spark.sql.delta.sources.DeltaSQLConf
 import io.delta.tables.execution._
+
 import org.apache.spark.annotation._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.plans.logical.{CreateTable, LogicalPlan, ReplaceTable}
 import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
 import org.apache.spark.sql.connector.expressions.Transform
-import org.apache.spark.sql.delta.sources.DeltaSQLConf
-import org.apache.spark.sql.delta.{DeltaErrors, DeltaTableUtils}
 import org.apache.spark.sql.execution.SQLExecution
 import org.apache.spark.sql.types.{DataType, StructField, StructType}
-
-import scala.collection.mutable
 
 /**
  * :: Evolving ::
