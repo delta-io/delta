4ac78e38c5b7b8b85329926e4b049f679cb8a39f
Peter Dowdy <peter.dowdy@gmail.com>
2023-02-25 23:41:23 -0800

    One more test fix
    Signed-off-by: Peter Dowdy <peter.dowdy@gmail.com>

--- core/src/test/scala/org/apache/spark/sql/delta/MergeIntoScalaSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/delta/MergeIntoScalaSuite.scala
@@ -597,7 +597,7 @@ class MergeIntoScalaSuite extends MergeIntoSuiteBase  with MergeIntoNotMatchedBy
         .execute()
 
       checkAnswer(readDeltaUserMetadataByPath(tempPath),
-        Row(null) :: Row(null) :: Row("test user metadata") :: Nil)
+        Row(null) :: Row("test user metadata") :: Nil)
     }
   }
 
