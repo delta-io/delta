1e6f40f8faf0560e53341abbcb44c27cac014651
Paddy Xu <xupaddy@gmail.com>
2023-03-09 15:50:33 +0100

    disable CDC reads from DV

--- core/src/main/scala/org/apache/spark/sql/delta/commands/cdc/CDCReader.scala
+++ core/src/main/scala/org/apache/spark/sql/delta/commands/cdc/CDCReader.scala
@@ -501,9 +501,11 @@ trait CDCReaderImpl extends DeltaLogging {
             totalFiles += 1L
             totalBytes += c.size
           case a: AddFile =>
+            assertNoDeletionVectors(v, a)
             totalFiles += 1L
             totalBytes += a.size
           case r: RemoveFile =>
+            assertNoDeletionVectors(v, r)
             totalFiles += 1L
             totalBytes += r.size.getOrElse(0L)
           case i: CommitInfo => commitInfo = Some(i)
@@ -707,6 +709,17 @@ trait CDCReaderImpl extends DeltaLogging {
    */
   def isCDCEnabledOnTable(metadata: Metadata, spark: SparkSession): Boolean = {
     ChangeDataFeedTableFeature.metadataRequiresFeatureToBeEnabled(metadata, spark)
+  }
+
+  def assertNoDeletionVectors(version: Long, file: FileAction): Unit = {
+    val dv = file match {
+      case a: AddFile => a.deletionVector
+      case r: RemoveFile => r.deletionVector
+      case _ => null
+    }
+    if (dv != null) {
+      throw DeltaErrors.changeDataFeedNotSupportedWithDeletionVectors(version)
+    }
   }
 
   /**
