dd72a517a152f76f80855ffae517d852663dac67
Lukas Rupprecht <lukas.rupprecht@databricks.com>
2023-03-27 14:39:10 -0700

    Fixes and adds more tests to DeltaTableUtilsSuite
    This PR fixes a failing test in DeltaTableUtilsSuite and adds more test cases.
    
    GitOrigin-RevId: dd462ac9997b786f42587fe0089b8c5150ecb212

--- core/src/test/scala/org/apache/spark/sql/delta/DeltaTableUtilsSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/delta/DeltaTableUtilsSuite.scala
@@ -16,14 +16,30 @@
 
 package org.apache.spark.sql.delta
 
-// scalastyle:off import.ordering.noEmptyLine
-import org.apache.hadoop.fs.Path
+import java.net.URI
+
+import org.apache.spark.sql.delta.test.DeltaSQLCommandTest
+import org.apache.hadoop.fs.{Path, RawLocalFileSystem}
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.test.SharedSparkSession
 
-class DeltaTableUtilsSuite extends SharedSparkSession {
+class DeltaTableUtilsSuite extends SharedSparkSession with DeltaSQLCommandTest {
 
+  override protected def sparkConf: SparkConf = super.sparkConf
+    .set("spark.hadoop.fs.s3.impl", classOf[MockS3FileSystem].getCanonicalName)
+
+  test("findDeltaTableRoot correctly combines paths") {
+    val path1 = new Path("s3://my-bucket")
+    assert(DeltaTableUtils.findDeltaTableRoot(spark, path1).isEmpty)
+    val path2 = new Path("s3://my-bucket/")
+    assert(DeltaTableUtils.findDeltaTableRoot(spark, path2).isEmpty)
+    withTempDir { dir =>
+      sql(s"CREATE TABLE myTable (id INT) USING DELTA LOCATION '${dir.getAbsolutePath}'")
+      val path = new Path(s"file://${dir.getAbsolutePath}")
+      assert(DeltaTableUtils.findDeltaTableRoot(spark, path).contains(path))
+    }
+  }
 
   test("safeConcatPaths") {
     val basePath = new Path("s3://my-bucket/subfolder")
@@ -33,4 +49,9 @@ class DeltaTableUtilsSuite extends SharedSparkSession {
     assert(DeltaTableUtils.safeConcatPaths(basePathEmpty, "_delta_log") ==
       new Path("s3://my-bucket/_delta_log"))
   }
+}
+
+private class MockS3FileSystem extends RawLocalFileSystem {
+  override def getScheme: String = "s3"
+  override def getUri: URI = URI.create("s3://my-bucket")
 }
