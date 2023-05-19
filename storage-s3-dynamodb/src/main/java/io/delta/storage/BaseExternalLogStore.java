0a6d532502390cb6bbb75c91e13fb3cc1a8c58dc
Allison Portis <allison.portis@databricks.com>
2023-05-18 20:21:01 -0700

    catch exception

--- storage-s3-dynamodb/src/main/java/io/delta/storage/BaseExternalLogStore.java
+++ storage-s3-dynamodb/src/main/java/io/delta/storage/BaseExternalLogStore.java
@@ -374,6 +374,11 @@ public abstract class BaseExternalLogStore extends HadoopFileSystemLogStore {
     /**
      * Method for assuring consistency on filesystem according to the external cache.
      * Method tries to rewrite TransactionLog entry from temporary path if it does not exist.
+     *
+     * Should never throw a FileAlreadyExistsException.
+     * - If we see one when copying the temp file, we can assume the target file N.json already
+     *   exists and a concurrent writer has already copied the contents of T(N).
+     * - We will never see one when writing to the external cache since overwrite=true.
      */
     private void fixDeltaLog(FileSystem fs, ExternalCommitEntry entry) throws IOException {
         if (entry.complete) {
@@ -396,7 +401,12 @@ public abstract class BaseExternalLogStore extends HadoopFileSystemLogStore {
                     fixDeltaLogPutCompleteDbEntry(entry);
                     LOG.info("fixed file {}", entry.fileName);
                     return;
-                } catch(Throwable e) {
+                } catch (java.nio.file.FileAlreadyExistsException e) {
+                    LOG.info("file already copied {}:", e.getClass().getSimpleName(), e);
+                    copied = true;
+                    // should we fixDeltaLogPutCompleteDbEntry after this to be safe?
+                    // Don't return so that we mark Db entry as complete
+                } catch (Throwable e) {
                     LOG.info("{}:", e.getClass().getSimpleName(), e);
                     if (retry >= 3) {
                         throw e;
