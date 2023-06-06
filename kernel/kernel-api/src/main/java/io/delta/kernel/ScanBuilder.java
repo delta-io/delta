125664e2be8b727753c7e2b9b5220a25168c3847
Allison Portis <allison.portis@databricks.com>
2023-06-05 19:40:22 -0700

    respond to comments

--- kernel/kernel-api/src/main/java/io/delta/kernel/ScanBuilder.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/ScanBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (2023) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.delta.kernel;
 
 import io.delta.kernel.client.TableClient;
@@ -23,7 +39,8 @@ public interface ScanBuilder {
             throws InvalidExpressionException;
 
     /**
-     * Apply the given <i>readSchema</i>.
+     * Apply the given <i>readSchema</i>. If the builder already has a projection applied, calling
+     * this again replaces the existing projection.
      *
      * @param tableClient {@link TableClient} instance to use in Delta Kernel.
      * @param readSchema Subset of columns to read from the Delta table.
