2f857e6fccf129413d3ca55c10dacb6f8cf5676c
Johan Lasperas <johan.lasperas@databricks.com>
2023-06-23 23:34:10 +0200

    Improve generating and writing out changes in Merge
    This changes is part of a larger effort to improve merge performance, see https://github.com/delta-io/delta/issues/1827
    
    ## Description
    This change rewrites the way modified data is written out in merge to improve performance. `writeAllChanges` now generates a dataframe containing all the updated and copied rows to write out by building a large expression that selectively applies the right merge action to each row. This replaces the previous method that relied on applying a function to individual rows.
    
    Changes:
    - Move `findTouchedFiles` and `writeAllchanges` to a dedicated new trait `ClassicMergeExecutor` implementing the regular merge path when `InsertOnlyMergeExecutor` is not used.
    - Introduce methods in `MergeOutputGeneration` to transform the merge clauses into expressions that can be applied to generate the output of the merge operation (both main data and CDC data).
    
    This change fully preserve the behavior of merge which is extensively tested in `MergeIntoSuiteBase`, `MergeIntoSQLSuite`, `MergeIntoScalaSuite`, `MergeCDCSuite`, `MergeIntoMetricsBase`, `MergeIntoNotMatchedBySourceSuite`.
    
    Closes delta-io/delta#1854
    
    GitOrigin-RevId: d8c8a0e9439c6710978f2ec345cb94b2b9b19e0e

---
+++ spark/src/main/scala/org/apache/spark/sql/delta/commands/merge/ClassicMergeExecutor.scala
@@ -0,0 +1,359 @@
+/*
+ * Copyright (2021) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.delta.commands.merge
+
+import scala.collection.JavaConverters._
+
+import org.apache.spark.sql.delta._
+import org.apache.spark.sql.delta.actions.{AddCDCFile, AddFile, FileAction}
+import org.apache.spark.sql.delta.commands.MergeIntoCommandBase
+import org.apache.spark.sql.delta.commands.cdc.CDCReader.{CDC_TYPE_COLUMN_NAME, CDC_TYPE_NOT_CDC}
+import org.apache.spark.sql.delta.commands.merge.MergeOutputGeneration.{SOURCE_ROW_INDEX_COL, TARGET_ROW_INDEX_COL}
+import org.apache.spark.sql.delta.util.SetAccumulator
+
+import org.apache.spark.sql.{Column, Dataset, SparkSession}
+import org.apache.spark.sql.catalyst.expressions.{Literal, Or}
+import org.apache.spark.sql.functions.{coalesce, col, count, input_file_name, lit, monotonically_increasing_id, sum}
+
+/**
+ * Trait with merge execution in two phases:
+ *
+ * Phase 1: Find the input files in target that are touched by the rows that satisfy
+ *    the condition and verify that no two source rows match with the same target row.
+ *    This is implemented as an inner-join using the given condition. See [[findTouchedFiles]]
+ *    for more details. In the special case that there is no update clause we write all the non-
+ *    matching source data as new files and skip phase 2.
+ *    Issues an error message when the ON search_condition of the MERGE statement can match
+ *    a single row from the target table with multiple rows of the source table-reference.
+ *
+ * Phase 2: Read the touched files again and write new files with updated and/or inserted rows.
+ *    If there are updates, then use a outer join using the given condition to write the
+ *    updates and inserts (see [[writeAllChanges()]]. If there are no matches for updates,
+ *    only inserts, then write them directy (see [[writeInsertsOnlyWhenNoMatches()]].
+ *
+ * See [[InsertOnlyMergeExecutor]] for the optimized executor used in case there are only inserts.
+ */
+trait ClassicMergeExecutor extends MergeIntoMaterializeSource with MergeOutputGeneration {
+  self: MergeIntoCommandBase =>
+  import MergeIntoCommandBase._
+
+  /**
+   * Find the target table files that contain the rows that satisfy the merge condition. This is
+   * implemented as an inner-join between the source query/table and the target table using
+   * the merge condition.
+   */
+  protected def findTouchedFiles(
+      spark: SparkSession,
+      deltaTxn: OptimisticTransaction
+    ): (Seq[AddFile], DeduplicateCDFDeletes) = recordMergeOperation(
+      extraOpType = "findTouchedFiles",
+      status = "MERGE operation - scanning files for matches",
+      sqlMetricName = "scanTimeMs") {
+
+    val columnComparator = spark.sessionState.analyzer.resolver
+
+    // Accumulator to collect all the distinct touched files
+    val touchedFilesAccum = new SetAccumulator[String]()
+    spark.sparkContext.register(touchedFilesAccum, TOUCHED_FILES_ACCUM_NAME)
+
+    // UDFs to records touched files names and add them to the accumulator
+    val recordTouchedFileName =
+      DeltaUDF.intFromStringBoolean { (fileName, shouldRecord) => {
+        if (shouldRecord) {
+          touchedFilesAccum.add(fileName)
+        }
+        1
+      }}.asNondeterministic()
+
+    // Prune non-matching files if we don't need to collect them for NOT MATCHED BY SOURCE clauses.
+    val dataSkippedFiles =
+      if (notMatchedBySourceClauses.isEmpty) {
+        deltaTxn.filterFiles(getTargetOnlyPredicates(spark))
+      } else {
+        deltaTxn.filterFiles()
+      }
+
+    // Join the source and target table using the merge condition to find touched files. An inner
+    // join collects all candidate files for MATCHED clauses, a right outer join also includes
+    // candidates for NOT MATCHED BY SOURCE clauses.
+    // In addition, we attach two columns
+    // - a monotonically increasing row id for target rows to later identify whether the same
+    //     target row is modified by multiple user or not
+    // - the target file name the row is from to later identify the files touched by matched rows
+    val joinType = if (notMatchedBySourceClauses.isEmpty) "inner" else "right_outer"
+
+    // When they are only MATCHED clauses, after the join we prune files that have no rows that
+    // satisfy any of the clause conditions.
+    val matchedPredicate =
+      if (isMatchedOnly) {
+        matchedClauses
+          .map(clause => clause.condition.getOrElse(Literal(true)))
+          .reduce((a, b) => Or(a, b))
+      } else Literal(true)
+
+    // Compute the columns needed for the inner join.
+    val targetColsNeeded = {
+      condition.references.map(_.name) ++ deltaTxn.snapshot.metadata.partitionColumns ++
+        matchedPredicate.references.map(_.name)
+    }
+
+    val columnsToDrop = deltaTxn.snapshot.metadata.schema.map(_.name)
+      .filterNot { field =>
+        targetColsNeeded.exists { name => columnComparator(name, field) }
+      }
+    val incrSourceRowCountExpr = incrementMetricAndReturnBool("numSourceRows", true)
+    // We can't use filter() directly on the expression because that will prevent
+    // column pruning. We don't need the SOURCE_ROW_PRESENT_COL so we immediately drop it.
+    val sourceDF = getSourceDF()
+      .withColumn(SOURCE_ROW_PRESENT_COL, new Column(incrSourceRowCountExpr))
+      .filter(SOURCE_ROW_PRESENT_COL)
+      .drop(SOURCE_ROW_PRESENT_COL)
+    val targetPlan =
+      buildTargetPlanWithFiles(
+        spark,
+        deltaTxn,
+        dataSkippedFiles,
+        columnsToDrop)
+    val targetDF = Dataset.ofRows(spark, targetPlan)
+      .withColumn(ROW_ID_COL, monotonically_increasing_id())
+      .withColumn(FILE_NAME_COL, input_file_name())
+
+    val joinToFindTouchedFiles =
+      sourceDF.join(targetDF, new Column(condition), joinType)
+
+    // Process the matches from the inner join to record touched files and find multiple matches
+    val collectTouchedFiles = joinToFindTouchedFiles
+      .select(col(ROW_ID_COL),
+        recordTouchedFileName(col(FILE_NAME_COL), new Column(matchedPredicate)).as("one"))
+
+    // Calculate frequency of matches per source row
+    val matchedRowCounts = collectTouchedFiles.groupBy(ROW_ID_COL).agg(sum("one").as("count"))
+
+    // Get multiple matches and simultaneously collect (using touchedFilesAccum) the file names
+    import org.apache.spark.sql.delta.implicits._
+    val (multipleMatchCount, multipleMatchSum) = matchedRowCounts
+      .filter("count > 1")
+      .select(coalesce(count(new Column("*")), lit(0)), coalesce(sum("count"), lit(0)))
+      .as[(Long, Long)]
+      .collect()
+      .head
+
+    val hasMultipleMatches = multipleMatchCount > 0
+    throwErrorOnMultipleMatches(hasMultipleMatches, spark)
+    if (hasMultipleMatches) {
+      // This is only allowed for delete-only queries.
+      // This query will count the duplicates for numTargetRowsDeleted in Job 2,
+      // because we count matches after the join and not just the target rows.
+      // We have to compensate for this by subtracting the duplicates later,
+      // so we need to record them here.
+      val duplicateCount = multipleMatchSum - multipleMatchCount
+      multipleMatchDeleteOnlyOvercount = Some(duplicateCount)
+    }
+
+    // Get the AddFiles using the touched file names.
+    val touchedFileNames = touchedFilesAccum.value.iterator().asScala.toSeq
+    logTrace(s"findTouchedFiles: matched files:\n\t${touchedFileNames.mkString("\n\t")}")
+
+    val nameToAddFileMap = generateCandidateFileMap(targetDeltaLog.dataPath, dataSkippedFiles)
+    val touchedAddFiles = touchedFileNames.map(f =>
+      getTouchedFile(targetDeltaLog.dataPath, f, nameToAddFileMap))
+
+    if (metrics("numSourceRows").value == 0 && (dataSkippedFiles.isEmpty ||
+      dataSkippedFiles.forall(_.numLogicalRecords.getOrElse(0) == 0))) {
+      // The target table is empty, and the optimizer optimized away the join entirely OR the
+      // source table is truly empty. In that case, scanning the source table once is the only
+      // way to get the correct metric.
+      val numSourceRows = sourceDF.count()
+      metrics("numSourceRows").set(numSourceRows)
+    }
+
+    metrics("numTargetFilesBeforeSkipping") += deltaTxn.snapshot.numOfFiles
+    metrics("numTargetBytesBeforeSkipping") += deltaTxn.snapshot.sizeInBytes
+    val (afterSkippingBytes, afterSkippingPartitions) =
+      totalBytesAndDistinctPartitionValues(dataSkippedFiles)
+    metrics("numTargetFilesAfterSkipping") += dataSkippedFiles.size
+    metrics("numTargetBytesAfterSkipping") += afterSkippingBytes
+    metrics("numTargetPartitionsAfterSkipping") += afterSkippingPartitions
+    val (removedBytes, removedPartitions) = totalBytesAndDistinctPartitionValues(touchedAddFiles)
+    metrics("numTargetFilesRemoved") += touchedAddFiles.size
+    metrics("numTargetBytesRemoved") += removedBytes
+    metrics("numTargetPartitionsRemovedFrom") += removedPartitions
+    val dedupe = DeduplicateCDFDeletes(
+      hasMultipleMatches && isCdcEnabled(deltaTxn),
+      includesInserts)
+    (touchedAddFiles, dedupe)
+  }
+
+  /**
+   * Write new files by reading the touched files and updating/inserting data using the source
+   * query/table. This is implemented using a full-outer-join using the merge condition.
+   *
+   * Note that unlike the insert-only code paths with just one control column ROW_DROPPED_COL, this
+   * method has a second control column CDC_TYPE_COL_NAME used for handling CDC when enabled.
+   */
+  protected def writeAllChanges(
+      spark: SparkSession,
+      deltaTxn: OptimisticTransaction,
+      filesToRewrite: Seq[AddFile],
+      deduplicateCDFDeletes: DeduplicateCDFDeletes)
+    : Seq[FileAction] = recordMergeOperation(
+      extraOpType =
+        if (shouldOptimizeMatchedOnlyMerge(spark)) "writeAllUpdatesAndDeletes"
+        else "writeAllChanges",
+      status = s"MERGE operation - Rewriting ${filesToRewrite.size} files",
+      sqlMetricName = "rewriteTimeMs") {
+
+    require(!deduplicateCDFDeletes.enabled || isCdcEnabled(deltaTxn),
+      "CDF delete duplication is enabled but overall the CDF generation is disabled")
+
+    // Generate a new target dataframe that has same output attributes exprIds as the target plan.
+    // This allows us to apply the existing resolved update/insert expressions.
+    val targetPlan = buildTargetPlanWithFiles(
+      spark,
+      deltaTxn,
+      filesToRewrite,
+      columnsToDrop = Nil)
+    val baseTargetDF = Dataset.ofRows(spark, targetPlan)
+    val joinType = if (shouldOptimizeMatchedOnlyMerge(spark)) {
+      "rightOuter"
+    } else {
+      "fullOuter"
+    }
+
+    logDebug(s"""writeAllChanges using $joinType join:
+       |  source.output: ${source.outputSet}
+       |  target.output: ${target.outputSet}
+       |  condition: $condition
+       |  newTarget.output: ${baseTargetDF.queryExecution.logical.outputSet}
+       """.stripMargin)
+
+    // Expressions to update metrics
+    val incrSourceRowCountExpr = incrementMetricAndReturnBool("numSourceRowsInSecondScan", true)
+    val incrNoopCountExpr = incrementMetricAndReturnBool("numTargetRowsCopied", false)
+
+    // Apply an outer join to find both, matches and non-matches. We are adding two boolean fields
+    // with value `true`, one to each side of the join. Whether this field is null or not after
+    // the outer join, will allow us to identify whether the resultant joined row was a
+    // matched inner result or an unmatched result with null on one side.
+    val joinedDF = {
+      val sourceDF = if (deduplicateCDFDeletes.enabled && deduplicateCDFDeletes.includesInserts) {
+        // Add row index for the source rows to identify inserted rows during the cdf deleted rows
+        // deduplication. See [[deduplicateCDFDeletes()]]
+        getSourceDF()
+          .withColumn(SOURCE_ROW_INDEX_COL, monotonically_increasing_id())
+      } else {
+        getSourceDF()
+      }
+      val targetDF = baseTargetDF
+        .withColumn(TARGET_ROW_PRESENT_COL, lit(true))
+      sourceDF
+        .withColumn(SOURCE_ROW_PRESENT_COL, new Column(incrSourceRowCountExpr))
+        .join(
+        if (deduplicateCDFDeletes.enabled) {
+          targetDF.withColumn(TARGET_ROW_INDEX_COL, monotonically_increasing_id())
+        } else targetDF,
+        new Column(condition), joinType)
+    }
+
+    // Precompute conditions in matched and not matched clauses and generate
+    // the joinedDF with precomputed columns and clauses with rewritten conditions.
+    val (joinedAndPrecomputedConditionsDF, clausesWithPrecompConditions) =
+        generatePrecomputedConditionsAndDF(
+          joinedDF,
+          clauses = matchedClauses ++ notMatchedClauses ++ notMatchedBySourceClauses)
+
+    val cdcEnabled = DeltaConfigs.CHANGE_DATA_FEED.fromMetaData(deltaTxn.metadata)
+
+
+    // The target output columns need to be marked as nullable here, as they are going to be used
+    // to reference the output of an outer join.
+    val targetOutputCols = getTargetOutputCols(deltaTxn, makeNullable = true)
+
+    // If there are N columns in the target table, the full outer join output will have:
+    // - N columns for target table
+    // - ROW_DROPPED_COL to define whether the generated row should dropped or written
+    // - if CDC is enabled, also CDC_TYPE_COLUMN_NAME containing the type of change being performed
+    //   in a particular row
+    // (N+1 or N+2 or N+3 columns depending on CDC disabled / enabled and if Row IDs are preserved)
+    val outputColNames =
+      targetOutputCols.map(_.name) ++
+        Seq(ROW_DROPPED_COL) ++
+        (if (cdcEnabled) Seq(CDC_TYPE_COLUMN_NAME) else Seq())
+
+    // Copy expressions to copy the existing target row and not drop it (ROW_DROPPED_COL=false),
+    // and in case CDC is enabled, set it to CDC_TYPE_NOT_CDC.
+    // (N+1 or N+2 or N+3 columns depending on CDC disabled / enabled and if Row IDs are preserved)
+    val noopCopyExprs =
+      targetOutputCols ++
+        Seq(incrNoopCountExpr) ++
+        (if (cdcEnabled) Seq(CDC_TYPE_NOT_CDC) else Seq())
+
+    // Generate output columns.
+    val outputCols = generateWriteAllChangesOutputCols(
+      targetOutputCols,
+      outputColNames,
+      noopCopyExprs,
+      clausesWithPrecompConditions,
+      cdcEnabled
+    )
+
+    val outputDF = if (cdcEnabled) {
+      generateCdcAndOutputRows(
+          joinedAndPrecomputedConditionsDF,
+          outputCols,
+          outputColNames,
+          noopCopyExprs,
+          deduplicateCDFDeletes)
+        .filter(s"$ROW_DROPPED_COL = false")
+        .drop(ROW_DROPPED_COL)
+    } else { // change data capture is off, just output the normal data
+      // Apply the expressions on the join output. The filter ensures we only consider rows
+      // that are not dropped. And the drop ensures that the dropped flag does not leak out
+      // to the output.
+      joinedAndPrecomputedConditionsDF
+        .select(outputCols: _*)
+        .filter(s"$ROW_DROPPED_COL = false")
+        .drop(ROW_DROPPED_COL)
+    }
+
+    logDebug("writeAllChanges: join output plan:\n" + outputDF.queryExecution)
+
+    // Write to Delta
+    val newFiles = writeFiles(spark, deltaTxn, outputDF)
+
+    // Update metrics
+    val (addedBytes, addedPartitions) = totalBytesAndDistinctPartitionValues(newFiles)
+    metrics("numTargetFilesAdded") += newFiles.count(_.isInstanceOf[AddFile])
+    metrics("numTargetChangeFilesAdded") += newFiles.count(_.isInstanceOf[AddCDCFile])
+    metrics("numTargetChangeFileBytes") += newFiles.collect{ case f: AddCDCFile => f.size }.sum
+    metrics("numTargetBytesAdded") += addedBytes
+    metrics("numTargetPartitionsAddedTo") += addedPartitions
+    if (multipleMatchDeleteOnlyOvercount.isDefined) {
+      // Compensate for counting duplicates during the query.
+      val actualRowsDeleted =
+        metrics("numTargetRowsDeleted").value - multipleMatchDeleteOnlyOvercount.get
+      assert(actualRowsDeleted >= 0)
+      metrics("numTargetRowsDeleted").set(actualRowsDeleted)
+      val actualRowsMatchedDeleted =
+        metrics("numTargetRowsMatchedDeleted").value - multipleMatchDeleteOnlyOvercount.get
+      assert(actualRowsMatchedDeleted >= 0)
+      metrics("numTargetRowsMatchedDeleted").set(actualRowsMatchedDeleted)
+    }
+
+    newFiles
+  }
+}
