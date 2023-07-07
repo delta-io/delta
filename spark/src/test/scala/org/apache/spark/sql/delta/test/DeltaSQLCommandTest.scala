975daae690bce6295e5f8814f2515562553ed2ad
Fredrik Klauss <fredrik.klauss@databricks.com>
2023-07-07 18:56:10 +0200

    Set spark.sql.extensions to enable Delta in tests instead of overriding SparkSession
    * Set spark.sql.extensions instead of overriding extensions of SparkSession object.
    * The current implementation overrode  the extensions of SparkSession, as it did not pick up spark.sql.extensions. This was fixed and now it picks up extensions set in spark.sql.extensions
    
    GitOrigin-RevId: cbd1654496225baffca02ec3072cbdc2297c1e3c

--- spark/src/test/scala/org/apache/spark/sql/delta/test/DeltaSQLCommandTest.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/test/DeltaSQLCommandTest.scala
@@ -20,34 +20,20 @@ import org.apache.spark.sql.delta.catalog.DeltaCatalog
 import io.delta.sql.DeltaSparkSessionExtension
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.{SparkSession, SparkSessionExtensions}
-import org.apache.spark.sql.test.{SharedSparkSession, TestSparkSession}
-
-/**
- * Because `TestSparkSession` doesn't pick up the conf `spark.sql.extensions` in Spark 2.4.x, we use
- * this class to inject Delta's extension in our tests.
- *
- * @see https://issues.apache.org/jira/browse/SPARK-25003
- */
-class DeltaTestSparkSession(sparkConf: SparkConf) extends TestSparkSession(sparkConf) {
-  override val extensions: SparkSessionExtensions = {
-    val extensions = new SparkSessionExtensions
-    new DeltaSparkSessionExtension().apply(extensions)
-    extensions
-  }
-}
+import org.apache.spark.sql.internal.{SQLConf, StaticSQLConf}
+import org.apache.spark.sql.test.SharedSparkSession
 
 /**
  * A trait for tests that are testing a fully set up SparkSession with all of Delta's requirements,
  * such as the configuration of the DeltaCatalog and the addition of all Delta extensions.
  */
-trait DeltaSQLCommandTest { self: SharedSparkSession =>
+trait DeltaSQLCommandTest extends SharedSparkSession {
 
-  override protected def createSparkSession: TestSparkSession = {
-    SparkSession.cleanupAnyExistingSession()
-    val session = new DeltaTestSparkSession(sparkConf)
-    session.conf.set(SQLConf.V2_SESSION_CATALOG_IMPLEMENTATION.key, classOf[DeltaCatalog].getName)
-    session
+  override protected def sparkConf: SparkConf = {
+    super.sparkConf
+      .set(StaticSQLConf.SPARK_SESSION_EXTENSIONS.key,
+        classOf[DeltaSparkSessionExtension].getName)
+      .set(SQLConf.V2_SESSION_CATALOG_IMPLEMENTATION.key,
+        classOf[DeltaCatalog].getName)
   }
 }
