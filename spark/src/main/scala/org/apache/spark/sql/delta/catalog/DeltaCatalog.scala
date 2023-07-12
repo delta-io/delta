d0e1223f135bb0392c72685839ddb672f5a27c55
Ming DAI <ming.dai@databricks.com>
2023-07-12 16:40:12 -0700

    Minor refactor to create a helper function for IcebergTablePlaceHolder
    GitOrigin-RevId: 47a900e16bead9e2374dc08499ea6f82400c6d91

--- spark/src/main/scala/org/apache/spark/sql/delta/catalog/DeltaCatalog.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/catalog/DeltaCatalog.scala
@@ -196,7 +196,7 @@ class DeltaCatalog extends DelegatingCatalogExtension
           if (isPathIdentifier(ident)) {
             newDeltaPathTable(ident)
           } else if (isIcebergPathIdentifier(ident)) {
-            IcebergTablePlaceHolder(TableIdentifier(ident.name(), Some("iceberg")))
+            newIcebergPathTable(ident)
           } else {
             throw e
           }
@@ -724,6 +724,10 @@ trait SupportsPathIdentifier extends TableCatalog { self: DeltaCatalog =>
 
   protected def isIcebergPathIdentifier(ident: Identifier): Boolean = {
     hasIcebergNamespace(ident) && new Path(ident.name()).isAbsolute
+  }
+
+  protected def newIcebergPathTable(ident: Identifier): IcebergTablePlaceHolder = {
+    IcebergTablePlaceHolder(TableIdentifier(ident.name(), Some("iceberg")))
   }
 
   protected def isPathIdentifier(ident: Identifier): Boolean = {
