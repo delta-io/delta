0b0382aa4cb3b0a854958906ba37d09c99a7df84
Jacek Laskowski <jacek@japila.pl>
2023-07-30 13:46:20 +0200

    A true fix (fingers crossed)

--- spark/src/main/scala/org/apache/spark/sql/delta/commands/merge/ClassicMergeExecutor.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/commands/merge/ClassicMergeExecutor.scala
@@ -100,7 +100,8 @@ trait ClassicMergeExecutor extends MergeIntoMaterializeSource with MergeOutputGe
       if (isMatchedOnly) {
         matchedClauses
           .flatMap(_.condition)
-          .foldLeft[Expression](Literal.TrueLiteral)(Or.apply)
+          .reduceOption((a, b) => Or(a, b))
+          .getOrElse(Literal.TrueLiteral)
       } else Literal.TrueLiteral
 
     // Compute the columns needed for the inner join.
