84eabee84e9ddf4fae77b70ae12506991c61f7c6
Dhruv Arya <dhruv.arya@databricks.com>
2023-09-18 16:12:27 -0700

    Minor refactor in DeltaConfig and TableFeature
    Minor refactor in DeltaConfig and TableFeature. No logical changes. Only affects code related to V2 Checkpoints.
    
    GitOrigin-RevId: 44dc4df44543a34af531354b962d29bedaec55e1

--- spark/src/main/scala/org/apache/spark/sql/delta/DeltaConfig.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/DeltaConfig.scala
@@ -637,9 +637,11 @@ trait DeltaConfigsBase extends DeltaLogging {
     "must be Serializable"
   )
 
+  val CHECKPOINT_POLICY_CONFIG_KEY = "checkpointPolicy-dev"
+
   /** Policy to decide what kind of checkpoint to write to a table. */
   val CHECKPOINT_POLICY = buildConfig[CheckpointPolicy.Policy](
-    key = "checkpointPolicy-dev",
+    key = CHECKPOINT_POLICY_CONFIG_KEY,
     defaultValue = CheckpointPolicy.Classic.name,
     fromString = str => CheckpointPolicy.fromName(str),
     validationFunction = (v => CheckpointPolicy.ALL.exists(_.name == v.name)),
