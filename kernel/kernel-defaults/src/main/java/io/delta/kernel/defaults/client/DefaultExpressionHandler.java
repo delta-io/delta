f648008e97baed83179bfe77c614c4b59e8cdaf6
Venki Korukanti <venki.korukanti@databricks.com>
2023-09-19 16:14:03 -0700

    [Kernel] Add `PredicateEvaluator` interface and default implementation

--- kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/client/DefaultExpressionHandler.java
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/client/DefaultExpressionHandler.java
@@ -24,11 +24,14 @@ import io.delta.kernel.client.ExpressionHandler;
 import io.delta.kernel.data.ColumnVector;
 import io.delta.kernel.expressions.Expression;
 import io.delta.kernel.expressions.ExpressionEvaluator;
+import io.delta.kernel.expressions.Predicate;
+import io.delta.kernel.expressions.PredicateEvaluator;
 import io.delta.kernel.types.DataType;
 import io.delta.kernel.types.StructType;
 
 import io.delta.kernel.defaults.internal.data.vector.DefaultBooleanVector;
 import io.delta.kernel.defaults.internal.expressions.DefaultExpressionEvaluator;
+import io.delta.kernel.defaults.internal.expressions.DefaultPredicateEvaluator;
 import static io.delta.kernel.defaults.internal.DefaultKernelUtils.checkArgument;
 
 /**
@@ -41,6 +44,11 @@ public class DefaultExpressionHandler implements ExpressionHandler {
         Expression expression,
         DataType outputType) {
         return new DefaultExpressionEvaluator(inputSchema, expression, outputType);
+    }
+
+    @Override
+    public PredicateEvaluator getPredicateEvaluator(StructType inputSchema, Predicate predicate) {
+        return new DefaultPredicateEvaluator(inputSchema, predicate);
     }
 
     @Override
