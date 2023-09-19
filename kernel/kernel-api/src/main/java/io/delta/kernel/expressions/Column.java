e033b092638fc3c6101c661fd81d4f269607283c
Venki Korukanti <venki.korukanti@databricks.com>
2023-09-19 16:56:35 -0700

    stylefix

--- kernel/kernel-api/src/main/java/io/delta/kernel/expressions/Column.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/expressions/Column.java
@@ -19,7 +19,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import static java.lang.String.format;
 
 import io.delta.kernel.annotation.Evolving;
