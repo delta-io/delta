aa1285441b3899d6ffc746a35da909c1953e232f
Ryan Johnson <ryan.johnson@databricks.com>
2023-09-28 10:13:52 -0700

    [Spark] Propagate catalog table through DeltaSink
    #### Which Delta project/connector is this regarding?
    
    -Spark
    - [ ] Standalone
    - [ ] Flink
    - [ ] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    
    In order to implement https://github.com/delta-io/delta/issues/2052 for streaming writes, `DeltaSink` needs to track the catalog table, if any, so it can properly initialize the transactions it executes. We can't change the Spark DataSource API that creates the sink, so instead we add logic in `DeltaAnalysis` that extracts the catalog table from the `WriteToStream` and applies it to the underlying `DeltaSink`.
    
    New unit test.
    
    ## Does this PR introduce _any_ user-facing changes?
    
    No.
    
    Closes https://github.com/delta-io/delta/pull/2109
    
    GitOrigin-RevId: b1abc21ecdbe863611fee2dc82a519d2f214bf43

--- spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSink.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSink.scala
@@ -27,6 +27,7 @@ import org.apache.hadoop.fs.Path
 
 // scalastyle:off import.ordering.noEmptyLine
 import org.apache.spark.sql._
+import org.apache.spark.sql.catalyst.catalog.CatalogTable
 import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.execution.SQLExecution
 import org.apache.spark.sql.execution.metric.{SQLMetric, SQLMetrics}
@@ -39,12 +40,13 @@ import org.apache.spark.util.Utils
 /**
  * A streaming sink that writes data into a Delta Table.
  */
-class DeltaSink(
+case class DeltaSink(
     sqlContext: SQLContext,
     path: Path,
     partitionColumns: Seq[String],
     outputMode: OutputMode,
-    options: DeltaOptions)
+    options: DeltaOptions,
+    catalogTable: Option[CatalogTable] = None)
   extends Sink
     with ImplicitMetadataOperation
     with DeltaLogging {
