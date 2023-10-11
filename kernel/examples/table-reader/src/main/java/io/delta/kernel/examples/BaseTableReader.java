d45cbe23c3a8522739f02f7cde8fe85c6c618149
Venki Korukanti <venki.korukanti@gmail.com>
2023-10-10 22:23:27 -0700

    [Kernel] Update `kernel/examples` to work with latest API changes
    ## Description
    There have been changes to Kernel APIs since the last update to the `kernel/examples`. Fix the examples to work with the latest API changes.
    
    ## How was this patch tested?
    Manually ran the `<repo-root>/kernel/examples/run-kernel-examples.py` and verified the build and display results are valid.

--- kernel/examples/table-reader/src/main/java/io/delta/kernel/examples/BaseTableReader.java
+++ kernel/examples/table-reader/src/main/java/io/delta/kernel/examples/BaseTableReader.java
@@ -16,6 +16,9 @@
 package io.delta.kernel.examples;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -35,14 +38,12 @@ import org.apache.hadoop.conf.Configuration;
 
 import io.delta.kernel.TableNotFoundException;
 import io.delta.kernel.client.TableClient;
-import io.delta.kernel.data.ColumnVector;
-import io.delta.kernel.data.ColumnarBatch;
-import io.delta.kernel.data.DataReadResult;
-import io.delta.kernel.types.StructField;
-import io.delta.kernel.types.StructType;
+import io.delta.kernel.data.FilteredColumnarBatch;
+import io.delta.kernel.data.Row;
+import io.delta.kernel.types.*;
+import io.delta.kernel.utils.CloseableIterator;
 
 import io.delta.kernel.defaults.client.DefaultTableClient;
-import io.delta.kernel.defaults.internal.data.vector.VectorUtils;
 
 /**
  * Base class for reading Delta Lake tables using the Delta Kernel APIs.
@@ -88,18 +89,18 @@ public abstract class BaseTableReader {
         return new StructType(selectedFields);
     }
 
-    protected static int printData(DataReadResult dataReadResult, int maxRowsToPrint) {
+    protected static int printData(FilteredColumnarBatch data, int maxRowsToPrint) {
         int printedRowCount = 0;
-        ColumnarBatch data = dataReadResult.getData();
-        Optional<ColumnVector> selectionVector = dataReadResult.getSelectionVector();
-        for (int rowId = 0; rowId < data.getSize(); rowId++) {
-            if (!selectionVector.isPresent() || selectionVector.get().getBoolean(rowId)) {
-                printRow(data, rowId);
+        try (CloseableIterator<Row> rows = data.getRows()) {
+            while (rows.hasNext()) {
+                printRow(rows.next());
                 printedRowCount++;
                 if (printedRowCount == maxRowsToPrint) {
                     break;
                 }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return printedRowCount;
     }
@@ -108,12 +109,11 @@ public abstract class BaseTableReader {
         System.out.printf(formatter(schema.length()), schema.fieldNames().toArray(new String[0]));
     }
 
-    protected static void printRow(ColumnarBatch batch, int rowId) {
-        int numCols = batch.getSchema().length();
-        Object[] rowValues = IntStream.range(0, numCols).mapToObj(colOrdinal -> {
-            ColumnVector columnVector = batch.getColumnVector(colOrdinal);
-            return VectorUtils.getValueAsObject(columnVector, rowId);
-        }).toArray();
+    protected static void printRow(Row row){
+        int numCols = row.getSchema().length();
+        Object[] rowValues = IntStream.range(0, numCols)
+            .mapToObj(colOrdinal -> getValue(row, colOrdinal))
+            .toArray();
 
         // TODO: Need to handle the Row, Map, Array, Timestamp, Date types specially to
         // print them in the format they need. Copy this code from Spark CLI.
@@ -177,6 +177,53 @@ public abstract class BaseTableReader {
         return IntStream.range(0, length)
             .mapToObj(i -> "%20s")
             .collect(Collectors.joining("|")) + "\n";
+    }
+
+    private static String getValue(Row row, int columnOrdinal) {
+        DataType dataType = row.getSchema().at(columnOrdinal).getDataType();
+        if (row.isNullAt(columnOrdinal)) {
+            return null;
+        } else if (dataType instanceof BooleanType) {
+            return Boolean.toString(row.getBoolean(columnOrdinal));
+        } else if (dataType instanceof ByteType) {
+            return Byte.toString(row.getByte(columnOrdinal));
+        } else if (dataType instanceof ShortType) {
+            return Short.toString(row.getShort(columnOrdinal));
+        } else if (dataType instanceof IntegerType) {
+            return Integer.toString(row.getInt(columnOrdinal));
+        } else if (dataType instanceof DateType) {
+            // DateType data is stored internally as the number of days since 1970-01-01
+            int daysSinceEpochUTC = row.getInt(columnOrdinal);
+            return LocalDate.ofEpochDay(daysSinceEpochUTC).toString();
+        } else if (dataType instanceof LongType) {
+            return Long.toString(row.getLong(columnOrdinal));
+        } else if (dataType instanceof TimestampType) {
+            // TimestampType data is stored internally as the number of microseconds since epoch
+            long microSecsSinceEpochUTC = row.getLong(columnOrdinal);
+            LocalDateTime dateTime = LocalDateTime.ofEpochSecond(
+                microSecsSinceEpochUTC / 1_000_000 /* epochSecond */,
+                (int) (1000 * microSecsSinceEpochUTC % 1_000_000) /* nanoOfSecond */,
+                ZoneOffset.UTC);
+            return dateTime.toString();
+        } else if (dataType instanceof FloatType) {
+            return Float.toString(row.getFloat(columnOrdinal));
+        } else if (dataType instanceof DoubleType) {
+            return Double.toString(row.getDouble(columnOrdinal));
+        } else if (dataType instanceof StringType) {
+            return row.getString(columnOrdinal);
+        } else if (dataType instanceof BinaryType) {
+            return new String(row.getBinary(columnOrdinal));
+        } else if (dataType instanceof DecimalType) {
+            return row.getDecimal(columnOrdinal).toString();
+        } else if (dataType instanceof StructType) {
+            return "TODO: struct value";
+        } else if (dataType instanceof ArrayType) {
+            return "TODO: list value";
+        } else if (dataType instanceof MapType) {
+            return "TODO: map value";
+        } else {
+            throw new UnsupportedOperationException("unsupported data type: " + dataType);
+        }
     }
 }
 
