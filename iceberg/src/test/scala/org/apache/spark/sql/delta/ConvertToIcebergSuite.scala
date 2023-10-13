ec728e81ff0923b8b0c0c55318dde60ed0d3d9ec
Hao Jiang <hao.jiang@databricks.com>
2023-10-12 17:34:36 -0700

    Add UniForm examples

--- iceberg/src/test/scala/org/apache/spark/sql/delta/ConvertToIcebergSuite.scala
+++ iceberg/src/test/scala/org/apache/spark/sql/delta/ConvertToIcebergSuite.scala
@@ -116,6 +116,7 @@ class ConvertToIcebergSuite extends QueryTest with Eventually {
   def verifyReadWithIceberg(tableName: String, expectedAnswer: Seq[Row]): Unit = {
     withIcebergSparkSession { icebergSparkSession =>
       eventually(timeout(10.seconds)) {
+        icebergSparkSession.sql(s"REFRESH TABLE ${tableName}")
         val icebergDf = icebergSparkSession.read.format("iceberg").load(tableName)
         checkAnswer(icebergDf, expectedAnswer)
       }
