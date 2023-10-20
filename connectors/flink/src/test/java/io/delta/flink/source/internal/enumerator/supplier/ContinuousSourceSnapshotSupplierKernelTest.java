7890501c2ac79ed384d06aca49637467bc068f6d
Nick Lanham <nick@afternight.org>
2023-10-20 16:37:14 -0700

    TEST ALL THE TYPES

--- connectors/flink/src/test/java/io/delta/flink/source/internal/enumerator/supplier/ContinuousSourceSnapshotSupplierKernelTest.java
+++ connectors/flink/src/test/java/io/delta/flink/source/internal/enumerator/supplier/ContinuousSourceSnapshotSupplierKernelTest.java
@@ -30,10 +30,7 @@ import io.delta.kernel.data.ColumnVector;
 import io.delta.kernel.data.MapValue;
 import io.delta.kernel.defaults.internal.data.vector.DefaultGenericVector;
 import io.delta.kernel.internal.SnapshotImpl;
-import io.delta.kernel.types.DataType;
-import io.delta.kernel.types.IntegerType;
-import io.delta.kernel.types.StructField;
-import io.delta.kernel.types.StructType;
+import io.delta.kernel.types.*;
 
 import io.delta.standalone.DeltaLog;
 import io.delta.standalone.Snapshot;
@@ -60,15 +57,35 @@ class ContinuousSourceSnapshotSupplierKernelTest {
 	HashMap<String, String> metadata = new HashMap<>();
         metadata.put("key1", "value1");
         metadata.put("key2", "value2");
+	ArrayList<DataType> typesToTest = new ArrayList<DataType>();
+	ArrayType arrayType = new ArrayType(IntegerType.INTEGER, false);
+	typesToTest.add(arrayType);
+	typesToTest.add(BinaryType.BINARY);
+	typesToTest.add(BooleanType.BOOLEAN);
+	typesToTest.add(ByteType.BYTE);
+	typesToTest.add(DateType.DATE);
+	typesToTest.add(DecimalType.USER_DEFAULT);
+	typesToTest.add(DoubleType.DOUBLE);
+	typesToTest.add(FloatType.FLOAT);
+	typesToTest.add(IntegerType.INTEGER);
+	typesToTest.add(LongType.LONG);
+	typesToTest.add(new MapType(ShortType.SHORT, arrayType, false));
+	typesToTest.add(ShortType.SHORT);
+	typesToTest.add(StringType.STRING);
+	typesToTest.add(TimestampType.TIMESTAMP);
 	ArrayList<StructField> fields = new ArrayList<StructField>();
-	fields.add(
-	    new StructField(
-		"Field 1",
-		IntegerType.INTEGER,
-		false,
-		metadata
-	    )
-	);
+	int fnum = 1;
+	for (DataType dt : typesToTest) {
+	    fields.add(
+		new StructField(
+		    "Field " + fnum,
+		    dt,
+		    false,
+		    metadata
+		    )
+		);
+	    fnum++;
+	}
 	StructType schema = new StructType(fields);
         return new io.delta.kernel.internal.actions.Metadata(
             "id",
@@ -192,12 +209,41 @@ class ContinuousSourceSnapshotSupplierKernelTest {
 	    putString("key1", "value1").
 	    putString("key2", "value2").
 	    build();
-	io.delta.standalone.types.StructType expectedSchema = new io.delta.standalone.types.StructType()
-	    .add(new io.delta.standalone.types.StructField(
-		     "Field 1",
-		     new io.delta.standalone.types.IntegerType(),
-		     false,
-		     fmeta));
+	ArrayList<io.delta.standalone.types.DataType> expectedTypes =
+	    new ArrayList<io.delta.standalone.types.DataType>();
+	io.delta.standalone.types.ArrayType arrayType = new io.delta.standalone.types.ArrayType(
+	    new io.delta.standalone.types.IntegerType(), false
+	);
+	expectedTypes.add(arrayType);
+	expectedTypes.add(new io.delta.standalone.types.BinaryType());
+	expectedTypes.add(new io.delta.standalone.types.BooleanType());
+	expectedTypes.add(new io.delta.standalone.types.ByteType());
+	expectedTypes.add(new io.delta.standalone.types.DateType());
+	expectedTypes.add(io.delta.standalone.types.DecimalType.USER_DEFAULT);
+	expectedTypes.add(new io.delta.standalone.types.DoubleType());
+	expectedTypes.add(new io.delta.standalone.types.FloatType());
+	expectedTypes.add(new io.delta.standalone.types.IntegerType());
+	expectedTypes.add(new io.delta.standalone.types.LongType());
+	expectedTypes.add(
+	    new io.delta.standalone.types.MapType(
+		new io.delta.standalone.types.ShortType(),
+		arrayType,
+		false
+	    )
+	);
+	expectedTypes.add(new io.delta.standalone.types.ShortType());
+	expectedTypes.add(new io.delta.standalone.types.StringType());
+	expectedTypes.add(new io.delta.standalone.types.TimestampType());
+	io.delta.standalone.types.StructType expectedSchema = new io.delta.standalone.types.StructType();
+	int fnum = 1;
+	for (io.delta.standalone.types.DataType dt : expectedTypes) {
+	    expectedSchema = expectedSchema.add(new io.delta.standalone.types.StructField(
+						    "Field " + fnum,
+						    dt,
+						    false,
+						    fmeta));
+	    fnum++;
+	}
 	assertThat(metadata.getSchema(), equalTo(expectedSchema));
     }
 }
