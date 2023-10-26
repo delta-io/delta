b006e25dec24cfa00685a01bcd8704d935cefac1
Nick Lanham <nick@afternight.org>
2023-10-25 17:04:22 -0700

    Actually put it in DeltaGlobalCommitter

--- connectors/flink/src/main/scala/io/delta/flink/internal/KernelDeltaLogImpl.scala
+++ connectors/flink/src/main/scala/io/delta/flink/internal/KernelDeltaLogImpl.scala
@@ -5,11 +5,11 @@ import org.apache.hadoop.fs.Path
 
 import io.delta.kernel.Table
 import io.delta.kernel.defaults.client.DefaultTableClient
-import io.delta.kernel.internal.{SnapshotImpl => SnapshotImplKernel}
+import io.delta.kernel.internal.{SnapshotImpl => SnapshotImplKernel, TableImpl}
 import io.delta.standalone.VersionLog
 import io.delta.standalone.actions.{CommitInfo => CommitInfoJ}
 import io.delta.standalone.internal.{SnapshotImpl => StandaloneSnapshotImpl}
-import io.delta.standalone.internal.util.Clock
+import io.delta.standalone.internal.util.{Clock, SystemClock}
 
 /**
  * We want to be able to construct an OptimisticTransactionImpl that uses a delta log and a snapshot
@@ -18,14 +18,14 @@ import io.delta.standalone.internal.util.Clock
  * provides features used by flink+startTransaction.
  */
 class KernelDeltaLogImpl(
+    tableClient: DefaultTableClient,
+    table: TableImpl,
     hadoopConf: Configuration,
     logPath: Path,
     dataPath: Path,
     clock: Clock)
   extends DeltaLogImpl(hadoopConf, logPath, dataPath, clock) {
 
-  val tableClient = DefaultTableClient.create(hadoopConf)
-  val table = Table.forPath(tableClient, dataPath.toString)
   var currKernelSnapshot: Option[KernelSnapshotImpl] = None
 
   override def snapshot(): StandaloneSnapshotImpl = { // but is actually a KernelSnapshotImpl
@@ -54,6 +54,8 @@ class KernelDeltaLogImpl(
     new OptimisticTransactionImpl(this, snapshot)
   }
 
+  override def tableExists: Boolean = snapshot.version >= 0
+
   override def getChanges(startVersion: Long, failOnDataLoss: Boolean): java.util.Iterator[VersionLog] = {
     logWarning("KernelDeltaLogImpl falling back to DeltaLogImpl for getChanges")
     return super.getChanges(startVersion, failOnDataLoss);
@@ -68,7 +70,16 @@ class KernelDeltaLogImpl(
   override def getCommitInfoAt(version: Long): CommitInfoJ = {
     throw new RuntimeException()
   }
+}
 
-
-
+object KernelDeltaLogImpl {
+  def forTable(hadoopConf: Configuration, dataPath: String): KernelDeltaLogImpl = {
+    val tableClient = DefaultTableClient.create(hadoopConf)
+    val table = Table.forPath(tableClient, dataPath).asInstanceOf[TableImpl]
+    // Todo: Potentially we could get the resolved paths out of the table above
+    val rawPath = new Path(dataPath, "_delta_log")
+    val fs = rawPath.getFileSystem(hadoopConf)
+    val logPath = fs.makeQualified(rawPath)
+    new KernelDeltaLogImpl(tableClient, table, hadoopConf, logPath, logPath.getParent, new SystemClock)
+  }
 }
