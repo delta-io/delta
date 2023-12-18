a6a7c3c6a0dad326e39550591ba453173a600e06
Allison Portis <allison.portis@databricks.com>
2023-12-18 11:14:22 -1000

    [Kernel][Expressions] Update `AND` and `OR` expressions to follow SQL null semantics (#2361)

--- kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/expressions/DefaultExpressionEvaluator.java
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/expressions/DefaultExpressionEvaluator.java
@@ -258,15 +258,43 @@ public class DefaultExpressionEvaluator implements ExpressionEvaluator {
             this.input = input;
         }
 
+        /*
+        | Operand 1 | Operand 2 | `AND`      | `OR`       |
+        |-----------|-----------|------------|------------|
+        | True      | True      | True       | True       |
+        | True      | False     | False      | True       |
+        | True      | NULL      | NULL       | True       |
+        | False     | True      | False      | True       |
+        | False     | False     | False      | False      |
+        | False     | NULL      | False      | NULL       |
+        | NULL      | True      | NULL       | True       |
+        | NULL      | False     | False      | NULL       |
+        | NULL      | NULL      | NULL       | NULL       |
+         */
         @Override
         ColumnVector visitAnd(And and) {
             PredicateChildrenEvalResult argResults = evalBinaryExpressionChildren(and);
+            ColumnVector left = argResults.leftResult;
+            ColumnVector right = argResults.rightResult;
             int numRows = argResults.rowCount;
             boolean[] result = new boolean[numRows];
-            boolean[] nullability = evalNullability(argResults.leftResult, argResults.rightResult);
+            boolean[] nullability = new boolean[numRows];
             for (int rowId = 0; rowId < numRows; rowId++) {
-                result[rowId] = argResults.leftResult.getBoolean(rowId) &&
-                    argResults.rightResult.getBoolean(rowId);
+                boolean leftIsTrue = !left.isNullAt(rowId) && left.getBoolean(rowId);
+                boolean rightIsTrue = !right.isNullAt(rowId) && right.getBoolean(rowId);
+                boolean leftIsFalse = !left.isNullAt(rowId) && !left.getBoolean(rowId);
+                boolean rightIsFalse = !right.isNullAt(rowId) && !right.getBoolean(rowId);
+
+                if (leftIsFalse || rightIsFalse) {
+                    nullability[rowId] = false;
+                    result[rowId] = false;
+                } else if (leftIsTrue && rightIsTrue) {
+                    nullability[rowId] = false;
+                    result[rowId] = true;
+                } else {
+                    nullability[rowId] = true;
+                    // result[rowId] is undefined when nullability[rowId] = true
+                }
             }
             return new DefaultBooleanVector(numRows, Optional.of(nullability), result);
         }
@@ -274,12 +302,27 @@ public class DefaultExpressionEvaluator implements ExpressionEvaluator {
         @Override
         ColumnVector visitOr(Or or) {
             PredicateChildrenEvalResult argResults = evalBinaryExpressionChildren(or);
+            ColumnVector left = argResults.leftResult;
+            ColumnVector right = argResults.rightResult;
             int numRows = argResults.rowCount;
             boolean[] result = new boolean[numRows];
-            boolean[] nullability = evalNullability(argResults.leftResult, argResults.rightResult);
+            boolean[] nullability = new boolean[numRows];
             for (int rowId = 0; rowId < numRows; rowId++) {
-                result[rowId] = argResults.leftResult.getBoolean(rowId) ||
-                    argResults.rightResult.getBoolean(rowId);
+                boolean leftIsTrue = !left.isNullAt(rowId) && left.getBoolean(rowId);
+                boolean rightIsTrue = !right.isNullAt(rowId) && right.getBoolean(rowId);
+                boolean leftIsFalse = !left.isNullAt(rowId) && !left.getBoolean(rowId);
+                boolean rightIsFalse = !right.isNullAt(rowId) && !right.getBoolean(rowId);
+
+                if (leftIsTrue || rightIsTrue) {
+                    nullability[rowId] = false;
+                    result[rowId] = true;
+                } else if (leftIsFalse && rightIsFalse) {
+                    nullability[rowId] = false;
+                    result[rowId] = false;
+                } else {
+                    nullability[rowId] = true;
+                    // result[rowId] is undefined when nullability[rowId] = true
+                }
             }
             return new DefaultBooleanVector(numRows, Optional.of(nullability), result);
         }
