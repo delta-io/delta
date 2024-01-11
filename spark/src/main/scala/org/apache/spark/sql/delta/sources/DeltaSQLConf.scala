e1fa1011e83f499a807401a768458fe7537f6495
Paddy Xu <xupaddy@gmail.com>
2024-01-11 12:21:38 +0100

    [Spark][UPDATE with DV] Let UPDATE command write DVs by default
    #### Which Delta project/connector is this regarding?
    
    -Spark
    - [ ] Standalone
    - [ ] Flink
    - [ ] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    
    This PR flips the flag for enabling write DVs for UPDATE commands.
    Actually, this PR should have been created much earlier because all required works (code, test, benchmarking) are done.
    
    Existing tests.
    
    ## Does this PR introduce _any_ user-facing changes?
    
    Yes, after this PR all UPDATE commands will write DVs when possible.
    
    Closes delta-io/delta#2456
    
    Signed-off-by: Paddy Xu <xupaddy@gmail.com>
    GitOrigin-RevId: f8e4cc7ea09a1bf14bfabdc71ca61764133e38b5

--- spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
@@ -1421,7 +1421,7 @@ trait DeltaSQLConfBase {
       .internal()
       .doc("Enable persistent Deletion Vectors in the Update command.")
       .booleanConf
-      .createWithDefault(false)
+      .createWithDefault(true)
 
   val DELETION_VECTOR_PACKING_TARGET_SIZE =
     buildConf("deletionVectors.packing.targetSize")
