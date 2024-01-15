7dc14ee71e1009e7378d466e28c6a1a15177dcbb
fred-db <fredrik.klauss@databricks.com>
2024-01-15 18:35:13 +0100

    more tests

--- spark/src/main/scala/org/apache/spark/sql/delta/ConflictChecker.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/ConflictChecker.scala
@@ -24,10 +24,11 @@ import scala.collection.mutable
 import org.apache.spark.sql.delta.RowId.RowTrackingMetadataDomain
 import org.apache.spark.sql.delta.actions._
 import org.apache.spark.sql.delta.metering.DeltaLogging
+import org.apache.spark.sql.delta.sources.DeltaSQLConf
 import org.apache.spark.sql.delta.util.FileNames
 import org.apache.hadoop.fs.FileStatus
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.catalyst.expressions.{Expression, ExpressionSet, Or}
 import org.apache.spark.sql.types.StructType
 
@@ -120,7 +121,7 @@ private[delta] class ConflictChecker(
     spark: SparkSession,
     initialCurrentTransactionInfo: CurrentTransactionInfo,
     winningCommitFileStatus: FileStatus,
-    isolationLevel: IsolationLevel) extends DeltaLogging {
+    isolationLevel: IsolationLevel) extends DeltaLogging with ConflictCheckerPredicateElimination {
 
   protected val winningCommitVersion = FileNames.deltaVersion(winningCommitFileStatus)
   protected val startTimeMs = System.currentTimeMillis()
@@ -240,21 +241,68 @@ private[delta] class ConflictChecker(
     import org.apache.spark.sql.delta.implicits._
     val filesDf = files.toDF(spark)
 
+    spark.conf.get(DeltaSQLConf.DELTA_CONFLICT_DETECTION_WIDEN_NONDETERMINISTIC_PREDICATES) match {
+      case DeltaSQLConf.NonDeterministicPredicateWidening.OFF =>
+        getFirstFileMatchingPartitionPredicatesInternal(
+          filesDf, shouldWidenNonDeterministicPredicates = false)
+      case wideningMode =>
+        val fileWithWidening = getFirstFileMatchingPartitionPredicatesInternal(
+          filesDf, shouldWidenNonDeterministicPredicates = true)
+
+        fileWithWidening.flatMap { fileWithWidening =>
+          val fileWithoutWidening =
+            getFirstFileMatchingPartitionPredicatesInternal(
+              filesDf, shouldWidenNonDeterministicPredicates = false)
+          if (fileWithoutWidening.isEmpty) {
+            // Conflict due to widening of non-deterministic predicate.
+            recordDeltaEvent(deltaLog,
+              opType = "delta.conflictDetection.partitionLevelConcurrency." +
+                "additionalConflictDueToWideningOfNonDeterministicPredicate",
+              data = Map(
+                "wideningMode" -> wideningMode,
+                "predicate" ->
+                  currentTransactionInfo.readPredicates.map(_.partitionPredicate.toString)))
+          }
+          if (wideningMode == DeltaSQLConf.NonDeterministicPredicateWidening.ON) {
+            Some(fileWithWidening)
+          } else {
+            fileWithoutWidening
+          }
+        }
+    }
+  }
+
+  private def getFirstFileMatchingPartitionPredicatesInternal(
+      filesDf: DataFrame,
+      shouldWidenNonDeterministicPredicates: Boolean): Option[AddFile] = {
+
+    def rewritePredicateFn(
+        predicate: Expression,
+        shouldRewriteFilter: Boolean): DeltaTableReadPredicate = {
+      val rewrittenPredicate = if (shouldWidenNonDeterministicPredicates) {
+        eliminateNonDeterministicPredicates(Seq(predicate)).newPredicates
+      } else {
+        Seq(predicate)
+      }
+      DeltaTableReadPredicate(
+        partitionPredicates = rewrittenPredicate,
+        shouldRewriteFilter = shouldRewriteFilter)
+    }
+
     // we need to canonicalize the partition predicates per each group of rewrites vs. nonRewrites
     val canonicalPredicates = currentTransactionInfo.readPredicates
       .partition(_.shouldRewriteFilter) match {
         case (rewrites, nonRewrites) =>
           val canonicalRewrites =
-            ExpressionSet(rewrites.map(_.partitionPredicate)).map { e =>
-              DeltaTableReadPredicate(partitionPredicates = Seq(e))
-            }
+            ExpressionSet(rewrites.map(_.partitionPredicate)).map(
+              predicate => rewritePredicateFn(predicate, shouldRewriteFilter = true))
           val canonicalNonRewrites =
-            ExpressionSet(nonRewrites.map(_.partitionPredicate)).map { e =>
-              DeltaTableReadPredicate(partitionPredicates = Seq(e), shouldRewriteFilter = false)
-            }
+            ExpressionSet(nonRewrites.map(_.partitionPredicate)).map(
+              predicate => rewritePredicateFn(predicate, shouldRewriteFilter = false))
           canonicalRewrites ++ canonicalNonRewrites
       }
 
+    import org.apache.spark.sql.delta.implicits._
     val filesMatchingPartitionPredicates = canonicalPredicates.iterator
       .flatMap { readPredicate =>
         DeltaLog.filterFileList(
