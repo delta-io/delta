3c3027a3d85080ea6268790890abd1a4d71ba774
Carmen Kwan <carmen.kwan@databricks.com>
2024-02-15 16:22:54 +0100

    fix whitespace

--- spark/src/test/scala/org/apache/spark/sql/delta/DeltaProtocolVersionSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/DeltaProtocolVersionSuite.scala
@@ -3248,7 +3248,7 @@ trait DeltaProtocolVersionSuiteBase extends QueryTest
         }
 
         // Move past retention period.
-        clock.advance(TimeUnit.HOURS.toMillis( 1))
+        clock.advance(TimeUnit.HOURS.toMillis(1))
 
         sql(s"ALTER TABLE $table DROP FEATURE $featureName TRUNCATE HISTORY")
         assert(deltaLog.update().protocol === Protocol(1, 1))
