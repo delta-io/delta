51b79c7177059f6d5ba70a1ca2d6a2a1f5f92892
Ala Luszczak <ala@databricks.com>
2024-02-26 19:17:22 +0100

    fix wide bounds

--- spark/src/main/scala/org/apache/spark/sql/delta/stats/StatisticsCollection.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/stats/StatisticsCollection.scala
@@ -201,26 +201,29 @@ trait StatisticsCollection extends DeltaLogging {
     val logicalNumRecordsCol = physicalNumRecordsCol - dvCardinalityCol
     val nullCountCol = col(s"$statsColName.$NULL_COUNT")
     val tightBoundsCol = col(s"$statsColName.$TIGHT_BOUNDS")
+    val statsSchema = withStats.schema.apply(statsColName).dataType.asInstanceOf[StructType]
 
-    // Use the schema of the existing stats column. We only want to modify the existing
-    // nullCount stats. Note, when the column mapping mode is enabled, the schema uses
-    // the physical column names, not the logical names.
-    val nullCountSchema = withStats.schema
-      .apply(statsColName).dataType.asInstanceOf[StructType]
-      .apply(NULL_COUNT).dataType.asInstanceOf[StructType]
+    val allStatCols = ALL_STAT_FIELDS.flatMap {
+      case TIGHT_BOUNDS => Some(lit(false).as(TIGHT_BOUNDS))
+      case NULL_COUNT if statsSchema.names.contains(NULL_COUNT) =>
+        // Use the schema of the existing stats column. We only want to modify the existing
+        // nullCount stats. Note, when the column mapping mode is enabled, the schema uses
+        // the physical column names, not the logical names.
+        val nullCountSchema = statsSchema
+          .apply(NULL_COUNT).dataType.asInstanceOf[StructType]
 
-    // When bounds are tight and we are about to transition to wide, store the physical null count
-    // for ALL_NULLs columns.
-    val nullCountColSeq = applyFuncToStatisticsColumn(nullCountSchema, nullCountCol) {
-      case (c, _) =>
-        val allNullTightBounds = tightBoundsCol && (c === logicalNumRecordsCol)
-        Some(when(allNullTightBounds, physicalNumRecordsCol).otherwise(c))
-    }
-
-    val allStatCols = ALL_STAT_FIELDS.map {
-      case f if f == TIGHT_BOUNDS => lit(false).as(TIGHT_BOUNDS)
-      case f if f == NULL_COUNT => struct(nullCountColSeq: _*).as(NULL_COUNT)
-      case f => col(s"${statsColName}.${f}")
+        // When bounds are tight and we are about to transition to wide, store the physical null
+        // count for ALL_NULLs columns.
+        val nullCountColSeq = applyFuncToStatisticsColumn(nullCountSchema, nullCountCol) {
+          case (c, _) =>
+            val allNullTightBounds = tightBoundsCol && (c === logicalNumRecordsCol)
+            Some(when(allNullTightBounds, physicalNumRecordsCol).otherwise(c))
+        }
+        Some(struct(nullCountColSeq: _*).as(NULL_COUNT))
+      case f if statsSchema.names.contains(f) => Some(col(s"${statsColName}.${f}"))
+      case _ =>
+        // This stat is not present in the original stats schema, so we should not include it.
+        None
     }
 
     // This may be very expensive because it is rewriting JSON.
