3703fc8452ca62b4c86dadbce519bc98e9ba4722
B. Micheal Okutubo <micheal.okutubo@databricks.com>
2024-03-19 14:48:06 -0700

    use checkerror

--- spark/src/test/scala/org/apache/spark/sql/delta/DeltaDataFrameWriterV2Suite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/DeltaDataFrameWriterV2Suite.scala
@@ -33,7 +33,6 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.SharedSparkSession
 import org.apache.spark.sql.types.{LongType, StringType, StructType}
-import org.apache.spark.util.Utils
 
 // These tests are copied from Apache Spark (minus partition by expressions) and should work exactly
 // the same with Delta minus some writer options
@@ -676,9 +675,10 @@ class DeltaDataFrameWriterV2Suite
       spark.sql(s"CREATE TABLE $table(id bigint, p int) USING delta PARTITIONED BY (p)")
       def verifyNotImplicitCasting(f: => Unit): Unit = {
         val e = intercept[DeltaAnalysisException](f)
-        assert(e.getErrorClass == "DELTA_FAILED_TO_MERGE_FIELDS")
-        assert(Utils.exceptionString(e)
-          .contains("Failed to merge incompatible data types LongType and IntegerType"))
+        checkError(
+          exception = e.getCause.asInstanceOf[DeltaAnalysisException],
+          errorClass = "DELTA_MERGE_INCOMPATIBLE_DATATYPE",
+          parameters = Map("currentDataType" -> "LongType", "updateDataType" -> "IntegerType"))
       }
       verifyNotImplicitCasting {
         Seq(1 -> 1).toDF("id", "p").write.mode("append").format("delta").saveAsTable(table)
