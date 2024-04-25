4e43b9050af1b90dd8f147ffe0e78a9127e830ae
Venki Korukanti <venki.korukanti@gmail.com>
2024-04-25 16:14:29 -0700

    [Kernel[Writes] Add methods to encode Delta Log actions as `Row` objects (#2976)
    (Split from larger PR #2944)
    
    Add `toRow` for each of the action objects. Also add any missing actions
    such as `CommitInfo`.
    
    Also
    * Rename `READ_SCHEMA` to `FULL_SCHEMA` when all columns in an action
    are present. `READ_SCHEMA` is a term used to read just the subset of
    columns, but in some actions, it also represents the full schema.
    * Utility method to create single action `Row` object from a specific
    actions. Eg. `createMetadataSingleAction(Metadata metadata)` returns a
    `Row` of single action schema with `metaData` column representing the
    given `metadata` object.

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/actions/SingleAction.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/actions/SingleAction.java
@@ -15,7 +15,13 @@
  */
 package io.delta.kernel.internal.actions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import io.delta.kernel.data.Row;
 import io.delta.kernel.types.StructType;
+
+import io.delta.kernel.internal.data.GenericRow;
 
 public class SingleAction {
     /**
@@ -23,11 +29,66 @@ public class SingleAction {
      * of new checkpoint.
      */
     public static StructType CHECKPOINT_SCHEMA = new StructType()
-            .add("txn", SetTransaction.READ_SCHEMA)
+            .add("txn", SetTransaction.FULL_SCHEMA)
             .add("add", AddFile.FULL_SCHEMA)
             .add("remove", RemoveFile.FULL_SCHEMA)
-            .add("metaData", Metadata.READ_SCHEMA)
-            .add("protocol", Protocol.READ_SCHEMA);
+            .add("metaData", Metadata.FULL_SCHEMA)
+            .add("protocol", Protocol.FULL_SCHEMA);
     // Once we start supporting updating CDC or domain metadata enabled tables, we should add the
     // schema for those fields here.
+
+    // Schema to use when writing out the single action to the Delta Log.
+    public static StructType FULL_SCHEMA = new StructType()
+            .add("txn", SetTransaction.FULL_SCHEMA)
+            .add("add", AddFile.SCHEMA_WITH_STATS)
+            .add("remove", new StructType())
+            .add("metaData", Metadata.FULL_SCHEMA)
+            .add("protocol", Protocol.FULL_SCHEMA)
+            .add("cdc", new StructType())
+            .add("commitInfo", CommitInfo.FULL_SCHEMA);
+    // Once we start supporting updating CDC or domain metadata enabled tables, we should add the
+    // schema for those fields here.
+
+    private static final int TXN_ORDINAL = FULL_SCHEMA.indexOf("txn");
+    private static final int ADD_FILE_ORDINAL = FULL_SCHEMA.indexOf("add");
+    private static final int REMOVE_FILE_ORDINAL = FULL_SCHEMA.indexOf("remove");
+    private static final int METADATA_ORDINAL = FULL_SCHEMA.indexOf("metaData");
+    private static final int PROTOCOL_ORDINAL = FULL_SCHEMA.indexOf("protocol");
+    private static final int COMMIT_INFO_ORDINAL = FULL_SCHEMA.indexOf("commitInfo");
+
+    public static Row createAddFileSingleAction(Row addFile) {
+        Map<Integer, Object> singleActionValueMap = new HashMap<>();
+        singleActionValueMap.put(ADD_FILE_ORDINAL, addFile);
+        return new GenericRow(FULL_SCHEMA, singleActionValueMap);
+    }
+
+    public static Row createProtocolSingleAction(Row protocol) {
+        Map<Integer, Object> singleActionValueMap = new HashMap<>();
+        singleActionValueMap.put(PROTOCOL_ORDINAL, protocol);
+        return new GenericRow(FULL_SCHEMA, singleActionValueMap);
+    }
+
+    public static Row createMetadataSingleAction(Row metadata) {
+        Map<Integer, Object> singleActionValueMap = new HashMap<>();
+        singleActionValueMap.put(METADATA_ORDINAL, metadata);
+        return new GenericRow(FULL_SCHEMA, singleActionValueMap);
+    }
+
+    public static Row createRemoveFileSingleAction(Row remove) {
+        Map<Integer, Object> singleActionValueMap = new HashMap<>();
+        singleActionValueMap.put(REMOVE_FILE_ORDINAL, remove);
+        return new GenericRow(FULL_SCHEMA, singleActionValueMap);
+    }
+
+    public static Row createCommitInfoSingleAction(Row commitInfo) {
+        Map<Integer, Object> singleActionValueMap = new HashMap<>();
+        singleActionValueMap.put(COMMIT_INFO_ORDINAL, commitInfo);
+        return new GenericRow(FULL_SCHEMA, singleActionValueMap);
+    }
+
+    public static Row createTxnSingleAction(Row txn) {
+        Map<Integer, Object> singleActionValueMap = new HashMap<>();
+        singleActionValueMap.put(TXN_ORDINAL, txn);
+        return new GenericRow(FULL_SCHEMA, singleActionValueMap);
+    }
 }
