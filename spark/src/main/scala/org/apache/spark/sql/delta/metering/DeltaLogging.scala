delta-io/delta

commit 679e4441556d3a38259b4b9579bc524beaf19dcb

Disentangle schema evolution in MERGE INTO command (#2709)
## What changes were proposed in this pull request?

### Context
Schema evolution in the MERGE INTO command relies on manually updating
the plan of the target table and replaces the original output attributes
to match the schema after evolution.
Manually updating the target plan is unnecessarily complex, error-prone
and ultimately wrong: the new attributes don't match the actual target
schema that was used to resolve all expressions. This worked so far for
schema evolution that only adds new fields or columns that are then
implicitly filled with `null`s on read but breaks when introducing type
evolution due to type mismatches that can't be reconciled.

### Changes
The target plan isn't manually updated to support schema evolution in
the MERGE INTO command anymore. Instead, the original target output is
used and we rely on the different expressions used to write out the data
to produce an output that supports schema evolution. E.p.:
- For target columns that are assigned to by an UPDATE action:
`generateUpdateExpressions` already does the heavy lifting, it is simply
updated to use the actual target output instead of trying to reference
the evolved schema. This allows simplifying a bit the method.
- For target columns that aren't assigned to by an UPDATE action (either
columns without an assignment or with a DELETE action and the deleted
row must be written to the CDC output): no-op expressions are generated
by casting the target output attributes to their corresponding type
after evolution in `getTargetOutputCols`.

The manual handling in `buildTargetPlanWithIndex` and `replaceFileIndex`
that isn't needed anymore is removed. This in turn allow splitting
`replaceFileIndex` in two methods with separate concerns:
`replaceFileIndex` (already exists) and `dropColumns` to manually remove
columns from the target plan.

## How was this patch tested?

spark/src/main/scala/org/apache/spark/sql/delta/metering/DeltaLogging.scala
@@ -36,6 +36,7 @@ import org.apache.spark.sql.util.ScalaExtensions._
 import org.apache.hadoop.fs.Path
 
 import org.apache.spark.SparkThrowable
+import org.apache.spark.util.Utils
 
 /**
  * Convenience wrappers for logging that include delta specific options and
@@ -134,6 +135,31 @@ trait DeltaLogging
         recordFrameProfile("Delta", opType) {
             thunk
         }
+    }
+  }
+
+  /**
+   * Helper method to check invariants in Delta code. Fails when running in tests, records a delta
+   * assertion event and logs a warning otherwise.
+   */
+  protected def deltaAssert(
+      check: => Boolean,
+      name: String,
+      msg: String,
+      deltaLog: DeltaLog = null,
+      data: AnyRef = null,
+      path: Option[Path] = None)
+    : Unit = {
+    if (Utils.isTesting) {
+      assert(check, msg)
+    } else {
+      recordDeltaEvent(
+        deltaLog = deltaLog,
+        opType = s"delta.assertions.$name",
+        data = data,
+        path = path
+      )
+      logWarning(msg)
     }
   }
 

spark/src/main/scala/org/apache/spark/sql/delta/DeltaErrors.scala
@@ -2503,10 +2503,10 @@ trait DeltaErrorsBase
     )
   }
 
-  def nonGeneratedColumnMissingUpdateExpression(column: Attribute): Throwable = {
+  def nonGeneratedColumnMissingUpdateExpression(columnName: String): Throwable = {
     new DeltaIllegalStateException(
       errorClass = "DELTA_NON_GENERATED_COLUMN_MISSING_UPDATE_EXPR",
-      messageParameters = Array(column.toString)
+      messageParameters = Array(columnName)
     )
   }
 

spark/src/main/scala/org/apache/spark/sql/delta/DeltaTable.scala
@@ -341,85 +341,55 @@ object DeltaTableUtils extends PredicateHelper
   }
 
   /**
-   * Replace the file index in a logical plan and return the updated plan.
-   * It's a common pattern that, in Delta commands, we use data skipping to determine a subset of
-   * files that can be affected by the command, so we replace the whole-table file index in the
-   * original logical plan with a new index of potentially affected files, while everything else in
-   * the original plan, e.g., resolved references, remain unchanged.
-   *
    * Many Delta meta-queries involve nondeterminstic functions, which interfere with automatic
-   * column pruning, so columns can be manually pruned from the new scan. Note that partition
-   * columns can never be dropped even if they're not referenced in the rest of the query.
+   * column pruning, so columns can be manually pruned from the scan. Note that partition columns
+   * can never be dropped even if they're not referenced in the rest of the query.
    *
    * @param spark the spark session to use
-   * @param target the logical plan in which we replace the file index
-   * @param fileIndex the new file index
+   * @param target the logical plan in which drop columns
    * @param columnsToDrop columns to drop from the scan
-   * @param newOutput If specified, new logical output to replace the current LogicalRelation.
-   *                  Used for schema evolution to produce the new schema-evolved types from
-   *                  old files, because `target` will have the old types.
    */
-  def replaceFileIndex(
+  def dropColumns(
       spark: SparkSession,
       target: LogicalPlan,
-      fileIndex: FileIndex,
-      columnsToDrop: Seq[String],
-      newOutput: Option[Seq[AttributeReference]]): LogicalPlan = {
+      columnsToDrop: Seq[String]): LogicalPlan = {
     val resolver = spark.sessionState.analyzer.resolver
 
-    var actualNewOutput = newOutput
-    var hasChar = false
-    var newTarget = target transformDown {
-      case l @ LogicalRelation(hfsr: HadoopFsRelation, _, _, _) =>
-        // Prune columns from the scan.
-        val finalOutput = actualNewOutput.getOrElse(l.output).filterNot { col =>
-          columnsToDrop.exists(resolver(_, col.name))
-        }
-
-        // If the output columns were changed e.g. by schema evolution, we need to update
-        // the relation to expose all the columns that are expected after schema evolution.
-        val newDataSchema = StructType(finalOutput.map(attr =>
-          StructField(attr.name, attr.dataType, attr.nullable, attr.metadata)))
-        val newBaseRelation = hfsr.copy(
-          location = fileIndex, dataSchema = newDataSchema)(
-          hfsr.sparkSession)
-        l.copy(relation = newBaseRelation, output = finalOutput)
-
-      case p @ Project(projectList, _) =>
-        // Spark does char type read-side padding via an additional Project over the scan node.
-        // `newOutput` references the Project attributes, we need to translate their expression IDs
-        // so that `newOutput` references attributes from the LogicalRelation instead.
+    // Spark does char type read-side padding via an additional Project over the scan node.
+    // When char type read-side padding is applied, we need to apply column pruning for the
+    // Project as well, otherwise the Project will contain missing attributes.
+    val hasChar = target.exists {
+      case Project(projectList, _) =>
         def hasCharPadding(e: Expression): Boolean = e.exists {
           case s: StaticInvoke => s.staticObject == classOf[CharVarcharCodegenUtils] &&
             s.functionName == "readSidePadding"
           case _ => false
         }
-        val charColMapping = AttributeMap(projectList.collect {
-          case a: Alias if hasCharPadding(a.child) && a.references.size == 1 =>
-            hasChar = true
-            val tableCol = a.references.head.asInstanceOf[AttributeReference]
-            a.toAttribute -> tableCol
-        })
-        actualNewOutput = newOutput.map(_.map { attr =>
-          charColMapping.get(attr).map { tableCol =>
-            attr.withExprId(tableCol.exprId)
-          }.getOrElse(attr)
-        })
-        p
+        projectList.exists {
+          case a: Alias => hasCharPadding(a.child) && a.references.size == 1
+          case _ => false
+        }
+      case _ => false
     }
 
-    if (hasChar) {
-      // When char type read-side padding is applied, we need to apply column pruning for the
-      // Project as well, otherwise the Project will contain missing attributes.
-      newTarget = newTarget.transformUp {
-        case p @ Project(projectList, child) =>
-          val newProjectList = projectList.filter { e =>
-            e.references.subsetOf(child.outputSet)
-          }
-          p.copy(projectList = newProjectList)
-      }
+    target transformUp {
+      case l@LogicalRelation(hfsr: HadoopFsRelation, _, _, _) =>
+        // Prune columns from the scan.
+        val prunedOutput = l.output.filterNot { col =>
+          columnsToDrop.exists(resolver(_, col.name))
+        }
+
+        val prunedSchema = StructType(prunedOutput.map(attr =>
+          StructField(attr.name, attr.dataType, attr.nullable, attr.metadata)))
+        val newBaseRelation = hfsr.copy(dataSchema = prunedSchema)(hfsr.sparkSession)
+        l.copy(relation = newBaseRelation, output = prunedOutput)
+
+      case p @ Project(projectList, child) if hasChar =>
+        val newProjectList = projectList.filter { e =>
+          e.references.subsetOf(child.outputSet)
+        }
+        p.copy(projectList = newProjectList)
     }
-    newTarget
   }
 
   /**

spark/src/main/scala/org/apache/spark/sql/delta/PreprocessTableMerge.scala
@@ -65,7 +65,7 @@ case class PreprocessTableMerge(override val conf: SQLConf)
       throw DeltaErrors.targetTableFinalSchemaEmptyException()
     }
 
-    val finalSchema = finalSchemaOpt.get
+    val postEvolutionTargetSchema = finalSchemaOpt.get
 
     def checkCondition(cond: Expression, conditionName: String): Unit = {
       if (!cond.deterministic) {
@@ -108,8 +108,8 @@ case class PreprocessTableMerge(override val conf: SQLConf)
           whenClauses = matched ++ notMatched ++ notMatchedBySource,
           identityColumns = additionalColumns,
           generatedColumns = generatedColumns,
-          allowStructEvolution = withSchemaEvolution,
-          finalSchema = finalSchema)
+          allowSchemaEvolution = withSchemaEvolution,
+          postEvolutionTargetSchema = postEvolutionTargetSchema)
         m.copy(m.condition, alignedActions)
       case m: DeltaMergeIntoMatchedDeleteClause => m // Delete does not need reordering
     }
@@ -120,9 +120,9 @@ case class PreprocessTableMerge(override val conf: SQLConf)
           m.resolvedActions,
           whenClauses = matched ++ notMatched ++ notMatchedBySource,
           identityColumns = additionalColumns,
-          generatedColumns,
-          allowStructEvolution = withSchemaEvolution,
-          finalSchema)
+          generatedColumns = generatedColumns,
+          allowSchemaEvolution = withSchemaEvolution,
+          postEvolutionTargetSchema = postEvolutionTargetSchema)
         m.copy(m.condition, alignedActions)
       case m: DeltaMergeIntoNotMatchedBySourceDeleteClause => m // Delete does not need reordering
     }
@@ -147,7 +147,7 @@ case class PreprocessTableMerge(override val conf: SQLConf)
       // Generate actions for columns that are not explicitly inserted. They might come from
       // the original schema of target table or the schema evolved columns. In either case they are
       // covered by `finalSchema`.
-      val implicitActions = finalSchema.filterNot { col =>
+      val implicitActions = postEvolutionTargetSchema.filterNot { col =>
         m.resolvedActions.exists { insertAct =>
           conf.resolver(insertAct.targetColNameParts.head, col.name)
         }
@@ -165,11 +165,11 @@ case class PreprocessTableMerge(override val conf: SQLConf)
         actions,
         source,
         generatedColumns.map(f => (f, true)) ++ additionalColumns.map(f => (f, false)),
-        finalSchema)
+        postEvolutionTargetSchema)
 
       trackHighWaterMarks ++= trackFromInsert
 
-      val alignedActions: Seq[DeltaMergeAction] = finalSchema.map { targetAttrib =>
+      val alignedActions: Seq[DeltaMergeAction] = postEvolutionTargetSchema.map { targetAttrib =>
         actionsWithGeneratedColumns.find { a =>
           conf.resolver(targetAttrib.name, a.targetColNameParts.head)
         }.map { a =>
@@ -264,10 +264,10 @@ case class PreprocessTableMerge(override val conf: SQLConf)
    * @param identityColumns Additional identity columns present in the table.
    * @param generatedColumns List of the generated columns in the table. See
    *                         [[UpdateExpressionsSupport]].
-   * @param allowStructEvolution Whether to allow structs to evolve. See
+   * @param allowSchemaEvolution Whether to allow schema to evolve. See
    *                             [[UpdateExpressionsSupport]].
-   * @param finalSchema The schema of the target table after the merge operation.
-   * @return Update actions aligned on the target output schema `finalSchema`.
+   * @param postEvolutionTargetSchema The schema of the target table after the merge operation.
+   * @return Update actions aligned on the target output schema `postEvolutionTargetSchema`.
    */
   private def alignUpdateActions(
       target: LogicalPlan,
@@ -275,8 +275,8 @@ case class PreprocessTableMerge(override val conf: SQLConf)
       whenClauses: Seq[DeltaMergeIntoClause],
       identityColumns: Seq[StructField],
       generatedColumns: Seq[StructField],
-      allowStructEvolution: Boolean,
-      finalSchema: StructType)
+      allowSchemaEvolution: Boolean,
+      postEvolutionTargetSchema: StructType)
     : Seq[DeltaMergeAction] = {
     // Get the operations for columns that already exist...
     val existingUpdateOps = resolvedActions.map { a =>
@@ -284,26 +284,17 @@ case class PreprocessTableMerge(override val conf: SQLConf)
     }
 
     // And construct operations for columns that the insert/update clauses will add.
-    val newUpdateOps = generateUpdateOpsForNewTargetFields(target, finalSchema, resolvedActions)
+    val newUpdateOps =
+      generateUpdateOpsForNewTargetFields(target, postEvolutionTargetSchema, resolvedActions)
 
-    // Get expressions for the final schema for alignment. Note that attributes which already
-    // exist in the target need to use the same expression ID, even if the schema will evolve.
-    val finalSchemaExprs =
-    finalSchema.map { field =>
-      target.resolve(Seq(field.name), conf.resolver).map { r =>
-        AttributeReference(field.name, field.dataType)(r.exprId)
-      }.getOrElse {
-        AttributeReference(field.name, field.dataType)()
-      }
-    }
-
-    // Use the helper methods for in UpdateExpressionsSupport to generate expressions such
-    // that nested fields can be updated (only for existing columns).
+    // Use the helper methods in UpdateExpressionsSupport to generate expressions such that nested
+    // fields can be updated (only for existing columns).
     val alignedExprs = generateUpdateExpressions(
-      finalSchemaExprs,
-      existingUpdateOps ++ newUpdateOps,
-      conf.resolver,
-      allowStructEvolution = allowStructEvolution,
+      targetSchema = postEvolutionTargetSchema,
+      updateOps = existingUpdateOps ++ newUpdateOps,
+      defaultExprs = target.output,
+      resolver = conf.resolver,
+      allowSchemaEvolution = allowSchemaEvolution,
       generatedColumns = generatedColumns)
 
     val alignedExprsWithGenerationExprs =
@@ -311,13 +302,13 @@ case class PreprocessTableMerge(override val conf: SQLConf)
         alignedExprs.map(_.get)
       } else {
         generateUpdateExprsForGeneratedColumns(target, generatedColumns, alignedExprs,
-          Some(finalSchemaExprs))
+          Some(postEvolutionTargetSchema))
       }
 
     alignedExprsWithGenerationExprs
-      .zip(finalSchemaExprs)
-      .map { case (expr, attrib) =>
-        DeltaMergeAction(Seq(attrib.name), expr, targetColNameResolved = true)
+      .zip(postEvolutionTargetSchema)
+      .map { case (expr, field) =>
+        DeltaMergeAction(Seq(field.name), expr, targetColNameResolved = true)
       }
   }
 
@@ -327,13 +318,13 @@ case class PreprocessTableMerge(override val conf: SQLConf)
    * the merge clause.
    *
    * @param target Logical plan node of the target table of merge.
-   * @param finalSchema The schema of the target table after the merge operation.
+   * @param postEvolutionTargetSchema The schema of the target table after the merge operation.
    * @param resolvedActions Merge actions of the update clause being processed.
    * @return List of update operations
    */
   private def generateUpdateOpsForNewTargetFields(
       target: LogicalPlan,
-      finalSchema: StructType,
+      postEvolutionTargetSchema: StructType,
       resolvedActions: Seq[DeltaMergeAction])
     : Seq[UpdateOperation] = {
     // Collect all fields in the final schema that were added by schema evolution.
@@ -342,7 +333,7 @@ case class PreprocessTableMerge(override val conf: SQLConf)
     val targetSchemaBeforeEvolution =
       target.schema.map(SchemaPruning.RootField(_, derivedFromAtt = false))
     val newTargetFields =
-      StructType(SchemaPruning.pruneSchema(finalSchema, targetSchemaBeforeEvolution)
+      StructType(SchemaPruning.pruneSchema(postEvolutionTargetSchema, targetSchemaBeforeEvolution)
       .filterNot { topLevelField => target.schema.exists(_.name == topLevelField.name) })
 
     /**
@@ -404,7 +395,7 @@ case class PreprocessTableMerge(override val conf: SQLConf)
     allActions: Seq[DeltaMergeAction],
     sourcePlan: LogicalPlan,
     columnWithDefaultExpr: Seq[(StructField, Boolean)],
-    finalSchema: StructType): (Seq[DeltaMergeAction], Set[String]) = {
+    postEvolutionTargetSchema: StructType): (Seq[DeltaMergeAction], Set[String]) = {
     val implicitColumns = columnWithDefaultExpr.filter {
       case (field, _) =>
         !explicitActions.exists { insertAct =>
@@ -414,9 +405,9 @@ case class PreprocessTableMerge(override val conf: SQLConf)
     if (implicitColumns.isEmpty) {
       return (allActions, Set[String]())
     }
-    assert(finalSchema.size == allActions.size,
+    assert(postEvolutionTargetSchema.size == allActions.size,
       "Invalid number of columns in INSERT clause with generated columns. Expected schema: " +
-      s"$finalSchema, INSERT actions: $allActions")
+      s"$postEvolutionTargetSchema, INSERT actions: $allActions")
 
     val track = mutable.Set[String]()
 

spark/src/main/scala/org/apache/spark/sql/delta/PreprocessTableUpdate.scala
@@ -67,11 +67,13 @@ case class PreprocessTableUpdate(sqlConf: SQLConf)
 
     val targetColNameParts = update.updateColumns.map(DeltaUpdateTable.getTargetColNameParts(_))
     val alignedUpdateExprs = generateUpdateExpressions(
-      update.child.output,
-      targetColNameParts,
-      update.updateExpressions,
-      conf.resolver,
-      generatedColumns)
+      targetSchema = update.child.schema,
+      defaultExprs = update.child.output,
+      nameParts = targetColNameParts,
+      updateExprs = update.updateExpressions,
+      resolver = conf.resolver,
+      generatedColumns = generatedColumns
+    )
     val alignedUpdateExprsAfterAddingGenerationExprs =
       if (alignedUpdateExprs.forall(_.nonEmpty)) {
         alignedUpdateExprs.map(_.get)

spark/src/main/scala/org/apache/spark/sql/delta/ResolveDeltaMergeInto.scala
@@ -227,7 +227,7 @@ object ResolveDeltaMergeInto {
       resolveClause(_, plansToResolveAction = Seq(target))
     }
 
-    val finalSchema = if (canEvolveSchema) {
+    val postEvolutionTargetSchema = if (canEvolveSchema) {
       // When schema evolution is enabled, add to the target table new columns or nested fields that
       // are assigned to in merge actions and not already part of the target schema. This is done by
       // collecting all assignments from merge actions and using them to filter out the source
@@ -298,7 +298,7 @@ object ResolveDeltaMergeInto {
       resolvedNotMatchedClauses,
       resolvedNotMatchedBySourceClauses,
       withSchemaEvolution = canEvolveSchema,
-      finalSchema = Some(finalSchema))
+      finalSchema = Some(postEvolutionTargetSchema))
 
     // Its possible that pre-resolved expressions (e.g. `sourceDF("key") = targetDF("key")`) have
     // attribute references that are not present in the output attributes of the children (i.e.,

commit 91cd61a867a0d3b243832f8ea486f5b8cba2e002

[Spark] Fix logging failure in deltaAssert helper (#3052)
## Description
Fix helper method `deltaAssert` introduced in
https://github.com/delta-io/delta/pull/2709 to not log failures when the
assertion holds.

## How was this patch tested?
The helper intentionally behaves differently in tests and in production
(failing vs. logging), there's no easy/meaningful way to test the prod
behavior from tests.

spark/src/main/scala/org/apache/spark/sql/delta/metering/DeltaLogging.scala
@@ -152,7 +152,7 @@ trait DeltaLogging
     : Unit = {
     if (Utils.isTesting) {
       assert(check, msg)
-    } else {
+    } else if (!check) {
       recordDeltaEvent(
         deltaLog = deltaLog,
         opType = s"delta.assertions.$name",