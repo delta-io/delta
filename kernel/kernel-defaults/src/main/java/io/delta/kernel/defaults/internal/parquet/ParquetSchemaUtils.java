aa907042bfece900b8b8893c2ed83b1706f2629a
Venki Korukanti <venki.korukanti@gmail.com>
2024-05-10 16:00:23 -0700

    [Kernel][Writes] Write `timestamp` as `INT64` type to Parquet data files (#3084)
    ## Description
    Write the `timestamp` as `INT64` physical format in Parquet. Currently,
    it is written as `INT96` which is a very old method of writing timestamp
    and deprecated a long time ago. Also, collect statistics, for
    `timestamp` type columns.
    
    ## How was this patch tested?
    Update the existing tests.

--- kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/parquet/ParquetSchemaUtils.java
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/parquet/ParquetSchemaUtils.java
@@ -229,8 +229,14 @@ class ParquetSchemaUtils {
         } else if (dataType instanceof DateType) {
             type = primitive(INT32, repetition).as(LogicalTypeAnnotation.dateType()).named(name);
         } else if (dataType instanceof TimestampType) {
-            // We are supporting only the INT96 format now.
-            type = primitive(INT96, repetition).named(name);
+            // Kernel is by default going to write as INT64 with isAdjustedToUTC set to true
+            // Delta-Spark writes as INT96 for legacy reasons (maintaining compatibility with
+            // unknown consumers with very, very old versions of Parquet reader). Kernel is a new
+            // project, and we are ok if it breaks readers (we use this opportunity to find such
+            // readers and ask them to upgrade).
+            type = primitive(INT64, repetition)
+                    .as(timestampType(true /* isAdjustedToUTC */, MICROS))
+                    .named(name);
         } else if (dataType instanceof TimestampNTZType) {
             // Write as INT64 with isAdjustedToUTC set to false
             type = primitive(INT64, repetition)
