a564b4a2179fe0c8b58d17ff9f9e5ad3aa5578f6
James DeLoye <james.deloye@databricks.com>
2024-05-23 13:55:28 -0700

    Update DeltaCreateTableLikeSuite.scala

--- spark/src/test/scala/org/apache/spark/sql/delta/DeltaCreateTableLikeSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/DeltaCreateTableLikeSuite.scala
@@ -418,7 +418,8 @@ class DeltaCreateTableLikeSuite extends QueryTest
   }
 
     test("CREATE TABLE LIKE where sourceTable is a parquet table and " +
-      "user explicitly provides table properties") {
+      "user explicitly provides table properties"
+      ){
     val srcTbl = "srcTbl"
     val targetTbl = "targetTbl"
     withTable(srcTbl, targetTbl) {
