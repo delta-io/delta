49b1060366d4e02f9c66183b820ef9106dbe2920
Thang Long VU <long.vu@databricks.com>
2024-05-29 11:19:18 +0200

    Add Return types

--- python/delta/connect/testing/utils.py
+++ python/delta/connect/testing/utils.py
@@ -18,16 +18,17 @@ import tempfile
 import shutil
 import os
 
+from pyspark import SparkConf
 from pyspark.testing.connectutils import ReusedConnectTestCase
 
 
 class DeltaTestCase(ReusedConnectTestCase):
     """
-    Test suite base for setting up a properly configured SparkSession for using Delta.
+    Test suite base for setting up a properly configured SparkSession for using Delta Connect.
     """
 
     @classmethod
-    def conf(cls):
+    def conf(cls) -> SparkConf:
         _conf = super(DeltaTestCase, cls).conf()
         _conf.set("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         _conf.set("spark.sql.catalog.spark_catalog",
@@ -38,11 +39,11 @@ class DeltaTestCase(ReusedConnectTestCase):
                   "org.apache.spark.sql.connect.delta.DeltaCommandPlugin")
         return _conf
 
-    def setUp(self):
+    def setUp(self) -> None:
         super(DeltaTestCase, self).setUp()
         self.tempPath = tempfile.mkdtemp()
         self.tempFile = os.path.join(self.tempPath, "tempFile")
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         super(DeltaTestCase, self).tearDown()
         shutil.rmtree(self.tempPath)
