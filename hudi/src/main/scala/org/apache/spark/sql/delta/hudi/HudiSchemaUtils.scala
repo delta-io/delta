0e5b85681955d19237c51865af58d8ae9ceedc06
Annie Wang <170372889+anniewang-db@users.noreply.github.com>
2024-07-08 10:44:22 -0700

    [Hudi] Flesh out tests and update column type support (#3339)
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md
    2. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      3. Be sure to keep the PR description updated to reflect all changes.
      4. Please write your PR title to summarize what this PR proposes.
    5. If possible, provide a concise example to reproduce the issue for a
    faster review.
    6. If applicable, include the corresponding issue number in the PR title
    and link it in the body.
    -->
    
    #### Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [ ] Spark
    - [ ] Standalone
    - [ ] Flink
    - [ ] Kernel
    - [x] Other (Hudi)
    
    ## Description
    
    <!--
    - Describe what this PR changes.
    - Describe why we need the change.
     
    If this PR resolves an issue be sure to include "Resolves #XXX" to
    correctly link and close the issue upon merge.
    -->
    This PR adds unit tests and also removes support of some data types that
    are not supported by Hudi (e.g. TIMESTAMP_NTZ, SHORT, BYTE, etc)
    
    ## How was this patch tested?
    
    <!--
    If tests were added, say they were added here. Please make sure to test
    the changes thoroughly including negative and positive cases if
    possible.
    If the changes were tested in any way other than unit tests, please
    clarify how you tested step by step (ideally copy and paste-able, so
    that other reviewers can test and check, and descendants can verify in
    the future).
    If the changes were not tested, please explain why.
    -->
    Unit tests
    
    ## Does this PR introduce _any_ user-facing changes?
    
    <!--
    If yes, please clarify the previous behavior and the change this PR
    proposes - provide the console output, description and/or an example to
    show the behavior difference if possible.
    If possible, please also clarify if this is a user-facing change
    compared to the released Delta Lake versions or within the unreleased
    branches such as master.
    If no, write 'No'.
    -->
    No

--- hudi/src/main/scala/org/apache/spark/sql/delta/hudi/HudiSchemaUtils.scala
+++ hudi/src/main/scala/org/apache/spark/sql/delta/hudi/HudiSchemaUtils.scala
@@ -16,12 +16,14 @@
 
 package org.apache.spark.sql.delta.hudi
 
-import org.apache.avro.{LogicalTypes, Schema}
-import org.apache.spark.sql.delta.metering.DeltaLogging
-import org.apache.spark.sql.types._
-
 import java.util
+
 import scala.collection.JavaConverters._
+
+import org.apache.spark.sql.delta.metering.DeltaLogging
+import org.apache.avro.{LogicalTypes, Schema}
+
+import org.apache.spark.sql.types._
 
 object HudiSchemaUtils extends DeltaLogging {
 
@@ -73,7 +75,8 @@ object HudiSchemaUtils extends DeltaLogging {
   private def convertAtomic[E <: DataType](elem: E, isNullable: Boolean) = elem match {
     case StringType => finalizeSchema(Schema.create(Schema.Type.STRING), isNullable)
     case LongType => finalizeSchema(Schema.create(Schema.Type.LONG), isNullable)
-    case IntegerType | ShortType => finalizeSchema(Schema.create(Schema.Type.INT), isNullable)
+    case IntegerType => finalizeSchema(
+      Schema.create(Schema.Type.INT), isNullable)
     case FloatType => finalizeSchema(Schema.create(Schema.Type.FLOAT), isNullable)
     case DoubleType => finalizeSchema(Schema.create(Schema.Type.DOUBLE), isNullable)
     case d: DecimalType => finalizeSchema(LogicalTypes.decimal(d.precision, d.scale)
@@ -84,8 +87,6 @@ object HudiSchemaUtils extends DeltaLogging {
       LogicalTypes.date.addToSchema(Schema.create(Schema.Type.INT)), isNullable)
     case TimestampType => finalizeSchema(
       LogicalTypes.timestampMicros.addToSchema(Schema.create(Schema.Type.LONG)), isNullable)
-    case TimestampNTZType => finalizeSchema(
-      LogicalTypes.localTimestampMicros.addToSchema(Schema.create(Schema.Type.LONG)), isNullable)
     case _ => throw new UnsupportedOperationException(s"Could not convert atomic type $elem")
   }
 }
