b8074fe3cec7e08bdffe5809c5eeec0e56d22434
Annie Wang <annie.wang@databricks.com>
2024-07-24 11:17:28 -0700

    remove extra import

--- hudi/src/test/scala/org/apache/spark/sql/delta/hudi/ConvertToHudiSuite.scala
+++ hudi/src/test/scala/org/apache/spark/sql/delta/hudi/ConvertToHudiSuite.scala
@@ -42,7 +42,6 @@ import org.apache.hudi.storage.hadoop.{HadoopStorageConfiguration, HoodieHadoopS
 import org.apache.spark.sql.QueryTest
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.avro.SchemaConverters
-import org.apache.spark.sql.test.SharedSparkSession
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.util.ManualClock
 
