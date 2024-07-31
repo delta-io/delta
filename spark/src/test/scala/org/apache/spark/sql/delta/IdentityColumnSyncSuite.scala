f68c5d6c33ce5f9574e9533857bdbdcf6a532ffc
zhipeng.mao <zhipeng.mao@databricks.com>
2024-07-31 13:11:53 +0200

    style error

--- spark/src/test/scala/org/apache/spark/sql/delta/IdentityColumnSyncSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/IdentityColumnSyncSuite.scala
@@ -221,7 +221,8 @@ trait IdentityColumnSyncSuiteBase
       val ex = intercept[AnalysisException] {
         sql(s"ALTER TABLE $tblName ALTER COLUMN id SYNC IDENTITY")
       }
-      assert(ex.getMessage.contains("ALTER TABLE ALTER COLUMN SYNC IDENTITY is only supported by Delta."))
+      assert(ex.getMessage.contains(
+        "ALTER TABLE ALTER COLUMN SYNC IDENTITY is only supported by Delta."))
     }
   }
 
@@ -237,7 +238,8 @@ trait IdentityColumnSyncSuiteBase
       val ex = intercept[AnalysisException] {
         sql(s"ALTER TABLE $tblName ALTER COLUMN id SYNC IDENTITY")
       }
-      assert(ex.getMessage.contains("ALTER TABLE ALTER COLUMN SYNC IDENTITY cannot be called on non IDENTITY columns."))
+      assert(ex.getMessage.contains(
+        "ALTER TABLE ALTER COLUMN SYNC IDENTITY cannot be called on non IDENTITY columns."))
     }
   }
 }
