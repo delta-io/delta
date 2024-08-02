ee4cb2fa81c485bb1a362b3dda4dd329885a54a8
Thang Long VU <long.vu@databricks.com>
2024-08-02 17:19:31 +0200

    Add ConflictChecker Rule

--- spark/src/test/scala/org/apache/spark/sql/delta/rowtracking/RowTrackingConflictResolutionSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/rowtracking/RowTrackingConflictResolutionSuite.scala
@@ -16,17 +16,27 @@
 
 package org.apache.spark.sql.delta.rowtracking
 
-import org.apache.spark.sql.delta.{DeltaLog, DeltaOperations, RowId, RowTrackingFeature}
+import org.apache.spark.sql.delta.{DeletionVectorsTestUtils, DeltaLog, DeltaOperations, DeltaTestUtils, FileMetadataMaterializationTracker, OptimisticTransaction, RowId, RowTrackingFeature}
+import org.apache.spark.sql.delta.DeltaOperations.{ManualUpdate, Truncate}
 import org.apache.spark.sql.delta.actions.{Action, AddFile}
+import org.apache.spark.sql.delta.actions.{Metadata, RemoveFile}
+import org.apache.spark.sql.delta.commands.backfill.{BackfillBatchIterator, BackfillCommandStats, RowTrackingBackfillBatch, RowTrackingBackfillExecutor}
+import org.apache.spark.sql.delta.deletionvectors.RoaringBitmapArray
 import org.apache.spark.sql.delta.rowid.RowIdTestUtils
+import org.apache.spark.sql.delta.sources.DeltaSQLConf
 import org.apache.spark.sql.delta.test.DeltaTestImplicits._
 
 import org.apache.spark.sql.QueryTest
 import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.dsl.expressions._
+import org.apache.spark.sql.catalyst.expressions.{EqualTo, Literal}
 import org.apache.spark.sql.test.SharedSparkSession
+import org.apache.spark.sql.types.{IntegerType, StructType}
 
 class RowTrackingConflictResolutionSuite extends QueryTest
-  with SharedSparkSession with RowIdTestUtils {
+  with DeletionVectorsTestUtils
+  with SharedSparkSession
+  with RowIdTestUtils {
 
   private val testTableName = "test_table"
 
@@ -145,6 +155,174 @@ class RowTrackingConflictResolutionSuite extends QueryTest
       txn.commit(Seq(addFile(filePath)), DeltaOperations.ManualUpdate)
 
       assertRowIdsAreValid(deltaLog)
+    }
+  }
+
+  /**
+   * Setup a test table with four files and return these files to the caller.
+   */
+  private def setupTableAndGetAllFiles(log: DeltaLog): (AddFile, AddFile, AddFile, AddFile) = {
+    val f1 = DeltaTestUtils.createTestAddFile(encodedPath = "a", partitionValues = Map("x" -> "1"))
+    val f2 = DeltaTestUtils.createTestAddFile(encodedPath = "b", partitionValues = Map("x" -> "1"))
+    val f3 = DeltaTestUtils.createTestAddFile(encodedPath = "c", partitionValues = Map("x" -> "2"))
+    val f4 = DeltaTestUtils.createTestAddFile(encodedPath = "d", partitionValues = Map("x" -> "2"))
+
+    val setupActions: Seq[Action] = Seq(
+      Metadata(
+        schemaString = new StructType().add("x", IntegerType).json,
+        partitionColumns = Seq("x")),
+      f1,
+      f2,
+      f3,
+      f4,
+      Action.supportedProtocolVersion().withFeature(RowTrackingFeature)
+    )
+
+    log.startTransaction().commit(setupActions, ManualUpdate)
+
+    (f1, f2, f3, f4)
+  }
+
+  /** Add a dummy DV to a file in a table. */
+  private def addDVToFileInTable(deltaLog: DeltaLog, file: AddFile): (AddFile, RemoveFile) = {
+    val dv = writeDV(deltaLog, RoaringBitmapArray(0L))
+    updateFileDV(file, dv)
+  }
+
+  /** Execute backfill on the table associated with the delta log passed in. */
+  private def executeBackfill(log: DeltaLog, backfillTxn: OptimisticTransaction): Unit = {
+    val maxBatchesInParallel = 1
+    val backfillStats = BackfillCommandStats(
+      backfillTxn.txnId,
+      nameOfTriggeringOperation = DeltaOperations.OP_SET_TBLPROPERTIES,
+      maxNumBatchesInParallel = maxBatchesInParallel)
+    val backfillExecutor = new RowTrackingBackfillExecutor(
+      spark,
+      backfillTxn,
+      FileMetadataMaterializationTracker.noopTracker,
+      maxBatchesInParallel,
+      backfillStats
+    )
+    val filesToBackfill =
+      RowTrackingBackfillExecutor.getCandidateFilesToBackfill(log.update())
+    val batches = new BackfillBatchIterator(
+      filesToBackfill,
+      FileMetadataMaterializationTracker.noopTracker,
+      maxNumFilesPerBin = 4,
+      constructBatch = RowTrackingBackfillBatch(_))
+
+    backfillExecutor.run(batches)
+  }
+
+  test("Backfill conflict with a delete, Delete wins") {
+    withTempDir { dir =>
+      val log = DeltaLog.forTable(spark, dir.getCanonicalPath)
+
+      // Setup
+      val (file1, file2, file3, file4) = setupTableAndGetAllFiles(log)
+
+      // Start Backfill.
+      val backfillTxn = log.startTransaction()
+
+      // A delete occurs in parallel. Delete wins.
+      val deleteTxn = log.startTransaction()
+      deleteTxn.filterFiles(EqualTo('x, Literal(1)) :: Nil)
+      val deleteActions = Seq(file1.remove, file2.remove)
+      // Truncate is a data-changing operation.
+      deleteTxn.commit(deleteActions, Truncate())
+
+      // Finish backfill.
+      executeBackfill(log, backfillTxn)
+
+      val finalFiles = log.update().allFiles.collect()
+      finalFiles.foreach(a => assert(a.baseRowId.nonEmpty))
+      assertRowIdsAreValid(log)
+      assert(finalFiles.map(_.path).toSet === Seq(file3, file4).map(_.path).toSet)
+    }
+  }
+
+  test("Backfill conflicts with a delete, Backfill wins") {
+    withTempDir { dir =>
+      val log = DeltaLog.forTable(spark, dir.getCanonicalPath)
+      // Setup
+      val (file1, file2, file3, file4) = setupTableAndGetAllFiles(log)
+
+      // Start delete
+      val deleteTxn = log.startTransaction()
+      deleteTxn.filterFiles(EqualTo('x, Literal(1)) :: Nil)
+
+      // Backfill occurs in parallel and wins.
+      val backfillTxn = log.startTransaction()
+      executeBackfill(log, backfillTxn)
+
+      val deleteActions = Seq(file1.remove, file2.remove)
+      // Truncate is a data-changing operation.
+      deleteTxn.commit(deleteActions, Truncate())
+
+      val finalFiles = log.update().allFiles.collect()
+      finalFiles.foreach(a => assert(a.baseRowId.nonEmpty))
+      assertRowIdsAreValid(log)
+      assert(finalFiles.map(_.path).toSet == Seq(file3, file4).map(_.path).toSet)
+    }
+  }
+
+  test("Backfill conflicts with a DV delete, Delete wins") {
+    withTempDir { dir =>
+      val log = DeltaLog.forTable(spark, dir.getCanonicalPath)
+
+      // Setup
+      val (file1, file2, file3, file4) = setupTableAndGetAllFiles(log)
+      enableDeletionVectorsInTable(log)
+
+      // Start Backfill
+      val backfillTxn = log.startTransaction()
+
+      // A delete occurs in parallel. Delete wins.
+      val deleteTxn = log.startTransaction()
+      deleteTxn.filterFiles(EqualTo('x, Literal(1)) :: Nil)
+      val (addFile1WithDV, removeFile1) = addDVToFileInTable(log, file1)
+      val (addFile2WithDV, removeFile2) = addDVToFileInTable(log, file2)
+      val deleteActions = Seq(addFile1WithDV, removeFile1, addFile2WithDV, removeFile2)
+      // Truncate is a data-changing operation.
+      deleteTxn.commit(deleteActions, Truncate())
+
+      // Finish Backfill
+      executeBackfill(log, backfillTxn)
+
+      val finalFiles = log.update().allFiles.collect()
+      finalFiles.foreach(a => assert(a.baseRowId.nonEmpty))
+      assertRowIdsAreValid(log)
+      val allFiles = Seq(file1, file2, file3, file4)
+      assert(finalFiles.map(_.path).toSet === allFiles.map(_.path).toSet)
+    }
+  }
+
+  test("Backfill conflicts with a DV delete, Backfill wins") {
+    withTempDir { dir =>
+      val log = DeltaLog.forTable(spark, dir.getCanonicalPath)
+      // Setup
+      val (file1, file2, file3, file4) = setupTableAndGetAllFiles(log)
+      enableDeletionVectorsInTable(log)
+
+      // Start delete
+      val deleteTxn = log.startTransaction()
+      deleteTxn.filterFiles(EqualTo('x, Literal(1)) :: Nil)
+
+      // Backfill occurs in parallel and wins.
+      val backfillTxn = log.startTransaction()
+      executeBackfill(log, backfillTxn)
+
+      val (addFile1WithDV, removeFile1) = addDVToFileInTable(log, file1)
+      val (addFile2WithDV, removeFile2) = addDVToFileInTable(log, file2)
+      val deleteActions = Seq(addFile1WithDV, removeFile1, addFile2WithDV, removeFile2)
+      // Truncate is a data-changing operation.
+      deleteTxn.commit(deleteActions, Truncate())
+
+      val finalFiles = log.update().allFiles.collect()
+      finalFiles.foreach(a => assert(a.baseRowId.nonEmpty))
+      assertRowIdsAreValid(log)
+      val allFiles = Seq(file1, file2, file3, file4)
+      assert(finalFiles.map(_.path).toSet === allFiles.map(_.path).toSet)
     }
   }
 }
