b1fdee86502eaa6329270f9502ad0adcde22d37b
Zihao Xu <xzhseh@gmail.com>
2024-08-05 12:12:30 -0700

    [Spark][Backport 3.2] Fix the semantic of `shouldRewriteToBeIcebergCompatible` in REORG UPGRADE UNIFORM (#3474)
    ## Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [x] Spark
    - [ ] Standalone
    - [ ] Flink
    - [ ] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    currently we utilize the helper function
    `shouldRewriteToBeIcebergCompatible` to filter the portion of parquet
    files that need to be rewritten when running `REORG UPGRADE UNIFORM`
    based on the tags in the `AddFile`.
    
    however, the `DeltaUpgradeUniformOperation.icebergCompatVersion` is
    accidentally shadowed, which will make
    `shouldRewriteToBeIcebergCompatible` always return `false` if the
    `AddFile.tags` is not `null` - this is not the expected semantic of this
    function.
    
    this PR introduces the fix for this problem and add unit tests to ensure
    the correctness.
    
    ## How was this patch tested?
    through unit tests in `UniFormE2ESuite.scala`.
    
    ## Does this PR introduce _any_ user-facing changes? 
    no.

--- spark/src/main/scala/org/apache/spark/sql/delta/commands/DeltaReorgTableCommand.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/commands/DeltaReorgTableCommand.scala
@@ -118,8 +118,9 @@ class DeltaUpgradeUniformOperation(icebergCompatVersion: Int) extends DeltaReorg
   override def filterFilesToReorg(snapshot: Snapshot, files: Seq[AddFile]): Seq[AddFile] = {
     def shouldRewriteToBeIcebergCompatible(file: AddFile): Boolean = {
       if (file.tags == null) return true
-      val icebergCompatVersion = file.tags.getOrElse(AddFile.Tags.ICEBERG_COMPAT_VERSION.name, "0")
-      !icebergCompatVersion.exists(_.toString == icebergCompatVersion)
+      val fileIcebergCompatVersion =
+        file.tags.getOrElse(AddFile.Tags.ICEBERG_COMPAT_VERSION.name, "0")
+      fileIcebergCompatVersion != icebergCompatVersion.toString
     }
     files.filter(shouldRewriteToBeIcebergCompatible)
   }
