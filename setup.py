3f74b7a0df8d4eca84035d3f54ead4c780887b7d
Allison Portis <allison.portis@databricks.com>
2024-08-13 16:48:19 -0700

    clean up python code

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ def get_version_from_sbt():
 VERSION = get_version_from_sbt()
 MAJOR_VERSION = int(VERSION.split(".")[0])
 
-packages_arg = ['delta']
-install_requires_arg = ['pyspark>=3.5.0,<3.6.0', 'importlib_metadata>=1.0.0']
-python_requires_arg = '>=3.6'
-
-# Delta 4.0+ contains Delta Connect code and uses Spark 4.0+
-if MAJOR_VERSION >= 4:
+if MAJOR_VERSION < 4:
+    packages_arg = ['delta']
+    install_requires_arg = ['pyspark>=3.5.0,<3.6.0', 'importlib_metadata>=1.0.0']
+    python_requires_arg = '>=3.6'
+else:  # MAJOR_VERSION >= 4
+    # Delta 4.0+ contains Delta Connect code and uses Spark 4.0+
     packages_arg = ['delta', 'delta.connect', 'delta.connect.proto']
     install_requires_arg = ['pyspark>=4.0.0.dev1', 'importlib_metadata>=1.0.0']
     python_requires_arg = '>=3.9'
