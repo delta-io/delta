b760bc0b9b76930f24360d3943c2e704e755f4e8
Allison Portis <allison.portis@databricks.com>
2024-08-15 17:10:50 -0700

    use encoderFor

--- spark/src/main/scala/org/apache/spark/sql/delta/DeltaUDF.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/DeltaUDF.scala
@@ -19,7 +19,7 @@ package org.apache.spark.sql.delta
 import org.apache.spark.sql.delta.sources.DeltaSQLConf
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.catalyst.encoders.{encoderFor, ExpressionEncoder}
 import org.apache.spark.sql.expressions.{SparkUserDefinedFunction, UserDefinedFunction}
 import org.apache.spark.sql.functions.udf
 
@@ -82,9 +82,8 @@ object DeltaUDF {
       orElse: => UserDefinedFunction): UserDefinedFunction = {
     if (SparkSession.active.sessionState.conf
       .getConf(DeltaSQLConf.INTERNAL_UDF_OPTIMIZATION_ENABLED)) {
-      val inputEncoders = template.inputEncoders
-        .map(_.map(_.asInstanceOf[ExpressionEncoder[_]].copy()))
-      val outputEncoder = template.outputEncoder.map(_.asInstanceOf[ExpressionEncoder[_]].copy())
+      val inputEncoders = template.inputEncoders.map(_.map(e => encoderFor(e)))
+      val outputEncoder = template.outputEncoder.map(e => encoderFor(e))
       template.copy(f = f, inputEncoders = inputEncoders, outputEncoder = outputEncoder)
     } else {
       orElse
