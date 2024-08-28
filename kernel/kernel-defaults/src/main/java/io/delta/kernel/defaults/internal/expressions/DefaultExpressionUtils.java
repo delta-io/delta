b040889fb82224538ca4dbebde78836b4a955724
Marko <marko.ilic@databricks.com>
2024-08-28 18:34:52 +0200

    changed string comparator to use binary comparator

--- kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/expressions/DefaultExpressionUtils.java
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/expressions/DefaultExpressionUtils.java
@@ -35,29 +35,22 @@ import java.util.stream.Collectors;
 class DefaultExpressionUtils {
 
   static final Comparator<BigDecimal> BIGDECIMAL_COMPARATOR = Comparator.naturalOrder();
+  static final Comparator<byte[]> BINARY_COMPARTOR =
+          (leftOp, rightOp) -> {
+            int i = 0;
+            while (i < leftOp.length && i < rightOp.length) {
+              if (leftOp[i] != rightOp[i]) {
+                return Byte.toUnsignedInt(leftOp[i]) - Byte.toUnsignedInt(rightOp[i]);
+              }
+              i++;
+            }
+            return Integer.compare(leftOp.length, rightOp.length);
+          };
   static final Comparator<String> STRING_COMPARATOR =
       (leftOp, rightOp) -> {
         byte[] leftBytes = leftOp.getBytes(StandardCharsets.UTF_8);
         byte[] rightBytes = rightOp.getBytes(StandardCharsets.UTF_8);
-        int i = 0;
-        while (i < leftBytes.length && i < rightBytes.length) {
-          if (leftBytes[i] != rightBytes[i]) {
-            return Byte.toUnsignedInt(leftBytes[i]) - Byte.toUnsignedInt(rightBytes[i]);
-          }
-          i++;
-        }
-        return Integer.compare(leftBytes.length, rightBytes.length);
-      };
-  static final Comparator<byte[]> BINARY_COMPARTOR =
-      (leftOp, rightOp) -> {
-        int i = 0;
-        while (i < leftOp.length && i < rightOp.length) {
-          if (leftOp[i] != rightOp[i]) {
-            return Byte.toUnsignedInt(leftOp[i]) - Byte.toUnsignedInt(rightOp[i]);
-          }
-          i++;
-        }
-        return Integer.compare(leftOp.length, rightOp.length);
+        return BINARY_COMPARTOR.compare(leftBytes, rightBytes);
       };
 
   private DefaultExpressionUtils() {}
