e6cd4935083ef85954944156e46d9d79d8f0eba4
Tulio Cavalcanti <tuliohcavalcanti@gmail.com>
2024-09-09 15:33:24 -0300

    remove unused imports

--- connectors/flink/src/test/java/io/delta/flink/sink/DeltaSinkITCase.java
+++ connectors/flink/src/test/java/io/delta/flink/sink/DeltaSinkITCase.java
@@ -1,8 +1,6 @@
 package io.delta.flink.sink;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
