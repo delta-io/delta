cb24cc57c73f8352cb6471527884e79a5df2e432
Johan Lasperas <johan.lasperas@databricks.com>
2024-10-02 14:01:52 +0200

    Fix char/varchar test

--- spark/src/test/scala/org/apache/spark/sql/delta/typewidening/TypeWideningFeatureCompatibilitySuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/typewidening/TypeWideningFeatureCompatibilitySuite.scala
@@ -124,7 +124,7 @@ trait TypeWideningCompatibilityTests {
   test("compatibility with char/varchar columns") {
     sql(s"CREATE TABLE delta.`$tempPath` (a byte, c char(4), v varchar(4)) USING DELTA")
     append(Seq((1.toByte, "abc", "def")).toDF("a", "c", "v"))
-    checkAnswer(readDeltaTable(tempPath), Seq(Row(1, "abc", "def")))
+    checkAnswer(readDeltaTable(tempPath), Seq(Row(1, "abc ", "def")))
 
     sql(s"ALTER TABLE delta.`$tempPath` CHANGE COLUMN a TYPE smallint")
     append(Seq((2.toShort, "ghi", "jkl")).toDF("a", "c", "v"))
@@ -141,7 +141,7 @@ trait TypeWideningCompatibilityTests {
           metadata = new MetadataBuilder()
             .putString("__CHAR_VARCHAR_TYPE_STRING", "varchar(4)")
             .build()))
-    checkAnswer(readDeltaTable(tempPath), Seq(Row(1, "abc", "def"), Row(2, "ghi", "jkl")))
+    checkAnswer(readDeltaTable(tempPath), Seq(Row(1, "abc ", "def"), Row(2, "ghi ", "jkl")))
 
     sql(s"ALTER TABLE delta.`$tempPath` CHANGE COLUMN c TYPE string")
     sql(s"ALTER TABLE delta.`$tempPath` CHANGE COLUMN v TYPE string")
