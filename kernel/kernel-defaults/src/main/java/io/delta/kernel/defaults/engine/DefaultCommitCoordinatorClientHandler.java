00976a129dc8df1e24d26c17a1a43ea3738d0751
Yuya Ebihara <ebyhry@gmail.com>
2024-10-11 13:45:02 +0900

    Fix typo

--- kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/engine/DefaultCommitCoordinatorClientHandler.java
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/engine/DefaultCommitCoordinatorClientHandler.java
@@ -95,7 +95,7 @@ public class DefaultCommitCoordinatorClientHandler implements CommitCoordinatorC
     //  table identifier into `CommitCoordinatorClient` in all APIs.
     return commitCoordinatorClient.registerTable(
         new Path(logPath),
-        Optional.empty() /* table identfier */,
+        Optional.empty() /* table identifier */,
         currentVersion,
         StorageKernelAPIAdapter.toStorageAbstractMetadata(currentMetadata),
         StorageKernelAPIAdapter.toStorageAbstractProtocol(currentProtocol));
@@ -116,7 +116,7 @@ public class DefaultCommitCoordinatorClientHandler implements CommitCoordinatorC
           commitCoordinatorClient.commit(
               logStore,
               hadoopConf,
-              new TableDescriptor(path, Optional.empty() /* table identfier */, tableConf),
+              new TableDescriptor(path, Optional.empty() /* table identifier */, tableConf),
               commitVersion,
               new Iterator<String>() {
                 @Override
@@ -150,7 +150,7 @@ public class DefaultCommitCoordinatorClientHandler implements CommitCoordinatorC
     Path path = new Path(logPath);
     LogStore logStore = LogStoreProvider.getLogStore(hadoopConf, path.toUri().getScheme());
     TableDescriptor tableDesc =
-        new TableDescriptor(path, Optional.empty() /* table identfier */, tableConf);
+        new TableDescriptor(path, Optional.empty() /* table identifier */, tableConf);
     commitCoordinatorClient.backfillToVersion(
         logStore, hadoopConf, tableDesc, version, lastKnownBackfilledVersion);
   }
