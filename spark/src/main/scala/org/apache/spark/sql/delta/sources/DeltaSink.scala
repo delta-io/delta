b1bf39f423855d937aef6d893c3dd0093eb1d390
Cuong Nguyen <cuong.nguyen@databricks.com>
2024-10-18 15:01:29 -0700

    Add some comments

--- spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSink.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSink.scala
@@ -56,6 +56,8 @@ case class DeltaSink(
     with UpdateExpressionsSupport
     with DeltaLogging {
 
+  // Use the catalog table to get the delta log if available so that the code in delta log,
+  // can differentiate between path-based and name-based table access.
   private val deltaLog = catalogTable match {
     case Some(table) => DeltaLog.forTable(sqlContext.sparkSession, table)
     case None => DeltaLog.forTable(sqlContext.sparkSession, path)
