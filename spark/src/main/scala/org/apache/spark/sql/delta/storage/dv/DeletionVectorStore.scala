d62ebfa45545e9815cdb119445f3642f5ff3ab22
zhipeng.mao <zhipeng.mao@databricks.com>
2024-11-07 17:18:58 +0100

    Update

--- spark/src/main/scala/org/apache/spark/sql/delta/storage/dv/DeletionVectorStore.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/storage/dv/DeletionVectorStore.scala
@@ -24,6 +24,7 @@ import java.util.zip.CRC32
 
 import org.apache.spark.sql.delta.DeltaErrors
 import org.apache.spark.sql.delta.actions.DeletionVectorDescriptor
+import org.apache.spark.sql.delta.commands.DeletionVectorUtils
 import org.apache.spark.sql.delta.deletionvectors.{RoaringBitmapArray, StoredBitmap}
 import org.apache.spark.sql.delta.metering.DeltaLogging
 import org.apache.spark.sql.delta.util.PathWithFileSystem
@@ -197,7 +198,9 @@ class HadoopFileSystemDVStore(hadoopConf: Configuration)
       reader.seek(offset)
       DeletionVectorStore.readRangeFromStream(reader, size)
     }
-    RoaringBitmapArray.readFrom(buffer)
+    DeletionVectorUtils.deserialize(
+      buffer,
+      debugInfo = Map("path" -> path, "offset" -> offset, "size" -> size))
   }
 
   override def createWriter(path: PathWithFileSystem): DeletionVectorStore.Writer = {
