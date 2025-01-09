5fda2c726980156bc8feae7f4f43c4f29c3672bd
Allison Portis <allison.portis@databricks.com>
2025-01-09 10:13:56 -0800

    Fix format

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/metrics/DeltaOperationReportImpl.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/metrics/DeltaOperationReportImpl.java
@@ -1,38 +1,36 @@
 package io.delta.kernel.internal.metrics;
 
-import java.util.Optional;
-import java.util.UUID;
+import static java.util.Objects.requireNonNull;
 
 import io.delta.kernel.metrics.DeltaOperationReport;
-import static java.util.Objects.requireNonNull;
+import java.util.Optional;
+import java.util.UUID;
 
 /** Basic POJO implementation of {@link DeltaOperationReport} */
 public abstract class DeltaOperationReportImpl implements DeltaOperationReport {
 
-    private final String tablePath;
-    private final UUID reportUUID;
-    private final Optional<Exception> exception;
+  private final String tablePath;
+  private final UUID reportUUID;
+  private final Optional<Exception> exception;
 
-    protected DeltaOperationReportImpl(
-        String tablePath,
-        Optional<Exception> exception) {
-        this.tablePath = requireNonNull(tablePath);
-        this.reportUUID = UUID.randomUUID();
-        this.exception = requireNonNull(exception);
-    }
+  protected DeltaOperationReportImpl(String tablePath, Optional<Exception> exception) {
+    this.tablePath = requireNonNull(tablePath);
+    this.reportUUID = UUID.randomUUID();
+    this.exception = requireNonNull(exception);
+  }
 
-    @Override
-    public String getTablePath() {
-        return tablePath;
-    }
+  @Override
+  public String getTablePath() {
+    return tablePath;
+  }
 
-    @Override
-    public UUID getReportUUID() {
-        return reportUUID;
-    }
+  @Override
+  public UUID getReportUUID() {
+    return reportUUID;
+  }
 
-    @Override
-    public Optional<Exception> getException() {
-        return exception;
-    }
+  @Override
+  public Optional<Exception> getException() {
+    return exception;
+  }
 }
