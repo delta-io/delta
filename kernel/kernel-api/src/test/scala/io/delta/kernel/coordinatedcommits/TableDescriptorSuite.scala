4846b7a205c5db8a98ca11709c35fca1fe521e10
Scott Sandre <scott.sandre@databricks.com>
2025-01-14 09:58:22 -0800

    Create TableDescriptorSuite.scala

---
+++ kernel/kernel-api/src/test/scala/io/delta/kernel/coordinatedcommits/TableDescriptorSuite.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright (2024) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.delta.kernel.coordinatedcommits
+
+import org.scalatest.funsuite.AnyFunSuite
+import io.delta.kernel.TableIdentifier
+import java.util.Optional
+
+import scala.collection.JavaConverters._
+
+class TableDescriptorSuite extends AnyFunSuite {
+
+  test("TableDescriptor should throw NullPointerException for null constructor arguments") {
+    assertThrows[NullPointerException] {
+      new TableDescriptor(null, Optional.empty(), Map.empty[String, String].asJava)
+    }
+    assertThrows[NullPointerException] {
+      new TableDescriptor("/delta/logPath", null, Map.empty[String, String].asJava)
+    }
+    assertThrows[NullPointerException] {
+      new TableDescriptor("/delta/logPath", Optional.empty(), null)
+    }
+  }
+
+  test("TableDescriptor should return the correct logPath, tableId, and tableConf") {
+    val logPath = "/delta/logPath"
+    val tableId = Optional.of(new TableIdentifier(Array("catalog", "schema"), "table"))
+    val tableConf = Map("key1" -> "value1", "key2" -> "value2").asJava
+
+    val tableDescriptor = new TableDescriptor(logPath, tableId, tableConf)
+
+    assert(tableDescriptor.getLogPath == logPath)
+    assert(tableDescriptor.getTableId == tableId)
+    assert(tableDescriptor.getTableConf == tableConf)
+  }
+
+  test("TableDescriptors with the same values should be equal") {
+    val logPath = "/delta/logPath"
+    val tableId = Optional.of(new TableIdentifier(Array("catalog", "schema"), "table"))
+    val tableConf = Map("key1" -> "value1", "key2" -> "value2").asJava
+
+    val tableDescriptor1 = new TableDescriptor(logPath, tableId, tableConf)
+    val tableDescriptor2 = new TableDescriptor(logPath, tableId, tableConf)
+
+    assert(tableDescriptor1 == tableDescriptor2)
+    assert(tableDescriptor1.hashCode == tableDescriptor2.hashCode)
+  }
+
+  test("TableDescriptors with different values should not be equal") {
+    val logPath = "/delta/logPath"
+    val tableId = Optional.of(new TableIdentifier(Array("catalog", "schema"), "table"))
+    val tableConf1 = Map("key1" -> "value1").asJava
+    val tableConf2 = Map("key1" -> "value2").asJava
+
+    val tableDescriptor1 = new TableDescriptor(logPath, tableId, tableConf1)
+    val tableDescriptor2 = new TableDescriptor(logPath, tableId, tableConf2)
+
+    assert(tableDescriptor1 != tableDescriptor2)
+  }
+
+  test("TableDescriptor toString format") {
+    val logPath = "/delta/logPath"
+    val tableId = Optional.of(new TableIdentifier(Array("catalog", "schema"), "table"))
+    val tableConf = Map("key1" -> "value1").asJava
+
+    val tableDescriptor = new TableDescriptor(logPath, tableId, tableConf)
+    val expectedString = "TableDescriptor{logPath='/delta/logPath', " +
+      "tableId=Optional[TableIdentifier{catalog.schema.table}], " +
+      "tableConf={key1=value1}}"
+    assert(tableDescriptor.toString == expectedString)
+  }
+}
