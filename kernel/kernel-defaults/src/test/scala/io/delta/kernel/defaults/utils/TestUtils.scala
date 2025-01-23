4122d70d9f17917b00831cfea33a7e3fb8ed77d8
Xin Huang <xin.huang@databricks.com>
2025-01-22 16:04:43 -0800

    clean unused test methods

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/utils/TestUtils.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/utils/TestUtils.scala
@@ -718,8 +718,5 @@ trait TestUtils extends Assertions with SQLHelper {
     }
     resource.getFile
   }
-
-  def copyTable(sourcePath: String, targetPath: String): Unit = {
-    FileUtils.copyDirectory(new File(sourcePath), new File(targetPath))
-  }
+  
 }
