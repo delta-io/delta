7e85686e79ed519beb494b55de0a9bb982d32a8e
Xin Huang <42597328+huan233usc@users.noreply.github.com>
2025-02-20 15:06:40 -0800

    [Kernel]Add simple crc post commit for incremental crc writing. (#4134)
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md
    2. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      3. Be sure to keep the PR description updated to reflect all changes.
      4. Please write your PR title to summarize what this PR proposes.
    5. If possible, provide a concise example to reproduce the issue for a
    faster review.
    6. If applicable, include the corresponding issue number in the PR title
    and link it in the body.
    -->
    
    #### Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [ ] Spark
    - [ ] Standalone
    - [ ] Flink
    - [X] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    
    <!--
    - Describe what this PR changes.
    - Describe why we need the change.
     
    If this PR resolves an issue be sure to include "Resolves #XXX" to
    correctly link and close the issue upon merge.
    -->
    This PR introduces a new post commit hook - ChecksumSimple, for writing
    CRC file after txn commit.
    CRC file will only be written only commit version - 1's snapshot reads
    CRC during state construction
    
    Other case will be handled in a separate PR
    
    ## How was this patch tested?
    
    <!--
    If tests were added, say they were added here. Please make sure to test
    the changes thoroughly including negative and positive cases if
    possible.
    If the changes were tested in any way other than unit tests, please
    clarify how you tested step by step (ideally copy and paste-able, so
    that other reviewers can test and check, and descendants can verify in
    the future).
    If the changes were not tested, please explain why.
    -->
    
    E2e test
    ## Does this PR introduce _any_ user-facing changes?
    
    <!--
    If yes, please clarify the previous behavior and the change this PR
    proposes - provide the console output, description and/or an example to
    show the behavior difference if possible.
    If possible, please also clarify if this is a user-facing change
    compared to the released Delta Lake versions or within the unreleased
    branches such as master.
    If no, write 'No'.
    -->
    No

---
+++ kernel/kernel-api/src/test/scala/io/delta/kernel/internal/checksum/ChecksumWriterSuite.scala
@@ -0,0 +1,197 @@
+/*
+ * Copyright (2025) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.delta.kernel.internal.checksum
+
+import java.util
+import java.util.{Collections, Optional}
+
+import io.delta.kernel.data.Row
+import io.delta.kernel.internal.actions.{Format, Metadata, Protocol}
+import io.delta.kernel.internal.checksum.CRCInfo.CRC_FILE_SCHEMA
+import io.delta.kernel.internal.data.GenericRow
+import io.delta.kernel.internal.fs.Path
+import io.delta.kernel.internal.util.VectorUtils
+import io.delta.kernel.internal.util.VectorUtils.{stringArrayValue, stringStringMapValue}
+import io.delta.kernel.test.{BaseMockJsonHandler, MockEngineUtils}
+import io.delta.kernel.types.StructType
+import io.delta.kernel.utils.CloseableIterator
+
+import org.scalatest.funsuite.AnyFunSuite
+
+/**
+ * Test suite for ChecksumWriter functionality.
+ */
+class ChecksumWriterSuite extends AnyFunSuite with MockEngineUtils {
+
+  private val FAKE_DELTA_LOG_PATH = new Path("/path/to/delta/log")
+
+  // Schema field indices in crc file
+  private val TABLE_SIZE_BYTES_IDX = CRC_FILE_SCHEMA.indexOf("tableSizeBytes")
+  private val NUM_FILES_IDX = CRC_FILE_SCHEMA.indexOf("numFiles")
+  private val NUM_METADATA_IDX = CRC_FILE_SCHEMA.indexOf("numMetadata")
+  private val NUM_PROTOCOL_IDX = CRC_FILE_SCHEMA.indexOf("numProtocol")
+  private val TXN_ID_IDX = CRC_FILE_SCHEMA.indexOf("txnId")
+  private val METADATA_IDX = CRC_FILE_SCHEMA.indexOf("metadata")
+  private val PROTOCOL_IDX = CRC_FILE_SCHEMA.indexOf("protocol")
+
+  test("write checksum") {
+    val jsonHandler = new MockCheckSumFileJsonWriter()
+    val checksumWriter = new ChecksumWriter(FAKE_DELTA_LOG_PATH)
+    val protocol = createTestProtocol()
+    val metadata = createTestMetadata()
+
+    def testChecksumWrite(txn: Optional[String]): Unit = {
+      val version = 1L
+      val tableSizeBytes = 100L
+      val numFiles = 1L
+
+      checksumWriter.writeCheckSum(
+        mockEngine(jsonHandler = jsonHandler),
+        new CRCInfo(version, metadata, protocol, tableSizeBytes, numFiles, txn))
+
+      verifyChecksumFile(jsonHandler, version)
+      verifyChecksumContent(jsonHandler.capturedCrcRow.get, tableSizeBytes, numFiles, txn)
+      verifyMetadataAndProtocol(jsonHandler.capturedCrcRow.get, metadata, protocol)
+    }
+
+    // Test with and without transaction ID
+    testChecksumWrite(Optional.of("txn"))
+    testChecksumWrite(Optional.empty())
+  }
+
+  private def verifyChecksumFile(jsonHandler: MockCheckSumFileJsonWriter, version: Long): Unit = {
+    assert(jsonHandler.checksumFilePath == s"$FAKE_DELTA_LOG_PATH/${"%020d".format(version)}.crc")
+    assert(jsonHandler.capturedCrcRow.isDefined)
+    assert(jsonHandler.capturedCrcRow.get.getSchema == CRC_FILE_SCHEMA)
+  }
+
+  private def verifyChecksumContent(
+      actualCheckSumRow: Row,
+      expectedTableSizeBytes: Long,
+      expectedNumFiles: Long,
+      expectedTxnId: Optional[String]): Unit = {
+    assert(!actualCheckSumRow.isNullAt(TABLE_SIZE_BYTES_IDX) && actualCheckSumRow.getLong(
+      TABLE_SIZE_BYTES_IDX) == expectedTableSizeBytes)
+    assert(!actualCheckSumRow.isNullAt(
+      NUM_FILES_IDX) && actualCheckSumRow.getLong(NUM_FILES_IDX) == expectedNumFiles)
+    assert(!actualCheckSumRow.isNullAt(
+      NUM_METADATA_IDX) && actualCheckSumRow.getLong(NUM_METADATA_IDX) == 1L)
+    assert(!actualCheckSumRow.isNullAt(
+      NUM_PROTOCOL_IDX) && actualCheckSumRow.getLong(NUM_PROTOCOL_IDX) == 1L)
+
+    if (expectedTxnId.isPresent) {
+      assert(actualCheckSumRow.getString(TXN_ID_IDX) == expectedTxnId.get())
+    } else {
+      assert(actualCheckSumRow.isNullAt(TXN_ID_IDX))
+    }
+  }
+
+  private def verifyMetadataAndProtocol(
+      actualRow: Row,
+      expectedMetadata: Metadata,
+      expectedProtocol: Protocol): Unit = {
+    checkMetadata(expectedMetadata, actualRow.getStruct(METADATA_IDX))
+    checkProtocol(expectedProtocol, actualRow.getStruct(PROTOCOL_IDX))
+  }
+
+  // TODO: implement compare in Metadata and remove this method
+  private def checkMetadata(expectedMetadata: Metadata, actualMetadataRow: Row): Unit = {
+    assert(actualMetadataRow.getSchema == Metadata.FULL_SCHEMA)
+
+    def getOptionalString(field: String): Optional[String] =
+      Optional.ofNullable(actualMetadataRow.getString(Metadata.FULL_SCHEMA.indexOf(field)))
+
+    assert(
+      actualMetadataRow.getString(Metadata.FULL_SCHEMA.indexOf("id")) == expectedMetadata.getId)
+    assert(getOptionalString("name") == expectedMetadata.getName)
+    assert(getOptionalString("description") == expectedMetadata.getDescription)
+
+    val formatRow = actualMetadataRow.getStruct(Metadata.FULL_SCHEMA.indexOf("format"))
+    assert(
+      formatRow
+        .getString(
+          Format.FULL_SCHEMA.indexOf("provider")) == expectedMetadata.getFormat.getProvider)
+
+    assert(
+      actualMetadataRow
+        .getString(
+          Metadata.FULL_SCHEMA.indexOf("schemaString")) == expectedMetadata.getSchemaString)
+    assert(
+      actualMetadataRow
+        .getArray(Metadata.FULL_SCHEMA.indexOf("partitionColumns"))
+        == expectedMetadata.getPartitionColumns)
+    assert(
+      Optional
+        .ofNullable(actualMetadataRow.getLong(Metadata.FULL_SCHEMA.indexOf("createdTime")))
+        == expectedMetadata.getCreatedTime)
+    assert(
+      VectorUtils
+        .toJavaMap(actualMetadataRow.getMap(Metadata.FULL_SCHEMA.indexOf("configuration")))
+        == expectedMetadata.getConfiguration)
+  }
+
+  // TODO: implement compare in Protocol and remove this method
+  private def checkProtocol(expectedProtocol: Protocol, actualProtocolRow: Row): Unit = {
+    assert(actualProtocolRow.getSchema == Protocol.FULL_SCHEMA)
+    assert(
+      expectedProtocol.getMinReaderVersion == actualProtocolRow
+        .getInt(Protocol.FULL_SCHEMA.indexOf("minReaderVersion")))
+    assert(
+      expectedProtocol.getMinWriterVersion == actualProtocolRow
+        .getInt(Protocol.FULL_SCHEMA.indexOf("minWriterVersion")))
+  }
+
+  private def createTestMetadata(): Metadata = {
+    new Metadata(
+      "id",
+      Optional.of("name"),
+      Optional.of("description"),
+      new Format("parquet", Collections.emptyMap()),
+      "schemaString",
+      new StructType(),
+      stringArrayValue(util.Arrays.asList("c3")),
+      Optional.of(123),
+      stringStringMapValue(new util.HashMap[String, String]() {
+        put("delta.appendOnly", "true")
+      }))
+  }
+
+  private def createTestProtocol(): Protocol = {
+    new Protocol(
+      /* minReaderVersion= */ 1,
+      /* minWriterVersion= */ 2,
+      Collections.emptySet(),
+      Collections.emptySet())
+  }
+}
+
+/**
+ * Mock implementation of JsonHandler for testing checksum file writing.
+ */
+class MockCheckSumFileJsonWriter extends BaseMockJsonHandler {
+  var capturedCrcRow: Option[Row] = None
+  var checksumFilePath: String = ""
+
+  override def writeJsonFileAtomically(
+      filePath: String,
+      data: CloseableIterator[Row],
+      overwrite: Boolean): Unit = {
+    checksumFilePath = filePath
+    assert(data.hasNext, "Expected data iterator to contain exactly one row")
+    capturedCrcRow = Some(data.next())
+    assert(!data.hasNext, "Expected data iterator to contain exactly one row")
+  }
+}
--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DeltaTableWritesSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DeltaTableWritesSuite.scala
@@ -45,6 +45,7 @@ import io.delta.kernel.types.TimestampType.TIMESTAMP
 import io.delta.kernel.utils.CloseableIterable
 import io.delta.kernel.utils.CloseableIterable.{emptyIterable, inMemoryIterable}
 
+/** Transaction commit in this suite IS REQUIRED TO use commitTransaction than .commit */
 class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBase {
 
   ///////////////////////////////////////////////////////////////////////////
@@ -96,7 +97,7 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
       val txnBuilder = table.createTransactionBuilder(engine, testEngineInfo, CREATE_TABLE)
 
       val txn = txnBuilder.withSchema(engine, testSchema).build(engine)
-      txn.commit(engine, emptyIterable())
+      commitTransaction(txn, engine, emptyIterable())
 
       {
         val ex = intercept[TableAlreadyExistsException] {
@@ -130,7 +131,7 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
 
       assert(txn.getSchema(engine) === testSchema)
       assert(txn.getPartitionColumns(engine) === Seq.empty.asJava)
-      val txnResult = txn.commit(engine, emptyIterable())
+      val txnResult = commitTransaction(txn, engine, emptyIterable())
 
       assert(txnResult.getVersion === 0)
       assertCheckpointReadiness(txnResult, isReadyForCheckpoint = false)
@@ -346,7 +347,7 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
       assert(txn.getSchema(engine) === schema)
       // Expect the partition column name is exactly same as the one in the schema
       assert(txn.getPartitionColumns(engine) === Seq("Part1", "part2").asJava)
-      val txnResult = txn.commit(engine, emptyIterable())
+      val txnResult = commitTransaction(txn, engine, emptyIterable())
 
       assert(txnResult.getVersion === 0)
       assertCheckpointReadiness(txnResult, isReadyForCheckpoint = false)
@@ -364,7 +365,7 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
       val table = Table.forPath(engine, tablePath)
       val txnBuilder = table.createTransactionBuilder(engine, testEngineInfo, CREATE_TABLE)
       val txn = txnBuilder.withSchema(engine, schema).build(engine)
-      val txnResult = txn.commit(engine, emptyIterable())
+      val txnResult = commitTransaction(txn, engine, emptyIterable())
 
       assert(txnResult.getVersion === 0)
       assertCheckpointReadiness(txnResult, isReadyForCheckpoint = false)
@@ -434,12 +435,12 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
       val stagedFiles = stageData(txnState, Map.empty, dataBatches1)
 
       val stagedActionsIterable = inMemoryIterable(stagedFiles)
-      val commitResult = txn.commit(engine, stagedActionsIterable)
+      val commitResult = commitTransaction(txn, engine, stagedActionsIterable)
       assert(commitResult.getVersion == 0)
 
       // try to commit the same transaction and expect failure
       val ex = intercept[IllegalStateException] {
-        txn.commit(engine, stagedActionsIterable)
+        commitTransaction(txn, engine, stagedActionsIterable)
       }
       assert(ex.getMessage.contains(
         "Transaction is already attempted to commit. Create a new transaction."))
@@ -561,7 +562,10 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
         // partition cols are not written in the commit info for inserts
         val partitionBy = if (i == 0) expPartCols else null
         verifyCommitInfo(tblPath, version = i, partitionBy, operation = WRITE)
-        verifyWrittenContent(tblPath, schema, if (i == 0) expV0Data else expV0Data ++ expV1Data)
+        verifyWrittenContent(
+          tblPath,
+          schema,
+          if (i == 0) expV0Data else expV0Data ++ expV1Data)
       }
     }
   }
@@ -794,6 +798,8 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
 
   test("insert into table - idempotent writes") {
     withTempDirAndEngine { (tblPath, engine) =>
+      // TODO: re-enable when CRC_FULL post commit hook is added, txn2 requires CRC_FULL
+      assume(this.suiteName != ("DeltaTableWriteWithCrcSuite"))
       val data = Seq(Map("part1" -> ofInt(1), "part2" -> ofInt(2)) -> dataPartitionBatches1)
       var expData = Seq.empty[TestRow] // as the data in inserted, update this.
 
@@ -822,7 +828,7 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
           txn: Transaction,
           actions: CloseableIterable[Row],
           expTblVer: Long): Unit = {
-        val commitResult = txn.commit(engine, actions)
+        val commitResult = commitTransaction(txn, engine, actions)
 
         expData = expData ++ data.flatMap(_._2).flatMap(_.toTestRows)
 
@@ -891,6 +897,8 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
 
   test("conflicts - creating new table - table created by other txn after current txn start") {
     withTempDirAndEngine { (tablePath, engine) =>
+      // TODO: re-enable when CRC_FULL post commit hook is added
+      assume(this.suiteName != ("DeltaTableWriteWithCrcSuite"))
       val losingTx = createTestTxn(engine, tablePath, Some(testSchema))
 
       // don't commit losingTxn, instead create a new txn and commit it
@@ -939,6 +947,8 @@ class DeltaTableWritesSuite extends DeltaTableWriteSuiteBase with ParquetSuiteBa
   // Different scenarios that have multiple winning txns and with a checkpoint in between.
   Seq(1, 5, 12).foreach { numWinningTxs =>
     test(s"conflicts - concurrent data append ($numWinningTxs) after the losing txn has started") {
+      // TODO: re-enable when CRC_FULL post commit hook is added
+      assume(this.suiteName != ("DeltaTableWriteWithCrcSuite"))
       withTempDirAndEngine { (tablePath, engine) =>
         val testData = Seq(Map.empty[String, Literal] -> dataBatches1)
         var expData = Seq.empty[TestRow]
