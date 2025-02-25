8cb315b85bf275d5fe7f58c41eb6ea074d72bf01
Venki Korukanti <venkateshwar.korukanti@databricks.com>
2025-02-24 16:41:39 -0800

    wip

---
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/engine/io/FileIO.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (2025) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.delta.kernel.defaults.engine.io;
+
+import io.delta.kernel.engine.FileReadRequest;
+import io.delta.kernel.utils.CloseableIterator;
+import io.delta.kernel.utils.FileStatus;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Optional;
+
+public interface FileIO
+{
+    /**
+     * List the paths in the same directory that are lexicographically greater or equal to (UTF-8
+     * sorting) the given `path`. The result should also be sorted by the file name.
+     *
+     * @param filePath Fully qualified path to a file
+     * @return Closeable iterator of files. It is the responsibility of the caller to close the
+     *     iterator.
+     * @throws FileNotFoundException if the file at the given path is not found
+     * @throws IOException for any other IO error.
+     */
+    CloseableIterator<FileStatus> listFrom(String filePath) throws IOException;
+
+    /**
+     * Resolve the given path to a fully qualified path.
+     *
+     * @param path Input path
+     * @return Fully qualified path.
+     * @throws FileNotFoundException If the given path doesn't exist.
+     * @throws IOException for any other IO error.
+     */
+    String resolvePath(String path) throws IOException;
+
+    /**
+     * Return an iterator of byte streams one for each read request in {@code readRequests}. The
+     * returned streams are in the same order as the given {@link FileReadRequest}s. It is the
+     * responsibility of the caller to close each returned stream.
+     *
+     * @param readRequests Iterator of read requests
+     * @return Data for each request as one {@link ByteArrayInputStream}.
+     * @throws IOException
+     */
+    CloseableIterator<ByteArrayInputStream> readFiles(CloseableIterator<FileReadRequest> readRequests)
+            throws IOException;
+
+    /**
+     * Create a directory at the given path including parent directories. This mimics the behavior of
+     * `mkdir -p` in Unix.
+     *
+     * @param path Full qualified path to create a directory at.
+     * @return true if the directory was created successfully, false otherwise.
+     * @throws IOException for any IO error.
+     */
+    boolean mkdirs(String path) throws IOException;
+
+    /**
+     * Open a {@link SeekableInputStream} at the given path which can be used to read
+     * the file from any arbitrary position in the file.
+     *
+     * @param path Fully qualified path to the file.
+     * @return {@link SeekableInputStream} instance.
+     * @throws IOException for any IO error.
+     */
+    SeekableInputStream open(String path) throws IOException;
+
+    /**
+     * Create a new file at the given path. If the file already exists , either at
+     * the time of creating the {@link OutputStream} or at the time of closing it,
+     * and {@code putIfAbsent} is true, then the file will not be created and an
+     * {@link java.nio.file.FileAlreadyExistsException} will be thrown. If
+     *
+     * @param path Fully qualified path to the file.
+     * @return {@link OutputStream} instance to write to the file.
+     * @throws IOException for any IO error.
+     */
+    OutputStream create(String path, boolean putIfAbsent) throws IOException;
+
+
+    /**
+     * Delete the file at given path.
+     *
+     * @param path the path to delete. If path is a directory throws an exception.
+     * @return true if delete is successful else false.
+     * @throws IOException for any IO error.
+     */
+    boolean delete(String path) throws IOException;
+
+    /**
+     * Get the configuration value for the given key.
+     * @param confKey configuration key name
+     * @return If no such value is present, an {@link Optional#empty()} is returned.
+     */
+    Optional<String> getConf(String confKey);
+}
