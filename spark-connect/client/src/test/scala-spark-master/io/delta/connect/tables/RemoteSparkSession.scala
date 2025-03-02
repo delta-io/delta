1441c43c14b37412d479805b58e5d5765b6eeffe
hvanhovell <herman@databricks.com>
2025-03-01 21:46:04 -0400

    Fix connect test compilation. Disable them for now.

--- spark-connect/client/src/test/scala-spark-master/io/delta/connect/tables/RemoteSparkSession.scala
+++ spark-connect/client/src/test/scala-spark-master/io/delta/connect/tables/RemoteSparkSession.scala
@@ -40,7 +40,7 @@ package io.delta.tables
 
 import org.scalatest.{BeforeAndAfterAll, Suite}
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.connect.SparkSession
 
 /**
  * An util class to start a local Delta Connect server in a different process for local E2E tests.
@@ -69,17 +69,14 @@ trait RemoteSparkSession extends BeforeAndAfterAll { self: Suite =>
   // TODO: Instead of hard-coding the path, use the findJar function from
   // Apache Spark's IntegrationTestUtils.scala.
   private val deltaConnectJar = s"$buildLocation/" +
-    "spark-connect/server/target/scala-2.13/delta-connect-server-assembly-3.3.0-SNAPSHOT.jar"
-
+    "spark-connect/server/target/scala-2.13/delta-connect-server-assembly-3.4.0-SNAPSHOT.jar"
   private val resources = s"$buildLocation/spark-connect/client/target/scala-2.13/resource_managed/test"
-  private val sparkConnectJar = s"$resources/spark-connect.jar"
-  private val sparkSubmit = s"$resources/spark/spark-4.0.0-preview1-bin-hadoop3/sbin/start-connect-server.sh"
+  private val sparkSubmit = s"$resources/spark/spark-4.0.0-preview1-bin-hadoop3/sbin/start-connect-server.sh" // HUH
 
   private lazy val server = {
     val command = Seq.newBuilder[String]
     command += sparkSubmit
-    command += "--driver-class-path" += s"$sparkConnectJar:$deltaConnectJar"
-    command += "--jars" += sparkConnectJar
+    command += "--driver-class-path" += deltaConnectJar
     command += "--class" += "io.delta.tables.SimpleDeltaConnectService"
     command += "--conf" += s"spark.connect.grpc.binding.port=$serverPort"
     command += "--conf" += "spark.connect.extensions.relation.classes=" +
