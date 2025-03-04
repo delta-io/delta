bd80e0862da2812c1a1e655e5380a9afc4f7a962
andreaschat-db <andreas.chatzistergiou@databricks.com>
2025-03-04 19:01:24 +0100

    v1

--- spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSource.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSource.scala
@@ -1412,9 +1412,18 @@ object DeltaSource extends DeltaLogging {
       deltaLog.getSnapshotAt(version)
       return true
     } catch {
-      case e: DeltaUnsupportedTableFeatureException => throw e
+      case e: DeltaUnsupportedTableFeatureException =>
+        recordDeltaEvent(
+          deltaLog = deltaLog,
+          opType = "dropFeature.validateProtocolAt.unsupportedFeatureFound",
+          data = Map("message" -> e.getMessage))
+        throw e
       case NonFatal(e) => // Suppress rest errors.
         logWarning(log"Protocol validation failed with '${MDC(DeltaLogKeys.EXCEPTION, e)}'.")
+        recordDeltaEvent(
+          deltaLog = deltaLog,
+          opType = "dropFeature.validateProtocolAt.error",
+          data = Map("message" -> e.getMessage))
     }
     false
   }
