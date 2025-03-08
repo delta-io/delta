64484fa640c727f243f77e5121e82a058293fed8
Oussama Saoudi <oussama.saoudi@databricks.com>
2025-03-07 20:43:08 -0800

    Add getVersion method to Transaction

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/TransactionImpl.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/TransactionImpl.java
@@ -126,6 +126,11 @@ public class TransactionImpl implements Transaction {
     return readSnapshot.getSchema();
   }
 
+  @Override
+  public long getVersion() {
+    return readSnapshot.getVersion()
+  }
+
   public Optional<SetTransaction> getSetTxnOpt() {
     return setTxnOpt;
   }
