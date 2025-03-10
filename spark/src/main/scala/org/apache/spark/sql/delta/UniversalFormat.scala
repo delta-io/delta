8b1422c8348407e8e1f7ad5ff349f983921035b3
Fred Storage Liu <fred.liu@databricks.com>
2025-03-10 10:35:53 -0700

    impl

--- spark/src/main/scala/org/apache/spark/sql/delta/UniversalFormat.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/UniversalFormat.scala
@@ -263,9 +263,8 @@ object UniversalFormat extends DeltaLogging {
   val ICEBERG_TABLE_TYPE_KEY = "table_type"
 
   /**
-   * Update CatalogTable to mark it readable by other table readers (iceberg for now).
-   * This method ensures 'table_type' = 'ICEBERG' when uniform is enabled,
-   * and ensure table_type is not 'ICEBERG' when uniform is not enabled
+   * HiveTableOperations ensures table_type is 'ICEBERG' when uniform is enabled
+   * This enforceSupportInCatalog ensure table_type is not 'ICEBERG' when uniform is not enabled
    * If the key has other values than 'ICEBERG', this method will not touch it for compatibility
    *
    * @param table    catalogTable before change
@@ -279,9 +278,6 @@ object UniversalFormat extends DeltaLogging {
     }
 
     (icebergEnabled(metadata), icebergInCatalog) match {
-      case (true, false) =>
-        Some(table.copy(properties = table.properties
-          + (ICEBERG_TABLE_TYPE_KEY -> ICEBERG_FORMAT)))
       case (false, true) =>
         Some(table.copy(properties =
           table.properties - ICEBERG_TABLE_TYPE_KEY))
