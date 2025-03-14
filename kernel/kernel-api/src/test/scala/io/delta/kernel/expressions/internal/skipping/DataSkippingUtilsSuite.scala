f4e13166945bdca943d0df0537293d21335fdcd5
ilicmarkodb <marko.ilic@databricks.com>
2025-03-14 23:04:30 +0100

    temp

--- kernel/kernel-api/src/test/scala/io/delta/kernel/expressions/internal/skipping/DataSkippingUtilsSuite.scala
+++ kernel/kernel-api/src/test/scala/io/delta/kernel/expressions/internal/skipping/DataSkippingUtilsSuite.scala
@@ -1,18 +1,33 @@
 package io.delta.kernel.expressions.internal.skipping
 
-import io.delta.kernel.expressions.{And, Column, Literal, Predicate}
-import io.delta.kernel.internal.skipping.DataSkippingPredicate
+import io.delta.kernel.expressions.{And, Column, Expression, Literal, Predicate}
+import io.delta.kernel.internal.skipping.{CollatedDataSkippingPredicate, DataSkippingPredicate}
 import io.delta.kernel.internal.skipping.DataSkippingUtils.constructDataSkippingFilter
-import io.delta.kernel.types.{StringType, StructType}
-import jdk.nashorn.internal.runtime.PrototypeObject.getConstructor
+import io.delta.kernel.types.{CollationIdentifier, StringType, StructType}
 import org.scalatest.funsuite.AnyFunSuite
 
+import java.util
 import java.util.Optional
+import scala.collection.JavaConverters.{mapAsJavaMapConverter, seqAsJavaListConverter, setAsJavaSetConverter}
 
 class DataSkippingUtilsSuite extends AnyFunSuite {
   val MIN = "minValues"
   val MAX = "maxValues"
-  val constructor = DataSkippingPredicate.class.getConstructor()
+  val dataSkippingPredicateConstructor =
+    classOf[DataSkippingPredicate].getDeclaredConstructor(
+      classOf[String],
+      classOf[util.List[Expression]],
+      classOf[util.Set[Column]],
+      classOf[util.Map[CollationIdentifier, util.Set[Column]]])
+  val collatedDataSkippingPredicateConstructor =
+    classOf[CollatedDataSkippingPredicate].getDeclaredConstructor(
+      classOf[String],
+      classOf[util.List[Expression]],
+      classOf[CollationIdentifier],
+      classOf[util.Set[Column]],
+      classOf[util.Map[CollationIdentifier, util.Set[Column]]])
+  dataSkippingPredicateConstructor.setAccessible(true)
+  collatedDataSkippingPredicateConstructor.setAccessible(true)
 
   test("constructDataSkippingFilter") {
     Seq(
@@ -26,14 +41,19 @@ class DataSkippingUtilsSuite extends AnyFunSuite {
         new StructType().add("a", StringType.STRING),
         new Predicate("<", new Column("a"), Literal.ofString("a")),
         Optional.of(
-          new DataSkippingPredicate("<", new Column(Array(MIN, "a")), Literal.ofString("a")))
+          dataSkippingPredicateConstructor.newInstance(
+            "<",
+            List[Expression](new Column(Array(MIN, "a")), Literal.ofString("a")).asJava,
+            Set[Column](new Column("a")).asJava,
+            Map.empty[CollationIdentifier, java.util.Set[Column]].asJava))
       )
     ).foreach {
       case (schema, predicate, expectedDataSkippingPredicate) =>
         val actualDataSkippingPredicate = constructDataSkippingFilter(predicate, schema)
         assert(actualDataSkippingPredicate.isPresent == expectedDataSkippingPredicate.isPresent)
         if (actualDataSkippingPredicate.isPresent) {
-          assert(actualDataSkippingPredicate.get == expectedDataSkippingPredicate.get)
+          assert(actualDataSkippingPredicate.get.toString
+            == expectedDataSkippingPredicate.get.toString)
         }
     }
   }
