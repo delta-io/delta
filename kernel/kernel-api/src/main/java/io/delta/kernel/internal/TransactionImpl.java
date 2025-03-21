727aa29776340ab415e410e7e6a8dc409b6b95db
Venki Korukanti <venkateshwar.korukanti@databricks.com>
2025-03-21 16:38:44 -0700

    fix the ordering of the checks

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/TransactionImpl.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/TransactionImpl.java
@@ -202,17 +202,19 @@ public class TransactionImpl implements Transaction {
    *     transaction.
    */
   public List<DomainMetadata> getDomainMetadatas() {
-    if (domainMetadatasAdded.isEmpty() && domainMetadatasRemoved.isEmpty()) {
-      // If no domain metadatas are added or removed, return an empty list. This is to avoid
-      // unnecessary loading of the domain metadatas from the snapshot (which is an expensive
-      // operation).
-      return Collections.emptyList();
-    }
-
     // If we have already processed the domain metadatas, then return the list.
     if (domainMetadatas.isPresent()) {
       return domainMetadatas.get();
     }
+
+    if (domainMetadatasAdded.isEmpty() && domainMetadatasRemoved.isEmpty()) {
+      // If no domain metadatas are added or removed, return an empty list. This is to avoid
+      // unnecessary loading of the domain metadatas from the snapshot (which is an expensive
+      // operation).
+      domainMetadatas = Optional.of(Collections.emptyList());
+      return Collections.emptyList();
+    }
+
     // Add all domain metadatas added in the transaction
     List<DomainMetadata> finalDomainMetadatas = new ArrayList<>(domainMetadatasAdded.values());
 
