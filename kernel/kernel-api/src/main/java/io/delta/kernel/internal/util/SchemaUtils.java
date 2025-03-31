1517688f7047a623aee3152261b898783f9596dc
Amogh Jahagirdar <amoghj@apache.org>
2025-03-31 15:59:37 -0600

    [Kernel] Validate updated schema compatibility (#4335)
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md
    2. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      3. Be sure to keep the PR description updated to reflect all changes.
      4. Please write your PR title to summarize what this PR proposes.
    5. If possible, provide a concise example to reproduce the issue for a
    faster review.
    6. If applicable, include the corresponding issue number in the PR title
    and link it in the body.
    -->
    
    #### Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [ ] Spark
    - [ ] Standalone
    - [ ] Flink
    - [x] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    
    This change is broken away from #4196 as part of schema evolution
    support. This change adds validation that the updated schema is
    compatible with the previous schema. Compatibility includes:
    1. making sure no new non-nullable fields are added
    2. no existing fields are tightened in nullability and no invalid type
    changes have occurred.
    
    Note: For now, "invalid type changes" includes any type change but we
    can later extend this to support whatever type promotion is desired.
    
    This change also cleans up the following:
    
    1. Removing some duplication in the SchemaUtils validateUpdatedSchema
    suite
    2. Removing usage of Longs for field IDs, they will always be 32 bit
    integers.
    
    ## How was this patch tested?
    
    Added unit tests
    
    ## Does this PR introduce _any_ user-facing changes?
    
    No

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/util/SchemaUtils.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/util/SchemaUtils.java
@@ -19,6 +19,7 @@ import static io.delta.kernel.internal.DeltaErrors.*;
 import static io.delta.kernel.internal.util.ColumnMapping.*;
 import static io.delta.kernel.internal.util.Preconditions.checkArgument;
 
+import io.delta.kernel.exceptions.KernelException;
 import io.delta.kernel.expressions.Column;
 import io.delta.kernel.expressions.Literal;
 import io.delta.kernel.internal.DeltaErrors;
@@ -336,9 +337,10 @@ public class SchemaUtils {
 
   /* Compute the SchemaChanges using field IDs */
   static SchemaChanges computeSchemaChangesById(
-      Map<Long, StructField> currentFieldIdToField, Map<Long, StructField> updatedFieldIdToField) {
+      Map<Integer, StructField> currentFieldIdToField,
+      Map<Integer, StructField> updatedFieldIdToField) {
     SchemaChanges.Builder schemaDiff = SchemaChanges.builder();
-    for (Map.Entry<Long, StructField> fieldInUpdatedSchema : updatedFieldIdToField.entrySet()) {
+    for (Map.Entry<Integer, StructField> fieldInUpdatedSchema : updatedFieldIdToField.entrySet()) {
       StructField existingField = currentFieldIdToField.get(fieldInUpdatedSchema.getKey());
       StructField updatedField = fieldInUpdatedSchema.getValue();
       // New field added
@@ -350,7 +352,7 @@ public class SchemaUtils {
       }
     }
 
-    for (Map.Entry<Long, StructField> entry : currentFieldIdToField.entrySet()) {
+    for (Map.Entry<Integer, StructField> entry : currentFieldIdToField.entrySet()) {
       if (!updatedFieldIdToField.containsKey(entry.getKey())) {
         schemaDiff.withRemovedField(entry.getValue());
       }
@@ -384,7 +386,7 @@ public class SchemaUtils {
     switch (columnMappingMode) {
       case ID:
       case NAME:
-        validateSchemaEvolutionById(currentSchema, newSchema, metadata);
+        validateSchemaEvolutionById(currentSchema, newSchema);
         return;
       case NONE:
         throw new UnsupportedOperationException(
@@ -395,36 +397,106 @@ public class SchemaUtils {
     }
   }
 
-  /* Validates a given schema evolution by using field ID
-  as the source of truth for identifying fields */
-  private static void validateSchemaEvolutionById(
-      StructType currentSchema, StructType newSchema, Metadata metadata) {
-    Map<Long, StructField> currentFieldsById = fieldsById(currentSchema);
-    Map<Long, StructField> updatedFieldsById = fieldsById(newSchema);
+  /**
+   * Validates a given schema evolution by using field ID as the source of truth for identifying
+   * fields
+   */
+  private static void validateSchemaEvolutionById(StructType currentSchema, StructType newSchema) {
+    Map<Integer, StructField> currentFieldsById = fieldsById(currentSchema);
+    Map<Integer, StructField> updatedFieldsById = fieldsById(newSchema);
     SchemaChanges schemaChanges = computeSchemaChangesById(currentFieldsById, updatedFieldsById);
-
-    // Validate physical name consistency on the updated fields
     validatePhysicalNameConsistency(schemaChanges.updatedFields());
-
-    // ToDo Validate IcebergCompatV2 nested IDs
-
-    // ToDo: Validate schema compatibility
+    // Validates that the updated schema does not contain breaking changes in terms of types and
+    // nullability
+    validateUpdatedSchemaCompatibility(schemaChanges);
+    // ToDo Potentially validate IcebergCompatV2 nested IDs
   }
 
-  /** Returns a map from field ID to the field in the schema */
-  private static Map<Long, StructField> fieldsById(StructType schema) {
+  /**
+   * Verifies that no non-nullable fields are added, no existing field nullability is tightened and
+   * no invalid type changes are performed
+   *
+   * <p>ToDo: Prevent moving fields outside of their containing struct
+   */
+  private static void validateUpdatedSchemaCompatibility(SchemaChanges schemaChanges) {
+    for (StructField addedField : schemaChanges.addedFields()) {
+      if (!addedField.isNullable()) {
+        throw new KernelException(
+            String.format("Cannot add non-nullable field %s", addedField.getName()));
+      }
+    }
+
+    for (Tuple2<StructField, StructField> updatedFields : schemaChanges.updatedFields()) {
+      // ToDo: See if recursion can be avoided by incorporating map key/value and array element
+      // updates in updatedFields
+      validateFieldCompatibility(updatedFields._1, updatedFields._2);
+    }
+  }
+
+  /**
+   * Validate that there was no change in type from existing field from new field, excluding
+   * modified, dropped, or added fields to structs. Validates that a field's nullability is not
+   * tightened
+   */
+  private static void validateFieldCompatibility(StructField existingField, StructField newField) {
+    if (existingField.isNullable() && !newField.isNullable()) {
+      throw new KernelException(
+          String.format(
+              "Cannot tighten the nullability of existing field %s", existingField.getName()));
+    }
+
+    // Both fields are structs, ensure there's no changes in the individual fields
+    // ToDo: Prevent additions, removals,
+    //  and type updates to struct fields when the struct is a map key
+    if (existingField.getDataType() instanceof StructType
+        && newField.getDataType() instanceof StructType) {
+      StructType existingStruct = (StructType) existingField.getDataType();
+      StructType newStruct = (StructType) newField.getDataType();
+      Map<Integer, StructField> existingNestedFields =
+          existingStruct.fields().stream()
+              .collect(Collectors.toMap(ColumnMapping::getColumnId, Function.identity()));
+
+      for (StructField newNestedField : newStruct.fields()) {
+        StructField existingNestedField = existingNestedFields.get(getColumnId(newNestedField));
+        if (existingNestedField != null) {
+          validateFieldCompatibility(existingNestedField, newNestedField);
+        }
+      }
+    } else if (existingField.getDataType() instanceof MapType
+        && newField.getDataType() instanceof MapType) {
+      MapType existingMapType = (MapType) existingField.getDataType();
+      MapType newMapType = (MapType) newField.getDataType();
+
+      validateFieldCompatibility(existingMapType.getKeyField(), newMapType.getKeyField());
+      validateFieldCompatibility(existingMapType.getValueField(), newMapType.getValueField());
+    } else if (existingField.getDataType() instanceof ArrayType
+        && newField.getDataType() instanceof ArrayType) {
+      ArrayType existingArrayType = (ArrayType) existingField.getDataType();
+      ArrayType newArrayType = (ArrayType) newField.getDataType();
+
+      validateFieldCompatibility(
+          existingArrayType.getElementField(), newArrayType.getElementField());
+    } else if (!existingField.getDataType().equivalent(newField.getDataType())) {
+      throw new KernelException(
+          String.format(
+              "Cannot change the type of existing field %s from %s to %s",
+              existingField.getName(), existingField.getDataType(), newField.getDataType()));
+    }
+  }
+
+  private static Map<Integer, StructField> fieldsById(StructType schema) {
     List<Tuple2<List<String>, StructField>> columnPathToStructField =
         filterRecursively(
             schema,
             true /* recurseIntoMapOrArrayElements */,
             false /* stopOnFirstMatch */,
             sf -> true);
-    Map<Long, StructField> columnIdToField = new HashMap<>();
+    Map<Integer, StructField> columnIdToField = new HashMap<>();
     for (Tuple2<List<String>, StructField> pathAndField : columnPathToStructField) {
       StructField field = pathAndField._2;
       checkArgument(hasColumnId(field), "Field %s is missing column id", field.getName());
       checkArgument(hasPhysicalName(field), "Field %s is missing physical name", field.getName());
-      long columnId = field.getMetadata().getLong(COLUMN_MAPPING_ID_KEY);
+      int columnId = getColumnId(field);
       checkArgument(
           !columnIdToField.containsKey(columnId),
           "Field %s with id %d already exists",
