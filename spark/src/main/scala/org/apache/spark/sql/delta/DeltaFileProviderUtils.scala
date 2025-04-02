5edfd7ce998fb4691855c0eed3f628e2a44afa98
Zihao Xu <xzhseh@gmail.com>
2025-04-02 00:47:33 +0000

    Extends DeltaLogging in DeltaFileProviderUtils

--- spark/src/main/scala/org/apache/spark/sql/delta/DeltaFileProviderUtils.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/DeltaFileProviderUtils.scala
@@ -30,7 +30,7 @@ import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.unsafe.types.UTF8String
 
-object DeltaFileProviderUtils {
+object DeltaFileProviderUtils extends DeltaLogging {
 
   protected def readThreadPool = SnapshotManagement.deltaLogAsyncUpdateThreadPool
 
