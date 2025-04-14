466635237872843c0467b76a89e941cf2c8273dd
Xin Huang <42597328+huan233usc@users.noreply.github.com>
2025-04-14 15:49:20 -0700

    [Kernel]Add domain metadata to CRC Info and implements the toRow()  (#4407)
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md
    2. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      3. Be sure to keep the PR description updated to reflect all changes.
      4. Please write your PR title to summarize what this PR proposes.
    5. If possible, provide a concise example to reproduce the issue for a
    faster review.
    6. If applicable, include the corresponding issue number in the PR title
    and link it in the body.
    -->
    
    #### Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [ ] Spark
    - [ ] Standalone
    - [ ] Flink
    - [x] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    
    <!--
    - Describe what this PR changes.
    - Describe why we need the change.
     
    If this PR resolves an issue be sure to include "Resolves #XXX" to
    correctly link and close the issue upon merge.
    -->
    
    This is 1/n for domain metadata in crc support, it adds domain metadata
    to crcInfo.java and implements the toRow() method.
    
    (see https://github.com/delta-io/delta/pull/4330/files for e2e poc)
    
    Follow up will be: (1)e2e integration to populate domain metadata in crc
    in a txn (2)load domain metadata from crc and use it for building
    snapshot.
    
    ## How was this patch tested?
    
    <!--
    If tests were added, say they were added here. Please make sure to test
    the changes thoroughly including negative and positive cases if
    possible.
    If the changes were tested in any way other than unit tests, please
    clarify how you tested step by step (ideally copy and paste-able, so
    that other reviewers can test and check, and descendants can verify in
    the future).
    If the changes were not tested, please explain why.
    -->
    Unit test
    ## Does this PR introduce _any_ user-facing changes?
    
    <!--
    If yes, please clarify the previous behavior and the change this PR
    proposes - provide the console output, description and/or an example to
    show the behavior difference if possible.
    If possible, please also clarify if this is a user-facing change
    compared to the released Delta Lake versions or within the unreleased
    branches such as master.
    If no, write 'No'.
    -->
    No

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/TransactionImpl.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/TransactionImpl.java
@@ -596,6 +596,7 @@ public class TransactionImpl implements Transaction {
               metricsResult.getTotalAddFilesSizeInBytes(),
               metricsResult.getNumAddFiles(),
               Optional.of(txnId.toString()),
+              Optional.empty(), // TODO: populate domain metadata
               metricsResult
                   .getTableFileSizeHistogram()
                   .map(FileSizeHistogram::fromFileSizeHistogramResult)));
@@ -617,6 +618,7 @@ public class TransactionImpl implements Transaction {
                         + metricsResult.getNumAddFiles()
                         - metricsResult.getNumRemoveFiles(),
                     Optional.of(txnId.toString()),
+                    Optional.empty(), // TODO: populate domain metadata
                     metricsResult
                         .getTableFileSizeHistogram()
                         .map(FileSizeHistogram::fromFileSizeHistogramResult)));
