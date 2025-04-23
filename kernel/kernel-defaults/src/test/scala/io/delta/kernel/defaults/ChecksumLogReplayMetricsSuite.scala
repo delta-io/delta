a97bcb31bc5ece7f418f71f58ad06732c9486475
Xin Huang <xin.huang@databricks.com>
2025-04-22 17:07:03 -0700

    fix comment

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/ChecksumLogReplayMetricsSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/ChecksumLogReplayMetricsSuite.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.delta.kernel.defaults
 import java.io.File
 import java.nio.file.Files
