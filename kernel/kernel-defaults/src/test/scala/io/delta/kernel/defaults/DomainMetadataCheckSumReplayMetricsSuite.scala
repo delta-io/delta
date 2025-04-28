37bac1d900e2282011a7c6cd0a69b83c0d008a69
Xin Huang <xin.huang@databricks.com>
2025-04-28 13:50:43 -0700

    address comments

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DomainMetadataCheckSumReplayMetricsSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DomainMetadataCheckSumReplayMetricsSuite.scala
@@ -56,13 +56,12 @@ class DomainMetadataCheckSumReplayMetricsSuite extends ChecksumLogReplayMetricsT
 
   // TODO: Remove to ChecksumLogReplayMetricsTestBase
   // after incremental CRC loading for domain metadata implemented
-  test(
-    "checksum missing read version, both checksum and checkpoint exist" +
-      " the read version the previous version => use checkpoint") {
+  test("checksum missing at read version and checkpoint version (readVersion - 1) " +
+    " => use checkpoint") {
     withTableWithCrc { (table, tablePath, engine) =>
       val checkpointVersion = 10
       val readVersion = checkpointVersion + 1
-      deleteChecksumFileForTable(tablePath, Seq(checkpointVersion + 1))
+      deleteChecksumFileForTable(tablePath, Seq(readVersion))
 
       loadSnapshotFieldsCheckMetrics(
         table,
@@ -71,6 +70,22 @@ class DomainMetadataCheckSumReplayMetricsSuite extends ChecksumLogReplayMetricsT
         expParquetVersionsRead = Seq(checkpointVersion),
         expParquetReadSetSizes = Seq(1),
         expChecksumReadSet = Seq(readVersion, checkpointVersion),
+        readVersion = readVersion)
+    }
+  }
+
+  test("checksum doesn't contain domain metadata => read from logs") {
+    withTableWithCrc { (table, tablePath, engine) =>
+      val readVersion = 5L
+      rewriteChecksumFileToExcludeDomainMetadata(engine, tablePath, readVersion)
+
+      loadSnapshotFieldsCheckMetrics(
+        table,
+        engine,
+        expJsonVersionsRead = Seq.range(readVersion, -1, -1),
+        expParquetVersionsRead = Nil,
+        expParquetReadSetSizes = Nil,
+        expChecksumReadSet = Seq(readVersion),
         readVersion = readVersion)
     }
   }
