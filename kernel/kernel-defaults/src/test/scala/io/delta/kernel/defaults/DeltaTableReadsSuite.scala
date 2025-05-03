1e76f7394cc0952ad8d7dddea5ffc61ca0ae6fdf
Nick Lanham <nick@databricks.com>
2025-05-02 17:15:18 -0700

    include testing removes

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DeltaTableReadsSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DeltaTableReadsSuite.scala
@@ -1070,6 +1070,7 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
   def testWithCompactions(
       versionsToWrite: Seq[Int], // highest version MUST be last!
       versionToRead: Option[Long],
+      doRemoves: Boolean,
       compactions: Seq[(Int, Int)],
       expectedDeltasToBeRead: Set[Int],
       expectedCompactionsToBeRead: Set[(Int, Int)]) {
@@ -1083,12 +1084,37 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
         }
       })
       val engine = new MetricsEngine(hadoopFileIO)
+      var expectedRows: Set[Long] = Set()
       versionsToWrite.foreach { i =>
-        spark.range(i * 10, i * 10 + 10).write
-          .format("delta")
-          .mode("append")
-          .save(tablePath)
+        // if we're removing, then on odd commits, remove the lower 10 of the previous 20 rows added
+        if (doRemoves && i % 2 == 1) {
+          val prev = i - 1;
+          val low = prev * 10
+          val high = prev * 10 + 10
+          val deleteQuery = "DELETE FROM delta.`%s` WHERE id >= %d AND id < %d".format(
+            tablePath,
+            low,
+            high)
+          spark.sql(deleteQuery)
+          if (versionToRead.isEmpty || versionToRead.get >= i) {
+            expectedRows --= (low until high).map(i => i.toLong)
+          }
+        } else {
+          val low = i * 10
+          // if we're removing, add 20 rows as the first 10 will be removed by the next version,
+          // otherwise add 10 rows
+          val high = if (doRemoves) low + 20 else low + 10
+          spark.range(low, high).write
+            .format("delta")
+            .mode("append")
+            .save(tablePath)
+
+          if (versionToRead.isEmpty || versionToRead.get >= i) {
+            expectedRows ++= (low until high).map(i => i.toLong)
+          }
+        }
       }
+
       val dataPath = new Path(s"file:${tablePath}")
       val logPath = new Path(s"file:${tablePath}", "_delta_log")
       // create the compaction file(s)
@@ -1103,12 +1129,9 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
       }
       engine.resetMetrics()
 
-      val rowCount =
-        versionToRead.map(v => (v.toInt + 1) * 10).getOrElse((versionsToWrite.last + 1) * 10)
-
       checkTable(
         path = tablePath,
-        expectedAnswer = (0 until rowCount).map(i => TestRow(i.toLong)),
+        expectedAnswer = expectedRows.toSeq.map(i => TestRow(i)),
         engine = engine,
         version = versionToRead)
 
@@ -1121,47 +1144,53 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
 
   Seq(Seq((0, 3)), Seq((3, 5)), Seq((5, 9)), Seq((0, 3), (5, 8))).foreach {
     compactions =>
-      val compactionStr = compactions.mkString(", ")
-      test(s"Compaction(s) at $compactionStr (no checkpoint)") {
-        // for these tests, write 0 - 9 (inclusive)
-        val versionsToWrite = (0 to 9)
-        var expectedDeltasToBeRead = versionsToWrite.toSet
-        compactions.foreach { compaction =>
-          // subtract out the compaction versions from the full set
-          expectedDeltasToBeRead &~= (compaction._1 to compaction._2).toSet
+      Seq(true, false).foreach { doRemoves =>
+        val compactionStr = compactions.mkString(", ")
+        test(s"Compaction(s) at $compactionStr (no checkpoint, removes: $doRemoves)") {
+          // for these tests, write 0 - 9 (inclusive)
+          val versionsToWrite = (0 to 9)
+          var expectedDeltasToBeRead = versionsToWrite.toSet
+          compactions.foreach { compaction =>
+            // subtract out the compaction versions from the full set
+            expectedDeltasToBeRead &~= (compaction._1 to compaction._2).toSet
+          }
+          testWithCompactions(
+            versionsToWrite,
+            versionToRead = None,
+            doRemoves,
+            compactions,
+            expectedDeltasToBeRead,
+            compactions.toSet)
         }
-        testWithCompactions(
-          versionsToWrite,
-          versionToRead = None,
-          compactions,
-          expectedDeltasToBeRead,
-          compactions.toSet)
       }
   }
 
   Seq(Seq((3, 5)), Seq((8, 11)), Seq((8, 12), (11, 15)), Seq((11, 13), (15, 17))).foreach {
     compactions =>
-      val compactionStr = compactions.mkString(", ")
-      test(s"Compaction(s) at $compactionStr (with checkpoint)") {
-        // for these tests, write 0 - 19 (inclusive), will checkpoint at 10
-        val versionsToWrite = (0 to 19)
-        val versionsAfterCheckpoint = (11 to 19)
-        var expectedDeltasToBeRead = versionsAfterCheckpoint.toSet
-        var expectedCompactionsToBeRead = Set[(Int, Int)]()
-        compactions.foreach { compaction =>
-          if (compaction._1 > 10) { // only use if after checkpoint
-            // subtract out the compaction versions from the full set
-            expectedDeltasToBeRead &~= (compaction._1 to compaction._2).toSet
-            // add to expected compactions
-            expectedCompactionsToBeRead += compaction
+      Seq(true, false).foreach { doRemoves =>
+        val compactionStr = compactions.mkString(", ")
+        test(s"Compaction(s) at $compactionStr (with checkpoint, removes: $doRemoves)") {
+          // for these tests, write 0 - 19 (inclusive), will checkpoint at 10
+          val versionsToWrite = (0 to 19)
+          val versionsAfterCheckpoint = (11 to 19)
+          var expectedDeltasToBeRead = versionsAfterCheckpoint.toSet
+          var expectedCompactionsToBeRead = Set[(Int, Int)]()
+          compactions.foreach { compaction =>
+            if (compaction._1 > 10) { // only use if after checkpoint
+              // subtract out the compaction versions from the full set
+              expectedDeltasToBeRead &~= (compaction._1 to compaction._2).toSet
+              // add to expected compactions
+              expectedCompactionsToBeRead += compaction
+            }
           }
+          testWithCompactions(
+            versionsToWrite,
+            versionToRead = None,
+            doRemoves,
+            compactions,
+            expectedDeltasToBeRead,
+            expectedCompactionsToBeRead)
         }
-        testWithCompactions(
-          versionsToWrite,
-          versionToRead = None,
-          compactions,
-          expectedDeltasToBeRead,
-          expectedCompactionsToBeRead)
       }
   }
 
@@ -1169,6 +1198,7 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
     testWithCompactions(
       versionsToWrite = (0 to 9),
       versionToRead = None,
+      doRemoves = true,
       compactions = Seq((0, 3), (2, 4)),
       expectedDeltasToBeRead = Set(0, 1, 5, 6, 7, 8, 9),
       expectedCompactionsToBeRead = Set((2, 4)))
@@ -1178,6 +1208,7 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
     testWithCompactions(
       versionsToWrite = (0 to 5),
       versionToRead = None,
+      doRemoves = true,
       compactions = Seq((0, 5)),
       expectedDeltasToBeRead = Set(),
       expectedCompactionsToBeRead = Set((0, 5)))
@@ -1187,6 +1218,7 @@ class DeltaTableReadsSuite extends AnyFunSuite with TestUtils {
     testWithCompactions(
       versionsToWrite = (0 to 9),
       versionToRead = Some(6),
+      doRemoves = true,
       compactions = Seq((1, 3), (5, 8)),
       expectedDeltasToBeRead = Set(0, 4, 5, 6),
       expectedCompactionsToBeRead = Set((1, 3)))
