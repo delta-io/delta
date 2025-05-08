bc45fb0d655ee3b11d9b41351e662b6264d749d9
Scott Sandre <scott.sandre@databricks.com>
2025-05-08 10:58:21 -0700

    WIP test; going to wait for team consensus

---
+++ kernel/kernel-api/src/test/scala/io/delta/kernel/internal/files/ParsedLogFileSuite.scala
@@ -0,0 +1,23 @@
+package io.delta.kernel.internal.files
+
+import io.delta.kernel.test.MockFileSystemClientUtils
+import org.scalatest.funsuite.AnyFunSuite
+
+class ParsedLogFileSuite extends AnyFunSuite with MockFileSystemClientUtils {
+  test("Parses all file types") {
+
+  }
+
+  test("Throws on unknown file type") {
+
+  }
+
+  test("Parses logCompaction start and end versions") {
+
+  }
+
+  test("Parses multi-part checkpoint part and numParts") {
+
+  }
+}
+
