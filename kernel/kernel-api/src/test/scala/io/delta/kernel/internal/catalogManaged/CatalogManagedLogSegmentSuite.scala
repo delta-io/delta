0b8be829632b25d8ca0f1ba2195efdaa6893a897
Scott Sandre <scott.sandre@databricks.com>
2025-05-28 22:16:51 +0000

    update test suite after rebasing with master

--- kernel/kernel-api/src/test/scala/io/delta/kernel/internal/catalogManaged/CatalogManagedLogSegmentSuite.scala
+++ kernel/kernel-api/src/test/scala/io/delta/kernel/internal/catalogManaged/CatalogManagedLogSegmentSuite.scala
@@ -16,21 +16,20 @@
 
 package io.delta.kernel.internal.catalogManaged
 
-import java.util.{Collections, Optional}
+import java.util.Collections
 
 import scala.collection.JavaConverters._
 
 import io.delta.kernel.TableManager
-import io.delta.kernel.data.{ArrayValue, ColumnVector, MapValue}
-import io.delta.kernel.internal.actions.{Format, Metadata, Protocol}
+import io.delta.kernel.internal.actions.Protocol
 import io.delta.kernel.internal.table.ResolvedTableInternal
 import io.delta.kernel.internal.util.FileNames
-import io.delta.kernel.test.{MockFileSystemClientUtils, VectorTestUtils}
+import io.delta.kernel.test.{ActionUtils, MockFileSystemClientUtils}
 import io.delta.kernel.types.{IntegerType, StructType}
 
 import org.scalatest.funsuite.AnyFunSuite
 
-class CatalogManagedSuite extends AnyFunSuite with MockFileSystemClientUtils with VectorTestUtils {
+class CatalogManagedSuite extends AnyFunSuite with MockFileSystemClientUtils with ActionUtils {
 
   private def testLogSegment(
       testName: String,
@@ -195,30 +194,6 @@ class CatalogManagedSuite extends AnyFunSuite with MockFileSystemClientUtils wit
     deltaVersions = Seq(),
     ratifiedCommitVersions = 0L to 3L,
     expectedExceptionClassOpt = Some(classOf[io.delta.kernel.exceptions.TableNotFoundException]))
-
-  // TODO: refactor to util trait
-  def testMetadata(
-      schema: StructType,
-      partitionCols: Seq[String] = Seq.empty,
-      tblProps: Map[String, String] = Map.empty): Metadata = {
-    new Metadata(
-      "id",
-      Optional.of("name"),
-      Optional.of("description"),
-      new Format("parquet", Collections.emptyMap()),
-      schema.toJson,
-      schema,
-      new ArrayValue() { // partitionColumns
-        override def getSize: Int = partitionCols.size
-        override def getElements: ColumnVector = stringVector(partitionCols)
-      },
-      Optional.empty(),
-      new MapValue() { // conf
-        override def getSize: Int = tblProps.size
-        override def getKeys: ColumnVector = stringVector(tblProps.toSeq.map(_._1))
-        override def getValues: ColumnVector = stringVector(tblProps.toSeq.map(_._2))
-      })
-  }
 
   ////////////////////
   // Laziness tests //
