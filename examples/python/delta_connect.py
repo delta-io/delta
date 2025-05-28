9a1f2ef349a744b30f84d4e06e5123f0268070f4
Thang Long VU <long.vu@databricks.com>
2025-05-28 15:12:28 +0200

    Address comment

--- examples/python/delta_connect.py
+++ examples/python/delta_connect.py
@@ -37,7 +37,7 @@ export SPARK_REMOTE="sc://localhost:15002"
 
 (3) Run this file i.e. python3 examples/python/delta_connect.py
 
-(4) Once finished QA-ing, stop the Spark Connect server.
+(4) Stop the Spark Connect server once QA is complete.
 sbin/stop-connect-server.sh
 """
 
@@ -89,13 +89,13 @@ else:
 
 # ------------------------ Write basic table and check that results match ----------------------
 
-# By table name
+# By table name.
 spark.range(5).write.format("delta").saveAsTable(tableName)
 assert_dataframe_equals(DeltaTable.forName(spark, tableName).toDF(), spark.range(5))
 assert_dataframe_equals(spark.read.format("delta").table(tableName), spark.range(5))
 assert_dataframe_equals(spark.sql(f"SELECT * FROM {tableName}"), spark.range(5))
 
-# By table path
+# By table path.
 spark.range(6).write.format("delta").save(filePath)
 assert_dataframe_equals(DeltaTable.forPath(spark, filePath).toDF(), spark.range(6))
 assert_dataframe_equals(spark.read.format("delta").load(filePath), spark.range(6))
@@ -103,28 +103,28 @@ assert_dataframe_equals(spark.sql(f"SELECT * FROM delta.`{filePath}`"), spark.ra
 
 deltaTable = DeltaTable.forPath(spark, filePath)
 
-# Update every even value by adding 100 to it
+# Update every even value by adding 100 to it.
 print("########### Update to the table(add 100 to every even value) ##############")
 deltaTable.update(
     condition="id % 2 == 0",
     set={"id": "id + 100"}
 )
 
-updateResult = spark.createDataFrame([(100,), (1,), (102,), (3,), (104,), (5,)], ["id"])
-assert_dataframe_equals(deltaTable.toDF(), updateResult)
-assert_dataframe_equals(spark.read.format("delta").load(filePath), updateResult)
-assert_dataframe_equals(spark.sql(f"SELECT * FROM delta.`{filePath}`"), updateResult)
+expectedUpdateResult = spark.createDataFrame([(100,), (1,), (102,), (3,), (104,), (5,)], ["id"])
+assert_dataframe_equals(deltaTable.toDF(), expectedUpdateResult)
+assert_dataframe_equals(spark.read.format("delta").load(filePath), expectedUpdateResult)
+assert_dataframe_equals(spark.sql(f"SELECT * FROM delta.`{filePath}`"), expectedUpdateResult)
 
-# Delete every even value
+# Delete every even value.
 print("######### Delete every even value ##############")
 deltaTable.delete(condition="id % 2 == 0")
 
-deleteResult = spark.createDataFrame([(1,), (3,), (5,)], ["id"])
-assert_dataframe_equals(deltaTable.toDF(), deleteResult)
-assert_dataframe_equals(spark.read.format("delta").load(filePath), deleteResult)
-assert_dataframe_equals(spark.sql(f"SELECT * FROM delta.`{filePath}`"), deleteResult)
+expectedDeleteResult = spark.createDataFrame([(1,), (3,), (5,)], ["id"])
+assert_dataframe_equals(deltaTable.toDF(), expectedDeleteResult)
+assert_dataframe_equals(spark.read.format("delta").load(filePath), expectedDeleteResult)
+assert_dataframe_equals(spark.sql(f"SELECT * FROM delta.`{filePath}`"), expectedDeleteResult)
 
-# Read old version of data using time travel
+# Read old version of data using time travel.
 print("######## Read old data using time travel ############")
 oldVersionDF = spark.read.format("delta").option("versionAsOf", 0).load(filePath)
 
