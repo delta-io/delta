b059e7132e87dc4712aa27abee662b8488f6a022
Sun Cao <sun.cao+data@databricks.com>
2025-06-02 16:48:24 -0700

    Update DeltaSharingUtils.scala

--- sharing/src/main/scala/io/delta/sharing/spark/DeltaSharingUtils.scala
+++ sharing/src/main/scala/io/delta/sharing/spark/DeltaSharingUtils.scala
@@ -20,7 +20,6 @@ import java.nio.charset.StandardCharsets.UTF_8
 import java.text.SimpleDateFormat
 import java.util.{TimeZone, UUID}
 
-import scala.collection.mutable.ListBuffer
 import scala.reflect.ClassTag
 
 import org.apache.spark.sql.delta._
@@ -33,13 +32,12 @@ import io.delta.sharing.client.util.JsonUtils
 import org.apache.spark.SparkEnv
 import org.apache.spark.delta.sharing.TableRefreshResult
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.delta.actions.DeletionVectorDescriptor
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.FileFormat
 import org.apache.spark.storage.{BlockId, StorageLevel}
 
 object DeltaSharingUtils extends Logging {
-
-  import org.apache.spark.sql.delta.actions.DeletionVectorDescriptor
 
   val STREAMING_SUPPORTED_READER_FEATURES: Seq[String] =
     Seq(
