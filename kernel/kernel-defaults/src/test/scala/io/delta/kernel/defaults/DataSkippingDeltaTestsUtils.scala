4369dd509e133a54e4435795b30e1f001d399594
Rakesh Veeramacheneni <rakesh.veeramacheneni@databricks.com>
2025-06-03 09:31:43 -0700

    [Kernel] Fix TIMESTAMP_NTZ stats serialization and add comprehensive timestamp data skipping tests

---
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DataSkippingDeltaTestsUtils.scala
@@ -0,0 +1,59 @@
+package io.delta.kernel.defaults
+
+import scala.collection.immutable.Seq
+
+import org.apache.spark.sql.delta.DeltaLog
+import org.apache.spark.sql.delta.actions.AddFile
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions.{Expression, PredicateHelper}
+
+/**
+ * Encapsulates a few Delta-Spark DataSkipping Utils.
+ * E.g A helper to get files in a deltaScan after applying a predicate.
+ */
+trait DataSkippingDeltaTestsUtils extends PredicateHelper {
+  def parse(spark: SparkSession, deltaLog: DeltaLog, predicate: String): Seq[Expression] = {
+    if (predicate == "True") return Seq(
+      org.apache.spark.sql.catalyst.expressions.Literal.TrueLiteral)
+
+    val filtered = spark.read.format("delta").load(deltaLog.dataPath.toString).where(predicate)
+    filtered
+      .queryExecution
+      .optimizedPlan
+      .expressions
+      .flatMap(splitConjunctivePredicates).toList
+  }
+
+  def filesRead(
+      spark: SparkSession,
+      deltaLog: DeltaLog,
+      predicate: String): Int = {
+    getFilesRead(spark, deltaLog, predicate, checkEmptyUnusedFilters = false).size
+  }
+
+  /**
+   * Returns the files that should be included in a scan after applying the given predicate on
+   * a snapshot of the Delta log.
+   *
+   * @param deltaLog                Delta log for a table.
+   * @param predicate               Predicate to run on the Delta table.
+   * @param checkEmptyUnusedFilters If true, check if there were no unused filters, meaning
+   *                                the given predicate was used as data or partition filters.
+   * @return The files that should be included in a scan after applying the predicate.
+   */
+  def getFilesRead(
+      spark: SparkSession,
+      deltaLog: DeltaLog,
+      predicate: String,
+      checkEmptyUnusedFilters: Boolean): Seq[AddFile] = {
+    val parsed: Seq[Expression] = parse(spark, deltaLog, predicate)
+    val res = deltaLog.snapshot.filesForScan(parsed)
+    assert(res.total.files.get == deltaLog.snapshot.numOfFiles)
+    assert(res.total.bytesCompressed.get == deltaLog.snapshot.sizeInBytes)
+    assert(res.scanned.files.get == res.files.size)
+    assert(res.scanned.bytesCompressed.get == res.files.map(_.size).sum)
+    assert(!checkEmptyUnusedFilters || res.unusedFilters.isEmpty)
+    res.files.toList
+  }
+}
