1cdf004f96e4929cc0bb215d23fd9867c59261b8
Venki Korukanti <venki.korukanti@gmail.com>
2025-06-11 18:09:47 -0700

    Revert "[Delta] Support absolute path in Clone" (#4740)

--- spark/src/main/scala/org/apache/spark/sql/delta/commands/ConvertToDeltaCommand.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/commands/ConvertToDeltaCommand.scala
@@ -305,13 +305,10 @@ abstract class ConvertToDeltaCommandBase(
     val shouldCollectStats = collectStats && statsEnabled
     val statsBatchSize = conf.getConf(DeltaSQLConf.DELTA_IMPORT_BATCH_SIZE_STATS_COLLECTION)
     var numFiles = 0L
-    val useAbsolutePath = manifest.supportAbsolutePath
     manifest.getFiles.grouped(statsBatchSize).flatMap { batch =>
       val adds = batch.map(
         ConvertUtils.createAddFile(
-          _, txn.deltaLog.dataPath, fs, conf, Some(partitionSchema),
-          useAbsolutePath || deltaPath.isDefined
-        ))
+          _, txn.deltaLog.dataPath, fs, conf, Some(partitionSchema), deltaPath.isDefined))
       if (shouldCollectStats) {
         logInfo(
           log"Collecting stats for a batch of " +
