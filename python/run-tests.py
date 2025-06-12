0839ca48a26015ec8ce1994eefbe04180496b9e4
Allison Portis <allison.portis@databricks.com>
2025-06-11 18:55:51 -0700

    fix line length

--- python/run-tests.py
+++ python/run-tests.py
@@ -61,7 +61,10 @@ def prepare(root_dir, use_spark_master):
     print("##### Preparing python tests & building packages #####")
     # Build package with python files in it
     sbt_path = path.join(root_dir, path.join("build", "sbt"))
-    ivy_caches_to_clear = [filepath for filepath in os.listdir(os.path.expanduser("~")) if filepath.startswith(".ivy")]
+    ivy_caches_to_clear = [
+        filepath for filepath in os.listdir(os.path.expanduser("~"))
+        if filepath.startswith(".ivy")
+    ]
     print(f"Clearing Ivy caches in: {ivy_caches_to_clear}")
     for filepath in ivy_caches_to_clear:
         delete_if_exists(os.path.expanduser(f"~/{filepath}/cache/io.delta"))
