6854f94fb52376d7ff5f327abdd349e3811e2623
Scott Sandre <scott.sandre@databricks.com>
2025-06-17 22:55:26 +0000

    pr feedback: comments

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/catalogManaged/CatalogManagedE2EReadSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/catalogManaged/CatalogManagedE2EReadSuite.scala
@@ -28,17 +28,35 @@ import io.delta.kernel.utils.FileStatus
 
 import org.scalatest.funsuite.AnyFunSuite
 
+/**
+ * Test suite for end-to-end reads of catalog-managed Delta tables.
+ *
+ * The goal of this suite is to simulate how a real "Catalog-Managed-Client" would read a
+ * catalog-managed Delta table, without introducing a full, or even partial (e.g. in-memory)
+ * catalog client implementation.
+ *
+ * The catalog boundary is simulated by tests manually providing [[ParsedLogData]]. For example,
+ * there can be X commits in the _staged_commits directory, and a given test can decide that Y
+ * commits (subset of X) are in fact "ratified". The test can then turn those commits into
+ * [[ParsedLogData]] and inject them into the [[io.delta.kernel.ResolvedTableBuilder]]. This is,
+ * in essence, doing exactly what we would expect a "Catalog-Managed-Client" to do.
+ */
 class CatalogManagedE2EReadSuite extends AnyFunSuite with TestUtilsWithTableManagerAPIs {
 
   test("simple e2e read of catalogOwned-preview table with staged ratified commits") {
     val tablePath = getTestResourceFilePath("catalog-owned-preview")
+
+    // Note: We need to *resolve* each test resource file path, because the table root file path
+    //       will itself be resolved when we create a ResolvedTable. If we resolved some paths but
+    //       not others, we would get an error like `File <commit-file> doesn't belong in the
+    //       transaction log at <log-path>`.
 
     val parsedLogData = Seq(
       // scalastyle:off line.size.limit
       getTestResourceFilePath("catalog-owned-preview/_delta_log/_staged_commits/00000000000000000001.4cb9708e-b478-44de-b203-53f9ba9b2876.json"),
       getTestResourceFilePath("catalog-owned-preview/_delta_log/_staged_commits/00000000000000000002.5b9bba4a-0085-430d-a65e-b0d38c1afbe9.json"))
       // scalastyle:on line.size.limit
-      .map { p => s"file:$p" }
+      .map { path => defaultEngine.getFileSystemClient.resolvePath(path) }
       .map { p => FileStatus.of(p) }
       .map { fs => ParsedLogData.forFileStatus(fs) }
       .toList
