83a342024e7712b3d3a450ad11111c811de002d2
mmmyr <mayanran194@gmail.com>
2025-06-26 19:11:00 +0000

    fix comments

--- kernel/kernel-api/src/main/java/io/delta/kernel/PaginatedScan.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/PaginatedScan.java
@@ -34,7 +34,7 @@ import java.util.Optional;
  * last page ended. For the first page, the {@code pageToken} should be {@code Optional.empty()}.
  *
  * <p>Consumers typically use {@link PaginatedScan} in a loop: they call {@code getScanFiles()} to
- * retrieve an iterator over the current page's scan files, After consuming the iterator, users
+ * retrieve an iterator over the current page's scan files. After consuming the iterator, users
  * should call {@code getCurrentPageToken()} to retrieve a token to pass into the next page request.
  * This allows users to scan the dataset incrementally, resuming from where they left off.
  */
@@ -51,8 +51,8 @@ public interface PaginatedScan extends Scan {
 
   /**
    * Returns a page token representing the current position in the ScanFiles iterator, allowing the
-   * scan to be resumed from that point in a subsequent paginated request. If the iterator is
-   * partially consumed, the token corresponds to the next unconsumed element.
+   * scan to be resumed from that point in a subsequent request. If the iterator is partially
+   * consumed, the token corresponds to the next unconsumed element.
    *
    * <p>Note: Since {@code getScanFiles()} returns an iterator over batches, this page token assumes
    * that the current batch has been fully consumed. As a result, the token points to the start of
@@ -62,11 +62,11 @@ public interface PaginatedScan extends Scan {
    * Optional.empty()}.
    *
    * <p>The returned token also includes metadata for validation purposes, such as detecting changes
-   * in query parameters or log segments between requests.
+   * in query parameters or underlying delta log content between requests.
    *
    * <p>Callers should invoke {@code getCurrentPageToken()} only after finishing consumption of the
-   * current iterator. Actually it is not required to consume the entire iterator; the returned page
-   * token will always point to the beginning of the next batch in the iterator.
+   * current iterator. If they don't consume the entire iterator; then the returned page token will
+   * always point to the beginning of the next batch in the iterator.
    *
    * @return an {@link Optional} {@link Row} encoding the current scan position and validation
    *     metadata.
