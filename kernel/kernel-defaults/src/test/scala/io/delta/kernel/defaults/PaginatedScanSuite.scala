af36f416e93eeb3f8c9a53d45ce104b7dcb722de
mmmyr <mayanran194@gmail.com>
2025-07-09 04:51:03 +0000

    minors

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/PaginatedScanSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/PaginatedScanSuite.scala
@@ -151,121 +151,126 @@ class PaginatedScanSuite extends AnyFunSuite with TestUtils
   // TODO: test call hasNext() twice
 
   test("Read First Page: basic pagination with single JSON file") {
+    withGoldenTable("kernel-pagination-oneJSON") { tablePath =>
+      /**
+       *  Log Segment List:
+       *  00000000000000000000.json contains 13 rows, 10 AddFiles in total
+       *  Non-AddFile rows: CommitInfo, Protocol, and Metadata
+       *  AddFile rows: 10 AddFiles
+       * Batch 1: 5 rows, 2 selected AddFiles (and 3 non AddFile rows)
+       * Batch 2: 5 rows, 5 selected AddFiles
+       * Batch 3: 3 rows, 3 selected AddFiles
+       */
 
-    /**
-     *  Log Segment List:
-     *  00000000000000000000.json contains 13 rows, 10 AddFiles in total
-     *  Non-AddFile rows: CommitInfo, Protocol, and Metadata
-     *  AddFile rows: 10 AddFiles
-     * Batch 1: 5 rows, 2 selected AddFiles (and 3 non AddFile rows)
-     * Batch 2: 5 rows, 5 selected AddFiles
-     * Batch 3: 3 rows, 3 selected AddFiles
-     */
+      // spark.range(0, 100, 1, 10).write.format("delta").save(tablePath)
 
-    val tablePath = GoldenTableUtils.goldenTablePath("kernel-pagination-oneJSON")
-    // spark.range(0, 100, 1, 10).write.format("delta").save(tablePath)
+      val JSONFile0 = "00000000000000000000.json"
+      val testCases = Seq(
+        (
+          "Page size 1",
+          1 /* page size */,
+          2 /* expectedFileCount */,
+          1 /* expectedBatchCount */,
+          JSONFile0,
+          4),
+        ("Page size 2", 2, 2, 1, JSONFile0, 4),
+        ("Page size 4", 4, 7, 2, JSONFile0, 9), // batch size go over page limit
+        ("Page size 7", 7, 7, 2, JSONFile0, 9), // batch size is equal to page limit
+        ("Page size 8", 8, 10, 3, JSONFile0, 12),
+        ("Page size 20", 20, 10, 3, JSONFile0, 12) // page limit isn't reached
+      )
 
-    val JSONFile0 = "00000000000000000000.json"
-    val testCases = Seq(
-      (
-        "Page size 1",
-        1 /* page size */,
-        2 /* expectedFileCount */,
-        1 /* expectedBatchCount */,
-        JSONFile0,
-        4),
-      ("Page size 2", 2, 2, 1, JSONFile0, 4),
-      ("Page size 4", 4, 7, 2, JSONFile0, 9), // batch size go over page limit
-      ("Page size 7", 7, 7, 2, JSONFile0, 9), // batch size is equal to page limit
-      ("Page size 8", 8, 10, 3, JSONFile0, 12),
-      ("Page size 20", 20, 10, 3, JSONFile0, 12) // page limit isn't reached
-    )
+      runPaginationTestCasesForFirstPage(tablePath, Optional.empty[Long](), testCases)
 
-    runPaginationTestCasesForFirstPage(tablePath, Optional.empty[Long](), testCases)
+      // TODO: test all page requests
+    }
   }
 
   test("Read First Page: basic pagination with multiple JSON files") {
+    withGoldenTable("kernel-pagination-allJSONs") { tablePath =>
+      /**
+       * Log Segment List:
+       * 00000000000000000000.json : 8 rows (5 AddFile row + 3 non-AddFile rows)
+       * 00000000000000000001.json : 6 rows (5 AddFile row + 1 non-AddFile row)
+       * 00000000000000000002.json : 6 rows (5 AddFile row + 1 non-AddFile row)
+       *
+       * 00000000000000000002.json contains 2 batches, 5 active AddFiles in total
+       * Batch 1: 5 rows, 4 selected AddFiles
+       * Batch 2: 1 rows, 1 selected AddFiles
+       *
+       * 00000000000000000001.json contains 2 batches, 5 active AddFiles in total
+       * Batch 1: 5 rows, 4 selected AddFiles
+       * Batch 2: 1 rows, 1 selected AddFiles
+       *
+       * 00000000000000000000.json contains 2 batches, 5 active AddFiles in total
+       * Batch 1: 5 rows, 2 selected AddFiles
+       * Batch 2: 3 rows, 3 selected AddFiles
+       */
 
-    /**
-     * Log Segment List:
-     * 00000000000000000000.json : 8 rows (5 AddFile row + 3 non-AddFile rows)
-     * 00000000000000000001.json : 6 rows (5 AddFile row + 1 non-AddFile row)
-     * 00000000000000000002.json : 6 rows (5 AddFile row + 1 non-AddFile row)
-     *
-     * 00000000000000000002.json contains 2 batches, 5 active AddFiles in total
-     * Batch 1: 5 rows, 4 selected AddFiles
-     * Batch 2: 1 rows, 1 selected AddFiles
-     *
-     * 00000000000000000001.json contains 2 batches, 5 active AddFiles in total
-     * Batch 1: 5 rows, 4 selected AddFiles
-     * Batch 2: 1 rows, 1 selected AddFiles
-     *
-     * 00000000000000000000.json contains 2 batches, 5 active AddFiles in total
-     * Batch 1: 5 rows, 2 selected AddFiles
-     * Batch 2: 3 rows, 3 selected AddFiles
-     */
-    val tablePath = GoldenTableUtils.goldenTablePath("kernel-pagination-allJSONs")
+      /**
+       * spark.range( 0, 50, 1, 5).write.format("delta").save(tablePath)
+       * spark.range(50, 100, 1, 5).write.format("delta").mode("append").save(tablePath)
+       * spark.range(100, 150, 1, 5).write.format("delta").mode("append").save(tablePath)
+       */
 
-    /**
-     *    spark.range( 0, 50, 1, 5).write.format("delta").save(tablePath)
-     *    spark.range(50, 100, 1, 5).write.format("delta").mode("append").save(tablePath)
-     *    spark.range(100, 150, 1, 5).write.format("delta").mode("append").save(tablePath)
-     */
+      val JSONFile0 = "00000000000000000000.json"
+      val JSONFile1 = "00000000000000000001.json"
+      val JSONFile2 = "00000000000000000002.json"
+      val testCases = Seq(
+        (
+          "Page size 1",
+          1 /* page size */,
+          4 /* expectedFileCount */,
+          1 /* expectedBatchCount */,
+          JSONFile2,
+          4),
+        ("Page size 4", 4, 4, 1, JSONFile2, 4),
+        ("Page size 5", 5, 5, 2, JSONFile2, 5), // batch size is equal to page limit
+        ("Page size 7", 7, 9, 3, JSONFile1, 4), // batch size is goes over page limit
+        ("Page size 8", 8, 9, 3, JSONFile1, 4),
+        ("Page size 18", 18, 15, 6, JSONFile0, 7))
 
-    val JSONFile0 = "00000000000000000000.json"
-    val JSONFile1 = "00000000000000000001.json"
-    val JSONFile2 = "00000000000000000002.json"
-    val testCases = Seq(
-      (
-        "Page size 1",
-        1 /* page size */,
-        4 /* expectedFileCount */,
-        1 /* expectedBatchCount */,
-        JSONFile2,
-        4),
-      ("Page size 4", 4, 4, 1, JSONFile2, 4),
-      ("Page size 5", 5, 5, 2, JSONFile2, 5), // batch size is equal to page limit
-      ("Page size 7", 7, 9, 3, JSONFile1, 4), // batch size is goes over page limit
-      ("Page size 8", 8, 9, 3, JSONFile1, 4),
-      ("Page size 18", 18, 15, 6, JSONFile0, 7))
+      runPaginationTestCasesForFirstPage(tablePath, Optional.empty[Long](), testCases)
 
-    runPaginationTestCasesForFirstPage(tablePath, Optional.empty[Long](), testCases)
+      // TODO: test all page requests
+    }
   }
 
   test("Read First Page: basic pagination with one checkpoint file") {
-    val tablePath = GoldenTableUtils.goldenTablePath("kernel-pagination-oneCheckpoint")
+    withGoldenTable("kernel-pagination-oneCheckpoint") { tablePath =>
+      /**
+       * 00000000000000000010.checkpoint.parquet contains 5 batches, 22 active AddFiles, 24 rows in total
+       * Batch 1: 5 rows, 5 selected AddFiles
+       * Batch 2: 5 rows, 5 selected AddFiles
+       * Batch 3: 5 rows, 5 selected AddFiles
+       * Batch 4: 5 rows, 3 selected AddFiles
+       * Batch 5: 4 rows, 4 selected AddFiles
+       */
+      val checkpoint10 = "00000000000000000010.checkpoint.parquet"
 
-    /**
-     * 00000000000000000010.checkpoint.parquet contains 5 batches, 22 active AddFiles, 24 rows in total
-     * Batch 1: 5 rows, 5 selected AddFiles
-     * Batch 2: 5 rows, 5 selected AddFiles
-     * Batch 3: 5 rows, 5 selected AddFiles
-     * Batch 4: 5 rows, 3 selected AddFiles
-     * Batch 5: 4 rows, 4 selected AddFiles
-     */
-    val checkpoint10 = "00000000000000000010.checkpoint.parquet"
+      val testCases = Seq(
+        (
+          "Page size 1",
+          1 /* page size */,
+          5 /* expectedFileCount */,
+          1 /* expectedBatchCount */,
+          checkpoint10,
+          4 /* row index in page token*/ ),
+        ("Page size 10", 10, 10, 2, checkpoint10, 9),
+        ("Page size 12", 12, 15, 3, checkpoint10, 14), // batch size goes over limit
+        ("Large Page size", 100, 22, 5, checkpoint10, 23) // page limit isn't reached
+      )
 
-    val testCases = Seq(
-      (
-        "Page size 1",
-        1 /* page size */,
-        5 /* expectedFileCount */,
-        1 /* expectedBatchCount */,
-        checkpoint10,
-        4 /* row index in page token*/ ),
-      ("Page size 10", 10, 10, 2, checkpoint10, 9),
-      ("Page size 12", 12, 15, 3, checkpoint10, 14), // batch size goes over limit
-      ("Page size 100", 100, 22, 5, checkpoint10, 23) // page limit isn't reached
-    )
+      runPaginationTestCasesForFirstPage(tablePath, Optional.of(10L), testCases)
 
-    runPaginationTestCasesForFirstPage(tablePath, Optional.of(10L), testCases)
+      // TODO: test all page requests
+    }
   }
 
   test("Read First Page: basic pagination with one checkpoint file " +
     "and multiple JSON files") {
-    val tablePath = GoldenTableUtils.goldenTablePath("kernel-pagination-oneCheckpoint")
-
-    /*
+    withGoldenTable("kernel-pagination-oneCheckpoint") { tablePath =>
+      /*
     for (i <- 0 until 10) {
       val mode = if (i == 0) "overwrite" else "append"
       spark.range(i * 10, (i + 1) * 10, 1, 2)
@@ -281,51 +286,58 @@ class PaginatedScanSuite extends AnyFunSuite with TestUtils
       spark.range(i * 10, (i + 1) * 10, 1, 2)
         .write.format("delta").mode("append").save(tablePath)
     }
-     */
+       */
 
-    /**
-     * Log segment list:
-     * 00000000000000000010.checkpoint.parquet
-     * 00000000000000000011.json
-     * 00000000000000000012.json
-     *
-     * 00000000000000000012.json contains 1 batch, 2 active AddFiles in total
-     * Batch 1: 3 rows, 2 selected AddFiles
-     *
-     * 00000000000000000011.json contains 1 batch, 2 active AddFiles in total
-     * Batch 1: 3 rows, 2 selected AddFiles
-     *
-     * 00000000000000000010.checkpoint.parquet contains 5 batches, 22 active AddFiles, 24 rows in total
-     * Batch 1: 5 rows, 5 selected AddFiles
-     * Batch 2: 5 rows, 5 selected AddFiles
-     * Batch 3: 5 rows, 5 selected AddFiles
-     * Batch 4: 5 rows, 3 selected AddFiles
-     * Batch 5: 4 rows, 4 selected AddFiles
-     */
+      /**
+       * Log segment list:
+       * 00000000000000000010.checkpoint.parquet
+       * 00000000000000000011.json
+       * 00000000000000000012.json
+       *
+       * 00000000000000000012.json contains 1 batch, 2 active AddFiles in total
+       * Batch 1: 3 rows, 2 selected AddFiles
+       *
+       * 00000000000000000011.json contains 1 batch, 2 active AddFiles in total
+       * Batch 1: 3 rows, 2 selected AddFiles
+       *
+       * 00000000000000000010.checkpoint.parquet contains 5 batches, 22 active AddFiles, 24 rows in total
+       * Batch 1: 5 rows, 5 selected AddFiles
+       * Batch 2: 5 rows, 5 selected AddFiles
+       * Batch 3: 5 rows, 5 selected AddFiles
+       * Batch 4: 5 rows, 3 selected AddFiles
+       * Batch 5: 4 rows, 4 selected AddFiles
+       */
 
-    val checkpoint10 = "00000000000000000010.checkpoint.parquet"
-    val JSONFile11 = "00000000000000000011.json"
-    val JSONFile12 = "00000000000000000012.json"
+      val checkpoint10 = "00000000000000000010.checkpoint.parquet"
+      val JSONFile11 = "00000000000000000011.json"
+      val JSONFile12 = "00000000000000000012.json"
 
-    val testCases = Seq(
-      (
-        "Page size 1",
-        1 /* page size */,
-        2 /* expectedFileCount */,
-        1 /* expectedBatchCount */,
-        JSONFile12,
-        2 /* row index in page token*/ ),
-      ("Page size 2", 2, 2, 1, JSONFile12, 2),
-      ("Page size 3", 3, 4, 2, JSONFile11, 2),
-      ("Page size 4", 4, 4, 2, JSONFile11, 2),
-      ("Page size 8", 8, 9, 3, checkpoint10, 4),
-      ("Page size 18", 18, 19, 5, checkpoint10, 14) // page limit isn't reached
-    )
+      val testCases = Seq(
+        (
+          "Page size 1",
+          1 /* page size */,
+          2 /* expectedFileCount */,
+          1 /* expectedBatchCount */,
+          JSONFile12,
+          2 /* row index in page token*/ ),
+        ("Page size 2", 2, 2, 1, JSONFile12, 2),
+        ("Page size 3", 3, 4, 2, JSONFile11, 2),
+        ("Page size 4", 4, 4, 2, JSONFile11, 2),
+        ("Page size 8", 8, 9, 3, checkpoint10, 4),
+        ("Page size 18", 18, 19, 5, checkpoint10, 14) // page limit isn't reached
+      )
 
-    runPaginationTestCasesForFirstPage(tablePath, Optional.empty[Long](), testCases)
+      runPaginationTestCasesForFirstPage(tablePath, Optional.empty[Long](), testCases)
+
+      // TODO: test all page requests
+    }
   }
 
-  test("Read First Page: basic pagination with multi-checkpoint files") {
+  test("Read Page: basic pagination with multi-checkpoint files") {
+    // TODO
+  }
+
+  test("Read Page: basic pagination with sidecar files") {
     // TODO
   }
 }
