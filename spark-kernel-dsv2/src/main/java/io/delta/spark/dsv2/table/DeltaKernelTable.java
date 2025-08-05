23f9f2f4b8383fa1d53abb270672cc1058ace7ed
Xin Huang <42597328+huan233usc@users.noreply.github.com>
2025-08-05 15:14:15 -0700

    [Kernel][Dsv2]Scaffolding of Table implementation (#5024)
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md
    2. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      3. Be sure to keep the PR description updated to reflect all changes.
      4. Please write your PR title to summarize what this PR proposes.
    5. If possible, provide a concise example to reproduce the issue for a
    faster review.
    6. If applicable, include the corresponding issue number in the PR title
    and link it in the body.
    -->
    
    #### Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [x] Spark
    - [ ] Standalone
    - [ ] Flink
    - [x] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    
    <!--
    - Describe what this PR changes.
    - Describe why we need the change.
     
    If this PR resolves an issue be sure to include "Resolves #XXX" to
    correctly link and close the issue upon merge.
    -->
    This PR introduces DeltaDsv2Table, a new implementation of Spark's Table
    interface from the DataSourceV2 API. This implementation is built
    entirely on top of the Delta Kernel API.
    
    Next: 
    line 1:update TestCatalog to work with DeltaDsv2Table
    line 2:implement SupportRead
    See https://github.com/delta-io/delta/issues/5001 for full plan
    
    
    ## How was this patch tested?
    
    <!--
    If tests were added, say they were added here. Please make sure to test
    the changes thoroughly including negative and positive cases if
    possible.
    If the changes were tested in any way other than unit tests, please
    clarify how you tested step by step (ideally copy and paste-able, so
    that other reviewers can test and check, and descendants can verify in
    the future).
    If the changes were not tested, please explain why.
    -->
    
    ## Does this PR introduce _any_ user-facing changes?
    
    <!--
    If yes, please clarify the previous behavior and the change this PR
    proposes - provide the console output, description and/or an example to
    show the behavior difference if possible.
    If possible, please also clarify if this is a user-facing change
    compared to the released Delta Lake versions or within the unreleased
    branches such as master.
    If no, write 'No'.
    -->
    No

---
+++ spark-kernel-dsv2/src/main/java/io/delta/spark/dsv2/table/DeltaKernelTable.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (2025) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.delta.spark.dsv2.table;
+
+import static java.util.Objects.requireNonNull;
+
+import io.delta.kernel.internal.SnapshotImpl;
+import io.delta.spark.dsv2.utils.SchemaUtils;
+import java.util.*;
+import org.apache.spark.sql.connector.catalog.CatalogV2Util;
+import org.apache.spark.sql.connector.catalog.Column;
+import org.apache.spark.sql.connector.catalog.Identifier;
+import org.apache.spark.sql.connector.catalog.Table;
+import org.apache.spark.sql.connector.catalog.TableCapability;
+import org.apache.spark.sql.connector.expressions.Expressions;
+import org.apache.spark.sql.connector.expressions.Transform;
+import org.apache.spark.sql.types.StructType;
+
+/** A DataSource V2 Table implementation for Delta Lake tables using the Delta Kernel API. */
+public class DeltaKernelTable implements Table {
+
+  private final Identifier identifier;
+  // TODO: [delta-io/delta#5029] Add getProperties() in snapshot to avoid using Impl class.
+  private final SnapshotImpl snapshot;
+
+  /**
+   * Creates a new DeltaKernelTable instance.
+   *
+   * @param identifier the table identifier
+   * @param snapshot the Delta table snapshot
+   */
+  public DeltaKernelTable(Identifier identifier, SnapshotImpl snapshot) {
+    this.identifier = requireNonNull(identifier, "identifier is null");
+    this.snapshot = requireNonNull(snapshot, "snapshot is null");
+  }
+
+  @Override
+  public String name() {
+    return identifier.name();
+  }
+
+  @Override
+  public StructType schema() {
+    return SchemaUtils.convertKernelSchemaToSparkSchema(snapshot.getSchema());
+  }
+
+  @Override
+  public Column[] columns() {
+    return CatalogV2Util.structTypeToV2Columns(schema());
+  }
+
+  @Override
+  public Transform[] partitioning() {
+    // Delta currently just support identity partition
+    return snapshot.getPartitionColumnNames().stream()
+        .map(Expressions::identity)
+        .toArray(Transform[]::new);
+  }
+
+  @Override
+  public Map<String, String> properties() {
+    return snapshot.getMetadata().getConfiguration();
+  }
+
+  @Override
+  public Set<TableCapability> capabilities() {
+    // TODO: [delta-io/delta#5001] fill in when implementing mix-in interface
+    return Collections.unmodifiableSet(new HashSet<>());
+  }
+}
