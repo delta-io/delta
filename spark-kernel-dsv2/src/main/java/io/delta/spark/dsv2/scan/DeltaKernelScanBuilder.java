244acc31d3db0d420ad4f22482bc534584a56a0b
Xin Huang <xin.huang@databricks.com>
2025-08-11 16:03:14 -0700

    save

--- spark-kernel-dsv2/src/main/java/io/delta/spark/dsv2/scan/DeltaKernelScanBuilder.java
+++ spark-kernel-dsv2/src/main/java/io/delta/spark/dsv2/scan/DeltaKernelScanBuilder.java
@@ -1,4 +1,24 @@
 package io.delta.spark.dsv2.scan;
 
-public class DeltaKernelScanBuilder {
+import io.delta.kernel.internal.SnapshotImpl;
+import org.apache.spark.sql.util.CaseInsensitiveStringMap;
+
+/**
+ * A Spark ScanBuilder implementation that wraps Delta Kernel's ScanBuilder. This allows Spark to
+ * use Delta Kernel for reading Delta tables.
+ */
+public class DeltaKernelScanBuilder implements org.apache.spark.sql.connector.read.ScanBuilder {
+
+    private final SnapshotImpl snapshot;
+    private final CaseInsensitiveStringMap scanOptions;
+
+    public DeltaKernelScanBuilder(CaseInsensitiveStringMap scanOptions, SnapshotImpl snapshot) {
+        this.snapshot = snapshot;
+        this.scanOptions = scanOptions;
+    }
+
+    @Override
+    public org.apache.spark.sql.connector.read.Scan build() {
+        throw new UnsupportedOperationException("batch read is not supported");
+    }
 }
