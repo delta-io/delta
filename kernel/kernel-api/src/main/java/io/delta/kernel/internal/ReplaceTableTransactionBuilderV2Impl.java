4f0967875b8090a7654866fb72609ea58a0c7646
Scott Sandre <scott.sandre@databricks.com>
2025-09-04 17:27:11 +0000

    handle ICT disblement + test

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/ReplaceTableTransactionBuilderV2Impl.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/ReplaceTableTransactionBuilderV2Impl.java
@@ -27,11 +27,40 @@ import io.delta.kernel.internal.tablefeatures.TableFeatures;
 import io.delta.kernel.transaction.DataLayoutSpec;
 import io.delta.kernel.transaction.ReplaceTableTransactionBuilder;
 import io.delta.kernel.types.StructType;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class ReplaceTableTransactionBuilderV2Impl implements ReplaceTableTransactionBuilder {
+
+  /**
+   * Delta-specific properties that should be preserved during REPLACE operations, unless their
+   * value is specifically set (overridden) during the REPLACE. All other properties should be
+   * reset.
+   *
+   * <p>For example, suppose at the time of REPLACE the table has property 'delta.foo' = 'bar' and
+   * that such property is included in this set.
+   *
+   * <ul>
+   *   <li>If the REPLACE statement does not specify 'delta.foo', then the new table will still have
+   *       'delta.foo' = 'bar'.
+   *   <li>If the REPLACE statement specifies 'delta.foo' = 'baz', then the new table will of course
+   *       have 'delta.foo' = 'baz'.
+   * </ul>
+   */
+  static final Set<String> TABLE_PROPERTY_KEYS_TO_PRESERVE =
+      new HashSet<String>() {
+        {
+          add(TableConfig.COLUMN_MAPPING_MAX_COLUMN_ID.getKey());
+
+          // Must retail all ICT properties, else a client would not know when ICT was enabled,
+          // which could result in a failed query or incorrect results.
+          //
+          // If ICT is explicitly disabled during REPLACE (or during any operation), we should then
+          // explicitly remove the ICT enablement version and timestamp properties.
+          add(TableConfig.IN_COMMIT_TIMESTAMPS_ENABLED.getKey());
+          add(TableConfig.IN_COMMIT_TIMESTAMP_ENABLEMENT_VERSION.getKey());
+          add(TableConfig.IN_COMMIT_TIMESTAMP_ENABLEMENT_TIMESTAMP.getKey());
+        }
+      };
 
   private final SnapshotImpl snapshot;
   private final StructType schema;
--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/InCommitTimestampSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/InCommitTimestampSuite.scala
@@ -42,6 +42,7 @@ import io.delta.kernel.utils.FileStatus
 
 import org.scalatest.funsuite.AnyFunSuite
 
+// TODO: Have this suite run using both the old and new Table APIs
 class InCommitTimestampSuite extends AnyFunSuite with WriteUtils {
 
   private def removeCommitInfoFromCommit(engine: Engine, version: Long, logPath: Path): Unit = {
@@ -577,6 +578,53 @@ class InCommitTimestampSuite extends AnyFunSuite with WriteUtils {
       assert(ex.getMessage.contains(String.format("Transaction has encountered a conflict and " +
         "can not be committed. Query needs to be re-executed using the latest version of the " +
         "table.")))
+    }
+  }
+
+  test("Disabling ICT removes enablement tracking properties") {
+    withTempDirAndEngine { (tablePath, engine) =>
+      // ===== GIVEN =====
+      // Create table without ICT. Note that this does not add ICT enablement tracking properties.
+      val txn1 = getCreateTxn(engine, tablePath, testSchema)
+      txn1.commit(engine, emptyIterable())
+
+      // Enable ICT. This should add enablement tracking properties.
+      setTablePropAndVerify(
+        engine = engine,
+        tablePath = tablePath,
+        isNewTable = false,
+        key = IN_COMMIT_TIMESTAMPS_ENABLED,
+        value = "true",
+        expectedValue = true)
+
+      val snapshotV1 = getTableManagerAdapter.getSnapshotAtLatest(engine, tablePath)
+
+      // Verify enablement properties are present
+      assertMetadataProp(snapshotV1, IN_COMMIT_TIMESTAMPS_ENABLED, true)
+      assertMetadataProp(
+        snapshotV1,
+        IN_COMMIT_TIMESTAMP_ENABLEMENT_TIMESTAMP,
+        Optional.of(snapshotV1.getTimestamp(engine)))
+      assertMetadataProp(
+        snapshotV1,
+        IN_COMMIT_TIMESTAMP_ENABLEMENT_VERSION,
+        Optional.of(1L))
+
+      // ===== WHEN =====
+      // Disable ICT. This should remove enablement tracking properties.
+      setTablePropAndVerify(
+        engine = engine,
+        tablePath = tablePath,
+        isNewTable = false,
+        key = IN_COMMIT_TIMESTAMPS_ENABLED,
+        value = "false",
+        expectedValue = false)
+
+      // ===== THEN =====
+      val snapshotV2 = getTableManagerAdapter.getSnapshotAtLatest(engine, tablePath)
+      assertMetadataProp(snapshotV2, IN_COMMIT_TIMESTAMPS_ENABLED, false)
+      assertHasNoMetadataProp(snapshotV2, IN_COMMIT_TIMESTAMP_ENABLEMENT_TIMESTAMP)
+      assertHasNoMetadataProp(snapshotV2, IN_COMMIT_TIMESTAMP_ENABLEMENT_VERSION)
     }
   }
 }
