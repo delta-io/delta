ca338a961f495eb96ae3a88b3091e32b0cf8f43e
Oussama Saoudi <oussama.sa216@gmail.com>
2025-09-18 18:56:52 -0700

    address comments

--- kernel/kernel-defaults/src/test/java/io/delta/kernel/defaults/benchmarks/WorkloadSpec.java
+++ kernel/kernel-defaults/src/test/java/io/delta/kernel/defaults/benchmarks/WorkloadSpec.java
@@ -16,28 +16,94 @@
 
 package io.delta.kernel.defaults.benchmarks;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import java.nio.file.Path;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.delta.kernel.engine.Engine;
+import java.io.File;
+import java.io.IOException;
 
-/** Base class for all workload specifications. */
+/**
+ * Base class for all workload specifications. Workload specifications define workloads and their
+ * parameters. These workloads can then be executed as benchmarks using the corresponding
+ * [[WorkloadRunner]].
+ *
+ * <p>This class uses Jackson annotations to support polymorphic deserialization based on the "type"
+ * field in the JSON.
+ */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({@JsonSubTypes.Type(value = ReadMetadata.class, name = "read_metadata")})
+@JsonSubTypes({@JsonSubTypes.Type(value = ReadMetadataSpec.class, name = "read")})
 public abstract class WorkloadSpec {
-  // The type of workload (e.g., "read_metadata") This is used by Jackson's polymorphic
-  // deserialization to automatically instantiate the correct subclass based on the "type" field in
-  // JSON.
+  /**
+   * The type of workload (e.g., "read_metadata") This is used by Jackson's polymorphic
+   * deserialization to automatically instantiate the correct subclass based on the "type" field in
+   * the JSON.
+   */
   private String type;
 
+  /** The name of the workload */
+  @JsonProperty("name")
+  public String name;
+
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  /** Default constructor for Jackson */
   protected WorkloadSpec() {}
 
   protected WorkloadSpec(String type) {
     this.type = type;
   }
 
+  /** @return the type of this workload. */
   public String getType() {
     return type;
   }
 
-  public abstract WorkloadRunner getRunner(Path baseWorkloadDirPath);
+  /**
+   * Creates a WorkloadRunner for this workload specification.
+   *
+   * @param baseWorkloadDirPath The base directory containing workload tables. This is used to
+   *     resolve relative table paths if present.
+   * @param engine The engine to use for executing the workload.
+   * @return the WorkloadRunner instance for this workload specification.
+   */
+  public abstract WorkloadRunner getRunner(String baseWorkloadDirPath, Engine engine);
+
+  // JSON Serialization/Deserialization
+
+  /**
+   * Loads a WorkloadSpec from the given JSON file path.
+   *
+   * @param workloadPath the path to the JSON file containing the workload specification.
+   * @return the WorkloadSpec instance parsed from the JSON file.
+   * @throws IOException if there is an error reading or parsing the file.
+   */
+  public static WorkloadSpec fromJsonPath(String workloadPath) throws IOException {
+    return objectMapper.readValue(new File(workloadPath), WorkloadSpec.class);
+  }
+
+  /**
+   * Loads a WorkloadSpec from the given JSON string.
+   *
+   * @param json the JSON string containing the workload specification.
+   * @return the WorkloadSpec instance parsed from the JSON string.
+   * @throws IOException if there is an error parsing the JSON.
+   */
+  public static WorkloadSpec fromJsonString(String json) throws IOException {
+    return objectMapper.readValue(json, WorkloadSpec.class);
+  }
+
+  /**
+   * Serializes this WorkloadSpec to a pretty-printed JSON string.
+   *
+   * @return the JSON string representation of this WorkloadSpec.
+   */
+  public String toJsonString() {
+    try {
+      return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to serialize WorkloadSpec to JSON", e);
+    }
+  }
 }
