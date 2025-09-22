34b0fb9f325a7ff93f0f50c89707429b7fa6b267
Allison Portis <allison.portis@databricks.com>
2025-09-22 14:49:00 -0700

    E2E test

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/catalogManaged/CatalogManagedE2EReadSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/catalogManaged/CatalogManagedE2EReadSuite.scala
@@ -16,11 +16,16 @@
 
 package io.delta.kernel.defaults.catalogManaged
 
+import java.util.Optional
+
 import scala.collection.JavaConverters._
 
 import io.delta.kernel.{SnapshotBuilder, TableManager}
 import io.delta.kernel.defaults.utils.{TestRow, TestUtilsWithTableManagerAPIs}
-import io.delta.kernel.internal.files.ParsedLogData
+import io.delta.kernel.exceptions.KernelException
+import io.delta.kernel.internal.DeltaHistoryManager
+import io.delta.kernel.internal.files.{ParsedDeltaData, ParsedLogData}
+import io.delta.kernel.internal.fs.Path
 import io.delta.kernel.internal.table.SnapshotBuilderImpl
 import io.delta.kernel.internal.tablefeatures.TableFeatures.{CATALOG_MANAGED_R_W_FEATURE_PREVIEW, IN_COMMIT_TIMESTAMP_W_FEATURE, TABLE_FEATURES_MIN_READER_VERSION, TABLE_FEATURES_MIN_WRITER_VERSION}
 import io.delta.kernel.utils.FileStatus
@@ -85,5 +90,89 @@ class CatalogManagedE2EReadSuite extends AnyFunSuite with TestUtilsWithTableMana
     val actualResult = readSnapshot(snapshot)
     val expectedResult = (0 to 199).map { x => TestRow(x / 100, x) }
     checkAnswer(actualResult, expectedResult)
+  }
+
+  test("e2e DeltaHistoryManager.getActiveCommitAtTimestamp with catalogOwned-preview table " +
+    "with staged ratified commits") {
+    // ===== GIVEN =====
+    val tablePath = getTestResourceFilePath("catalog-owned-preview")
+    val logPath = new Path(tablePath, "_delta_log")
+
+    // Note: We need to *resolve* each test resource file path, because the table root file path
+    //       will itself be resolved when we create the Snapshot. If we resolved some paths but
+    //       not others, we would get an error like `File <commit-file> doesn't belong in the
+    //       transaction log at <log-path>`.
+
+    val parsedDeltaData = Seq(
+      // scalastyle:off line.size.limit
+      getTestResourceFilePath("catalog-owned-preview/_delta_log/_staged_commits/00000000000000000001.4cb9708e-b478-44de-b203-53f9ba9b2876.json"),
+      getTestResourceFilePath("catalog-owned-preview/_delta_log/_staged_commits/00000000000000000002.5b9bba4a-0085-430d-a65e-b0d38c1afbe9.json"))
+      // scalastyle:on line.size.limit
+      .map { path => defaultEngine.getFileSystemClient.resolvePath(path) }
+      .map { p => FileStatus.of(p) }
+      .map { fs => ParsedDeltaData.forFileStatus(fs) }
+      .toList
+
+    val latestSnapshot = TableManager
+      .loadSnapshot(tablePath)
+      .asInstanceOf[SnapshotBuilderImpl]
+      .withLogData(parsedDeltaData.map(_.asInstanceOf[ParsedLogData]).asJava)
+      .build(defaultEngine)
+
+    def checkGetActiveCommitAtTimestamp(
+        timestamp: Long,
+        expectedVersion: Long,
+        canReturnLastCommit: Boolean = false,
+        canReturnEarliestCommit: Boolean = false): Unit = {
+      val activeCommit = DeltaHistoryManager.getActiveCommitAtTimestamp(
+        defaultEngine,
+        latestSnapshot,
+        logPath,
+        timestamp,
+        true, /* mustBeRecreatable */
+        canReturnLastCommit,
+        canReturnEarliestCommit,
+        Optional.of(parsedDeltaData.asJava))
+      assert(activeCommit.getVersion == expectedVersion)
+    }
+
+    // ===== WHEN =====
+    val v0Ts = 1749830855993L // published commit
+    val v1Ts = 1749830871085L // staged commit
+    val v2Ts = 1749830881799L // staged commit
+
+    // ===== THEN =====
+    // Query a timestamp before V0 should fail if canReturnEarliestCommit = false
+    val e1 = intercept[KernelException] {
+      checkGetActiveCommitAtTimestamp(v0Ts - 1, 0)
+    }
+    assert(e1.getMessage.contains("before the earliest available version"))
+
+    // Query a timestamp before V0 with canReturnEarliestCommit = true
+    checkGetActiveCommitAtTimestamp(v0Ts - 1, 0, canReturnEarliestCommit = true)
+
+    // Query @ V0
+    checkGetActiveCommitAtTimestamp(v0Ts, 0)
+
+    // Query between V0 and V1
+    checkGetActiveCommitAtTimestamp(v0Ts + 1, 0)
+
+    // Query at V1
+    checkGetActiveCommitAtTimestamp(v1Ts, 1)
+
+    // Query between V1 and V2
+    checkGetActiveCommitAtTimestamp(v1Ts + 1, 1)
+
+    // Query at V2
+    checkGetActiveCommitAtTimestamp(v2Ts, 2)
+
+    // Query a timestamp after V2 should fail with canReturnLastCommit = false
+    val e2 = intercept[KernelException] {
+      checkGetActiveCommitAtTimestamp(v2Ts + 1, 2)
+    }
+    assert(e2.getMessage.contains("is after the latest available version"))
+
+    // Query a timestamp after V2 with canReturnLastCommit = true
+    checkGetActiveCommitAtTimestamp(v2Ts + 1, 2, canReturnLastCommit = true)
   }
 }
