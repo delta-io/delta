b8ec65462378cfb3299b2f469564b43eceb71177
yhuang-db <itisyuchuan@gmail.com>
2025-10-01 13:50:19 -0700

    reformat SparkScanTest

--- kernel-spark/src/test/java/io/delta/kernel/spark/read/SparkScanTest.java
+++ kernel-spark/src/test/java/io/delta/kernel/spark/read/SparkScanTest.java
@@ -3,6 +3,7 @@ package io.delta.kernel.spark.read;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.delta.kernel.spark.SparkDsv2TestBase;
+import io.delta.kernel.spark.table.SparkTable;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -10,8 +11,10 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import org.apache.spark.sql.connector.catalog.Identifier;
+import org.apache.spark.sql.connector.expressions.Expression;
 import org.apache.spark.sql.connector.expressions.FieldReference;
 import org.apache.spark.sql.connector.expressions.LiteralValue;
+import org.apache.spark.sql.connector.expressions.filter.Predicate;
 import org.apache.spark.sql.connector.read.Scan;
 import org.apache.spark.sql.connector.read.ScanBuilder;
 import org.apache.spark.sql.execution.datasources.PartitionedFile;
@@ -24,69 +27,64 @@ import org.junit.jupiter.api.io.TempDir;
 public class SparkScanTest extends SparkDsv2TestBase {
 
   private static String tablePath;
-  private static final String tableName = "deltatbl_partition_prune";
+  private static final String tableName = "deltatbl_partitioned";
 
   @BeforeAll
-  public static void setupGoldenTable(@TempDir File tempDir) {
+  public static void setupPartitionedTable(@TempDir File tempDir) {
     createPartitionedTable(tableName, tempDir.getAbsolutePath());
     tablePath = tempDir.getAbsolutePath();
   }
 
   private final CaseInsensitiveStringMap options =
-      new CaseInsensitiveStringMap(
-          new java.util.HashMap<String, String>() {
-            {
-            }
-          });
-  private final io.delta.kernel.spark.table.SparkTable table =
-      new io.delta.kernel.spark.table.SparkTable(
+      new CaseInsensitiveStringMap(new java.util.HashMap<>());
+
+  private final SparkTable table =
+      new SparkTable(
           Identifier.of(new String[] {"spark_catalog", "default"}, tableName), tablePath, options);
 
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate cityPredicate =
-      new org.apache.spark.sql.connector.expressions.filter.Predicate(
+  protected static final Predicate cityPredicate =
+      new Predicate(
           "=",
-          new org.apache.spark.sql.connector.expressions.Expression[] {
+          new Expression[] {
             FieldReference.apply("city"), LiteralValue.apply("hz", DataTypes.StringType)
           });
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate datePredicate =
-      new org.apache.spark.sql.connector.expressions.filter.Predicate(
+
+  protected static final Predicate datePredicate =
+      new Predicate(
           "=",
-          new org.apache.spark.sql.connector.expressions.Expression[] {
+          new Expression[] {
             FieldReference.apply("date"), LiteralValue.apply("20180520", DataTypes.StringType)
           });
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate partPredicate =
-      new org.apache.spark.sql.connector.expressions.filter.Predicate(
+
+  protected static final Predicate partPredicate =
+      new Predicate(
           ">",
-          new org.apache.spark.sql.connector.expressions.Expression[] {
+          new Expression[] {
             FieldReference.apply("part"), LiteralValue.apply(1, DataTypes.IntegerType)
           });
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate dataPredicate =
-      new org.apache.spark.sql.connector.expressions.filter.Predicate(
+
+  protected static final Predicate dataPredicate =
+      new Predicate(
           ">",
-          new org.apache.spark.sql.connector.expressions.Expression[] {
+          new Expression[] {
             FieldReference.apply("cnt"), LiteralValue.apply(10, DataTypes.IntegerType)
           });
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate
-      negativeCityPredicate =
-          new org.apache.spark.sql.connector.expressions.filter.Predicate(
-              "=",
-              new org.apache.spark.sql.connector.expressions.Expression[] {
-                FieldReference.apply("city"), LiteralValue.apply("zz", DataTypes.StringType)
-              });
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate
-      interColPredicate =
-          new org.apache.spark.sql.connector.expressions.filter.Predicate(
-              "!=",
-              new org.apache.spark.sql.connector.expressions.Expression[] {
-                FieldReference.apply("city"), FieldReference.apply("date")
-              });
-  protected static final org.apache.spark.sql.connector.expressions.filter.Predicate
-      negativeInterColPredicate =
-          new org.apache.spark.sql.connector.expressions.filter.Predicate(
-              "=",
-              new org.apache.spark.sql.connector.expressions.Expression[] {
-                FieldReference.apply("city"), FieldReference.apply("date")
-              });
+
+  protected static final Predicate negativeCityPredicate =
+      new Predicate(
+          "=",
+          new Expression[] {
+            FieldReference.apply("city"), LiteralValue.apply("zz", DataTypes.StringType)
+          });
+
+  protected static final Predicate interColPredicate =
+      new Predicate(
+          "!=", new Expression[] {FieldReference.apply("city"), FieldReference.apply("date")});
+
+  protected static final Predicate negativeInterColPredicate =
+      new Predicate(
+          "=", new Expression[] {FieldReference.apply("city"), FieldReference.apply("date")});
+
   // a full set of cities in the golden table, repsents all partitions
   protected static final List<String> allCities =
       Arrays.asList("city=hz", "city=sh", "city=bj", "city=sz");
@@ -94,16 +92,13 @@ public class SparkScanTest extends SparkDsv2TestBase {
   @Test
   public void testDPP_singleFilter() throws Exception {
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {cityPredicate},
-        Arrays.asList("city=hz"));
+        table, options, new Predicate[] {cityPredicate}, Arrays.asList("city=hz"));
 
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {datePredicate},
-        Arrays.asList("date=20180520"));
+        table, options, new Predicate[] {datePredicate}, Arrays.asList("date=20180520"));
+
+    checkSupportsRuntimeFilters(
+        table, options, new Predicate[] {partPredicate}, Arrays.asList("part=2"));
   }
 
   @Test
@@ -111,191 +106,111 @@ public class SparkScanTest extends SparkDsv2TestBase {
     checkSupportsRuntimeFilters(
         table,
         options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {
-          cityPredicate, datePredicate
-        },
+        new Predicate[] {cityPredicate, datePredicate},
         Arrays.asList("date=20180520/city=hz"));
   }
 
   @Test
   public void testDPP_ANDFilters() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate andPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "AND",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
-              cityPredicate, datePredicate
-            });
+    Predicate andPredicate = new Predicate("AND", new Expression[] {cityPredicate, datePredicate});
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {andPredicate},
-        Arrays.asList("date=20180520/city=hz"));
+        table, options, new Predicate[] {andPredicate}, Arrays.asList("date=20180520/city=hz"));
   }
 
   @Test
   public void testDPP_ORFilters() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate orPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "OR",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
-              cityPredicate, datePredicate
-            });
+    Predicate orPredicate = new Predicate("OR", new Expression[] {cityPredicate, datePredicate});
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {orPredicate},
-        Arrays.asList("city=hz", "date=20180520"));
+        table, options, new Predicate[] {orPredicate}, Arrays.asList("city=hz", "date=20180520"));
   }
 
   @Test
   public void testDPP_NOTFilter() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate notPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "NOT", new org.apache.spark.sql.connector.expressions.Expression[] {cityPredicate});
+    Predicate notPredicate = new Predicate("NOT", new Expression[] {cityPredicate});
     checkSupportsRuntimeFilters(
         table,
         options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {notPredicate},
+        new Predicate[] {notPredicate},
         Arrays.asList("city=sh", "city=bj", "city=sz"));
   }
 
   @Test
   public void testDPP_INFilter() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate inPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
+    Predicate inPredicate =
+        new Predicate(
             "IN",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
+            new Expression[] {
               FieldReference.apply("city"),
               LiteralValue.apply("hz", DataTypes.StringType),
               LiteralValue.apply("sh", DataTypes.StringType)
             });
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {inPredicate},
-        Arrays.asList("city=hz", "city=sh"));
+        table, options, new Predicate[] {inPredicate}, Arrays.asList("city=hz", "city=sh"));
   }
 
   @Test
   public void testDPP_negativeFilter() throws Exception {
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {negativeCityPredicate},
-        Arrays.asList());
+        table, options, new Predicate[] {negativeCityPredicate}, Arrays.asList());
   }
 
   @Test
   public void testDPP_ANDNegativeFilter() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate andPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "AND",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
-              cityPredicate, negativeCityPredicate
-            });
-    checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {andPredicate},
-        Arrays.asList());
+    Predicate andPredicate =
+        new Predicate("AND", new Expression[] {cityPredicate, negativeCityPredicate});
+    checkSupportsRuntimeFilters(table, options, new Predicate[] {andPredicate}, Arrays.asList());
   }
 
   @Test
   public void testDPP_ORNegativeFilter() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate orPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "OR",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
-              cityPredicate, negativeCityPredicate
-            });
+    Predicate orPredicate =
+        new Predicate("OR", new Expression[] {cityPredicate, negativeCityPredicate});
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {orPredicate},
-        Arrays.asList("city=hz"));
+        table, options, new Predicate[] {orPredicate}, Arrays.asList("city=hz"));
   }
 
   @Test
   public void testDPP_nonPartitionColumnFilter() throws Exception {
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {
-          cityPredicate, dataPredicate
-        },
-        Arrays.asList("city=hz"));
+        table, options, new Predicate[] {cityPredicate, dataPredicate}, Arrays.asList("city=hz"));
   }
 
   @Test
   public void testDPP_nonPartitionColumnFilterOnly() throws Exception {
-    checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {dataPredicate},
-        allCities);
+    checkSupportsRuntimeFilters(table, options, new Predicate[] {dataPredicate}, allCities);
   }
 
   @Test
   public void testDPP_ANDDataPredicate() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate andPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "AND",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
-              cityPredicate, dataPredicate
-            });
-    checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {andPredicate},
-        allCities);
+    Predicate andPredicate = new Predicate("AND", new Expression[] {cityPredicate, dataPredicate});
+    checkSupportsRuntimeFilters(table, options, new Predicate[] {andPredicate}, allCities);
   }
 
   @Test
   public void testDPP_ORDataPredicate() throws Exception {
-    org.apache.spark.sql.connector.expressions.filter.Predicate orPredicate =
-        new org.apache.spark.sql.connector.expressions.filter.Predicate(
-            "OR",
-            new org.apache.spark.sql.connector.expressions.Expression[] {
-              cityPredicate, dataPredicate
-            });
-    checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {orPredicate},
-        allCities);
+    Predicate orPredicate = new Predicate("OR", new Expression[] {cityPredicate, dataPredicate});
+    checkSupportsRuntimeFilters(table, options, new Predicate[] {orPredicate}, allCities);
   }
 
   @Test
   public void testDPP_interColumnFilter() throws Exception {
-    checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {interColPredicate},
-        allCities);
+    checkSupportsRuntimeFilters(table, options, new Predicate[] {interColPredicate}, allCities);
   }
 
   @Test
   public void testDPP_negativeInterColumnFilter() throws Exception {
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {
-          negativeInterColPredicate
-        },
-        Arrays.asList());
+        table, options, new Predicate[] {negativeInterColPredicate}, Arrays.asList());
   }
 
   @Test
   public void testDPP_integerFilter() throws Exception {
     checkSupportsRuntimeFilters(
-        table,
-        options,
-        new org.apache.spark.sql.connector.expressions.filter.Predicate[] {partPredicate},
-        Arrays.asList("part=2"));
+        table, options, new Predicate[] {partPredicate}, Arrays.asList("part=2"));
   }
 
   protected static void checkSupportsRuntimeFilters(
-      io.delta.kernel.spark.table.SparkTable table,
+      SparkTable table,
       CaseInsensitiveStringMap scanOptions,
       org.apache.spark.sql.connector.expressions.filter.Predicate[] runtimeFilters,
       List<String> remainingPartitionValueAfterDpp)
