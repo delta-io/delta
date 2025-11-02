816f8cb828a906858c4b1d7655a04fd2a087b4ef
Harish Kesava Rao <harish.kesavarao@gmail.com>
2025-11-02 19:15:40 +0530

    Fix for test assertion wording mismatch

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DeltaTableReadsSuite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/DeltaTableReadsSuite.scala
@@ -821,7 +821,11 @@ trait AbstractDeltaTableReadsSuite extends AnyFunSuite { self: AbstractTestUtils
     val e = intercept[InvalidTableException] {
       latestSnapshot(goldenTablePath("versions-not-contiguous"))
     }
-    assert(e.getMessage.contains("versions are not contiguous: ([0, 2])"))
+    // Accept both old and new error message formats for non-contiguous versions
+    assert(
+      e.getMessage.contains("versions are not contiguous") && 
+      e.getMessage.contains("[0, 2]")
+    )
   }
 
   test("table protocol version greater than reader protocol version") {
