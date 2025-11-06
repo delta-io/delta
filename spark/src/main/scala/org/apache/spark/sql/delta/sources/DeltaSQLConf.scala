cac76269090de5eb52ba5a773f75cee4760b7b1c
Julek Sompolski <Juliusz Sompolski>
2025-11-06 17:00:56 +0100

    a

--- spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaSQLConf.scala
@@ -1042,13 +1042,6 @@ trait DeltaSQLConfBase extends DeltaSQLConfUtils {
       .intConf
       .createWithDefault(4)
 
-  val MERGE_MATERIALIZE_SOURCE_EAGER =
-    buildConf("merge.materializeSource.eager")
-      .internal()
-      .doc("Materialize the source eagerly before Job 1")
-      .booleanConf
-      .createWithDefault(true)
-
   val DELTA_LAST_COMMIT_VERSION_IN_SESSION =
     buildConf("lastCommitVersionInSession")
       .doc("The version of the last commit made in the SparkSession for any table.")
