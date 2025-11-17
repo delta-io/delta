2a642d6b7a68eff552fd4b0eb2fae8575075e600
ilicmarkodb <marko.ilic@databricks.com>
2025-11-17 23:52:07 +0100

    change equals

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/actions/DomainMetadata.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/actions/DomainMetadata.java
@@ -86,7 +86,7 @@ public class DomainMetadata {
       return null;
     }
     checkArgument(
-        row.getSchema().equals(FULL_SCHEMA),
+        row.getSchema().equivalentIgnoreCollations(FULL_SCHEMA),
         "Expected schema: %s, found: %s",
         FULL_SCHEMA,
         row.getSchema());
