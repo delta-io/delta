32ec2907cafd29c2e554a45e45e10a92806cb552
ilicmarkodb <marko.ilic@databricks.com>
2025-11-18 00:36:17 +0100

    fix

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/tablefeatures/TableFeatures.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/tablefeatures/TableFeatures.java
@@ -846,7 +846,6 @@ public class TableFeatures {
   private static boolean hasTypeColumn(StructType tableSchema, DataType type) {
     return new SchemaIterable(tableSchema)
         .stream()
-            .anyMatch(
-                element -> element.getField().getDataType().equivalentIgnoreCollationss(type));
+            .anyMatch(element -> element.getField().getDataType().equivalentIgnoreCollations(type));
   }
 }
