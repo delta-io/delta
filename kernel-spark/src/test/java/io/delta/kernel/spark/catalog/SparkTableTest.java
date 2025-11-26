c0424058d34c28ac9aa33acbee20278566265162
Gengliang Wang <gengliang@apache.org>
2025-11-26 12:02:53 -0800

    renames

--- kernel-spark/src/test/java/io/delta/kernel/spark/catalog/SparkTableTest.java
+++ kernel-spark/src/test/java/io/delta/kernel/spark/catalog/SparkTableTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.delta.kernel.spark.table;
+package io.delta.kernel.spark.catalog;
 
 import static org.apache.spark.sql.connector.catalog.TableCapability.BATCH_READ;
 import static org.junit.jupiter.api.Assertions.assertEquals;
