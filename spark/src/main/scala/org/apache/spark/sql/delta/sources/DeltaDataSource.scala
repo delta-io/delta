ed86909232bfc374647dc994da601d759b262e03
Juliusz Sompolski <Juliusz Sompolski>
2025-11-27 14:24:09 +0000

    Revert "update to option on table"
    This reverts commit 8caa9d6bb9abaf2779870c0c662ab7dc729d43a8.

--- spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaDataSource.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/sources/DeltaDataSource.scala
@@ -40,7 +40,7 @@ import org.apache.spark.sql.catalyst.catalog.CatalogTable
 import org.apache.spark.sql.catalyst.expressions.{EqualTo, Expression, Literal}
 import org.apache.spark.sql.catalyst.plans.logical.SubqueryAlias
 import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
-import org.apache.spark.sql.connector.catalog.{Table, TableProvider}
+import org.apache.spark.sql.connector.catalog.{RequiresDataFrameWriterV1SaveAsTableOverwriteWriteOption, Table, TableProvider}
 import org.apache.spark.sql.connector.expressions.Transform
 import org.apache.spark.sql.execution.streaming.{Sink, Source}
 import org.apache.spark.sql.sources._
@@ -57,6 +57,7 @@ class DeltaDataSource
   with CreatableRelationProvider
   with DataSourceRegister
   with TableProvider
+  with RequiresDataFrameWriterV1SaveAsTableOverwriteWriteOption
   with DeltaLogging {
 
   /**
