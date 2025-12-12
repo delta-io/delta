cc8ef13689aed17e48f136679adb918d8aa20800
Allison Portis <allison.portis@databricks.com>
2025-12-11 17:32:34 -0800

    Test compilation works

--- spark/src/test/scala/org/apache/spark/sql/delta/expressions/HilbertIndexSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/expressions/HilbertIndexSuite.scala
@@ -38,7 +38,7 @@ class HilbertIndexSuite extends SparkFunSuite {
     TestCase(6, 3)
   )
 
-  def gridTest[A](testNamePrefix: String, testTags: Tag*)(params: Seq[A])(
+  override def gridTest[A](testNamePrefix: String, testTags: Tag*)(params: Seq[A])(
     testFun: A => Unit): Unit = {
     for (param <- params) {
       test(testNamePrefix + s" ($param)", testTags: _*)(testFun(param))
