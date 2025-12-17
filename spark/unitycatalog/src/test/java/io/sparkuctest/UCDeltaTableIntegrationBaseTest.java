06c38b758b6f6862712591a7c1df1bc8ee9c8d94
openinx <openinx@gmail.com>
2025-12-16 17:16:21 -0800

    [Spark] Support the spark-uc tests run on various env.
    Signed-off-by: openinx <openinx@gmail.com>

--- spark/unitycatalog/src/test/java/io/sparkuctest/UCDeltaTableIntegrationBaseTest.java
+++ spark/unitycatalog/src/test/java/io/sparkuctest/UCDeltaTableIntegrationBaseTest.java
@@ -39,7 +39,6 @@ import org.junit.jupiter.api.BeforeAll;
  * <p>Subclasses must provide an executor by implementing the getSqlExecutor method.
  */
 public abstract class UCDeltaTableIntegrationBaseTest extends UnityCatalogSupport {
-
   /**
    * Provides all table types for parameterized tests. Tests can use this as a @MethodSource to test
    * different table types.
@@ -135,7 +134,10 @@ public abstract class UCDeltaTableIntegrationBaseTest extends UnityCatalogSuppor
     getSqlExecutor().checkWithSQL("SELECT * FROM " + tableName + " ORDER BY 1", expected);
   }
 
-  /** Helper method to run code with a temporary directory that gets cleaned up. */
+  /**
+   * Helper method to run code with a temporary directory that gets cleaned up. For local mode, uses
+   * local filesystem. For remote mode, uses cloud storage.
+   */
   protected void withTempDir(TempDirCode code) throws Exception {
     UnityCatalogInfo uc = unityCatalogInfo();
     Path tempDir = new Path(uc.baseTableLocation(), "temp-" + UUID.randomUUID());
