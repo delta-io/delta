cb0e5cf3d6765bc77b4943072848c0ebbb9c0f7e
openinx <openinx@gmail.com>
2025-12-18 10:41:42 -0800

    Add unit tests.
    Signed-off-by: openinx <openinx@gmail.com>

--- spark/unitycatalog/src/test/java/io/sparkuctest/UCRemoteTest.java
+++ spark/unitycatalog/src/test/java/io/sparkuctest/UCRemoteTest.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class RemoteUCTest extends UnityCatalogSupport {
+public class UCRemoteTest extends UnityCatalogSupport {
 
   private static final Map<String, String> ENV_VARS =
       ImmutableMap.of(
