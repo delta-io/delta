30be8731ffbb763fae83cab58cd4c5d590f0f478
ilicmarkodb <marko.ilic@databricks.com>
2025-12-30 15:43:54 +0100

    remove change for MapType key

--- kernel/kernel-api/src/main/java/io/delta/kernel/types/FieldMetadata.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/types/FieldMetadata.java
@@ -156,14 +156,11 @@ public final class FieldMetadata {
     }
     return filteredMetadata.entrySet().stream()
         .allMatch(
-            e ->
-                Objects.equals(e.getValue(), otherFilteredMetadata.get(e.getKey()))
-                    || (e.getValue() != null
-                        && e.getValue().getClass().isArray()
-                        && otherFilteredMetadata.get(e.getKey()).getClass().isArray()
-                        && Arrays.equals(
-                            (Object[]) e.getValue(),
-                            (Object[]) otherFilteredMetadata.get(e.getKey()))));
+            e -> {
+              Object value = e.getValue();
+              Object otherValue = otherFilteredMetadata.get(e.getKey());
+              return Objects.deepEquals(value, otherValue);
+            });
   }
 
   @Override
@@ -174,13 +171,11 @@ public final class FieldMetadata {
     if (this.metadata.size() != that.metadata.size()) return false;
     return this.metadata.entrySet().stream()
         .allMatch(
-            e ->
-                Objects.equals(e.getValue(), that.metadata.get(e.getKey()))
-                    || (e.getValue() != null
-                        && e.getValue().getClass().isArray()
-                        && that.metadata.get(e.getKey()).getClass().isArray()
-                        && Arrays.equals(
-                            (Object[]) e.getValue(), (Object[]) that.metadata.get(e.getKey()))));
+            e -> {
+              Object value = e.getValue();
+              Object otherValue = that.metadata.get(e.getKey());
+              return Objects.deepEquals(value, otherValue);
+            });
   }
 
   @Override
