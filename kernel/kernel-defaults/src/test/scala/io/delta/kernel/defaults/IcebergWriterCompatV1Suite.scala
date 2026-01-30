65a46f01497a65261313ee0f2f70163638ade04a
André Fonseca <1077309+andreAmorimF@users.noreply.github.com>
2026-01-30 17:18:21 -0300

    Iceberg compatibility test fix

--- kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/IcebergWriterCompatV1Suite.scala
+++ kernel/kernel-defaults/src/test/scala/io/delta/kernel/defaults/IcebergWriterCompatV1Suite.scala
@@ -470,7 +470,8 @@ trait IcebergWriterCompatV1SuiteBase
 
   testIncompatibleUnsupportedTableFeature(
     "changeDataFeed",
-    tablePropertiesToEnable = Map(TableConfig.CHANGE_DATA_FEED_ENABLED.getKey -> "true"))
+    tablePropertiesToEnable = Map(TableConfig.CHANGE_DATA_FEED_ENABLED.getKey -> "true"),
+    expectedErrorMessage = "Table features [changeDataFeed] are incompatible with icebergWriterCompatV1")
 
   testIncompatibleUnsupportedTableFeature(
     "invariants",
