8bb515e5c43363b7d6440629b9e51c2213e4b3ea
Felipe Fujiy Pessoto <fepessot@microsoft.com>
2026-02-28 00:10:10 +0000

    Fix tests.
    Signed-off-by: Felipe Fujiy Pessoto <fepessot@microsoft.com>

--- spark/src/test/scala/org/apache/spark/sql/delta/DeltaLogSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/DeltaLogSuite.scala
@@ -237,7 +237,7 @@ class DeltaLogSuite extends QueryTest
     }
   }
 
-  testQuietly("paths should be canonicalized") {
+  testQuietly("paths should not be canonicalized") {
     Seq("file:", "file://").foreach { scheme =>
       withTempDir { dir =>
         val log = DeltaLog.forTable(spark, dir)
@@ -246,7 +246,7 @@ class DeltaLogSuite extends QueryTest
         val add = AddFile(
           path, Map.empty, 100L, 10L, dataChange = true)
         val rm = RemoveFile(
-          s"$scheme$path", Some(200L), dataChange = false)
+          s"$path", Some(200L), dataChange = false)
 
         log.store.write(
           FileNames.unsafeDeltaFile(log.logPath, 0L),
@@ -268,7 +268,7 @@ class DeltaLogSuite extends QueryTest
     }
   }
 
-  testQuietly("paths should be canonicalized - special characters") {
+  testQuietly("paths should not be canonicalized - special characters") {
     Seq("file:", "file://").foreach { scheme =>
       withTempDir { dir =>
         val log = DeltaLog.forTable(spark, dir)
@@ -277,7 +277,7 @@ class DeltaLogSuite extends QueryTest
         val add = AddFile(
           path, Map.empty, 100L, 10L, dataChange = true)
         val rm = RemoveFile(
-          s"$scheme$path", Some(200L), dataChange = false)
+          s"$path", Some(200L), dataChange = false)
 
         log.store.write(
           FileNames.unsafeDeltaFile(log.logPath, 0L),
@@ -783,10 +783,11 @@ class DeltaLogSuite extends QueryTest
       val addFiles = Seq(
         AddFile(
           path = absolutePath,
-          partitionValues = Map(),
-          size = 128L,
+          partitionValues = Map.empty,
+          size = 1L,
           modificationTime = 1L,
-          dataChange = true
+          dataChange = true,
+          stats = "{\"numRecords\": 1}"
         ))
       val deltaLog = DeltaLog.forTable(spark, dir.getCanonicalPath)
       deltaLog.startTransaction().commit(addFiles, DeltaOperations.ManualUpdate)
