183ec8b9db4b82f5a532f1518540f1b7d1f7f317
Timothy Wang <timothy.art@gmail.com>
2026-03-06 09:21:54 +0000

    Use forked Unity Catalog branch in CI

--- project/scripts/setup_unitycatalog_main.sh
+++ project/scripts/setup_unitycatalog_main.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 UC_DIR="${UC_DIR:-/tmp/unitycatalog}"
 
 rm -rf "$UC_DIR"
-git clone https://github.com/unitycatalog/unitycatalog.git "$UC_DIR"
+git clone https://github.com/TimothyW553/unitycatalog.git "$UC_DIR"
 cd "$UC_DIR"
-git checkout main
+git checkout atomic-rtas
 ./build/sbt clean package publishM2
