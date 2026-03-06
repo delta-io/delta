59a87c234699b86d3f9e4263db5ff93e5370a46a
Timothy Wang <timothy.art@gmail.com>
2026-03-06 18:08:46 +0000

    Pin Unity Catalog CI setup to known good SHA

--- project/scripts/setup_unitycatalog_main.sh
+++ project/scripts/setup_unitycatalog_main.sh
@@ -3,9 +3,11 @@
 set -euo pipefail
 
 UC_DIR="${UC_DIR:-/tmp/unitycatalog}"
+UC_REPO="${UC_REPO:-https://github.com/TimothyW553/unitycatalog.git}"
+UC_REF="${UC_REF:-eea7e881c577439e9b0421d1293df630c36e2ef4}"
 
 rm -rf "$UC_DIR"
-git clone https://github.com/TimothyW553/unitycatalog.git "$UC_DIR"
+git clone "$UC_REPO" "$UC_DIR"
 cd "$UC_DIR"
-git checkout atomic-rtas
+git checkout "$UC_REF"
 ./build/sbt clean package publishM2
