c299b0f0a28586a43014dc75059f0d2db5041fc9
Vishnu Chandrashekhar <vishnu.c@databricks.com>
2026-03-31 18:17:23 -0700

    Harden 3p dependencies throughout project
    Cherry-pick of 6ac5ed5af from master with branch-specific trigger references.

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ MAJOR_VERSION = int(VERSION.split(".")[0])
 
 if MAJOR_VERSION < 4:
     packages_arg = ['delta', 'delta.exceptions']
-    install_requires_arg = ['pyspark>=3.5.2,<3.6.0', 'importlib_metadata>=1.0.0']
+    install_requires_arg = ['pyspark>=3.5.2,<3.6.0', 'importlib_metadata>=1.0.0,<=8.7.1']
     python_requires_arg = '>=3.6'
 else:  # MAJOR_VERSION >= 4
     # Delta 4.0+ contains Delta Connect code and uses Spark 4.0+
     packages_arg = ['delta', 'delta.connect', 'delta.connect.proto', 'delta.exceptions']
-    install_requires_arg = ['pyspark>=4.0.0', 'importlib_metadata>=1.0.0']
+    install_requires_arg = ['pyspark>=4.0.0,<4.1.0', 'importlib_metadata>=1.0.0,<=8.7.1']
     python_requires_arg = '>=3.9'
 
 class VerifyVersionCommand(install):
