f398f5f3d0777e80460de677cbf745543247e69c
You Zhou <98635051+PorridgeSwim@users.noreply.github.com>
2026-04-06 11:10:17 -0700

    [kernel-spark] Support ignoreFileDeletion read option in dsv2 (#6250)
    ## 🥞 Stacked PR
    Use this
    [link](https://github.com/delta-io/delta/pull/6250/files/7e400f87189bef892d3b0022c0aede238a0c84de..af230590d0d9c1a5c37cc6a7b6af404025a6d415)
    to review incremental changes.
    - [stack/ignoreDeletesV2](https://github.com/delta-io/delta/pull/6245)
    [[Files changed](https://github.com/delta-io/delta/pull/6245/files)]
    -
    [stack/skipChangeCommitsV2](https://github.com/delta-io/delta/pull/6246)
    [[Files
    changed](https://github.com/delta-io/delta/pull/6246/files/6e9962d4a30a63ed14786830bae2b668004a76c9..2e111cf6ac9d1e5f84d83d94412b05486f543613)]
    - [stack/ignoreChangesV2](https://github.com/delta-io/delta/pull/6249)
    [[Files
    changed](https://github.com/delta-io/delta/pull/6249/files/0d54da51e7eade47b8115d92aaf7be1e8e4c011f..7e400f87189bef892d3b0022c0aede238a0c84de)]
    -
    [**stack/ignoreFileDeletionV2**](https://github.com/delta-io/delta/pull/6250)
    [[Files
    changed](https://github.com/delta-io/delta/pull/6250/files/7e400f87189bef892d3b0022c0aede238a0c84de..af230590d0d9c1a5c37cc6a7b6af404025a6d415)]
    
    ---------
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md
    2. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      3. Be sure to keep the PR description updated to reflect all changes.
      4. Please write your PR title to summarize what this PR proposes.
    5. If possible, provide a concise example to reproduce the issue for a
    faster review.
    6. If applicable, include the corresponding issue number in the PR title
    and link it in the body.
    -->
    
    #### Which Delta project/connector is this regarding?
    <!--
    Please add the component selected below to the beginning of the pull
    request title
    For example: [Spark] Title of my pull request
    -->
    
    - [x] Spark
    - [ ] Standalone
    - [ ] Flink
    - [ ] Kernel
    - [ ] Other (fill in here)
    
    ## Description
    Support ignoreFileDeletion read option is DSv2, which skip a all remove
    file actions but keep the add file actions in the same commit. And also
    inform user that this read option has deprecated.
    <!--
    - Describe what this PR changes.
    - Describe why we need the change.
     
    If this PR resolves an issue be sure to include "Resolves #XXX" to
    correctly link and close the issue upon merge.
    -->
    
    ## How was this patch tested?
    Unit tests that test parity between v1 and v2 connector on both pure
    deletes commit (only remove) and change commit (add + remove).
    
    Integration tests:
    - streaming with ignoreFileDeletion = true allows both delete and change
    commits
    
    DV tests:
     - multiple deletion vectors per file - List((ignoreFileDeletion,true))
    - allow to delete files after staring a streaming query when
    ignoreFileDeletion is true
    <!--
    If tests were added, say they were added here. Please make sure to test
    the changes thoroughly including negative and positive cases if
    possible.
    If the changes were tested in any way other than unit tests, please
    clarify how you tested step by step (ideally copy and paste-able, so
    that other reviewers can test and check, and descendants can verify in
    the future).
    If the changes were not tested, please explain why.
    -->
    
    ## Does this PR introduce _any_ user-facing changes?
    No
    <!--
    If yes, please clarify the previous behavior and the change this PR
    proposes - provide the console output, description and/or an example to
    show the behavior difference if possible.
    If possible, please also clarify if this is a user-facing change
    compared to the released Delta Lake versions or within the unreleased
    branches such as master.
    If no, write 'No'.
    -->

--- spark-unified/src/test/scala/org/apache/spark/sql/delta/test/DeltaV2SourceSuite.scala
+++ spark-unified/src/test/scala/org/apache/spark/sql/delta/test/DeltaV2SourceSuite.scala
@@ -73,6 +73,7 @@ class DeltaV2SourceSuite extends DeltaSourceSuite with V2ForceTest {
     "streaming with ignoreDeletes = true still fails on change commits",
     "streaming with skipChangeCommits = true skips both delete and change commits",
     "streaming with ignoreChanges = true allows both delete and change commits",
+    "streaming with ignoreFileDeletion = true allows both delete and change commits",
 
     // ========== startingVersion option tests ==========
     "startingVersion",
