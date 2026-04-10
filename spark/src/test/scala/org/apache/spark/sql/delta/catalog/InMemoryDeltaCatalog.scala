e6329e8d37297aab1e99307dec604ee5ea7ce62a
Leonid Lygin <ionagamed@gmail.com>
2026-04-10 13:18:33 +0000

    add InMemorySparkTable

---
+++ spark/src/test/scala/org/apache/spark/sql/delta/catalog/InMemoryDeltaCatalog.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright (2025) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.delta.catalog
+
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.jdk.CollectionConverters._
+
+import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.connector.catalog.{Identifier, Table}
+import org.apache.spark.sql.connector.expressions.Transform
+
+/**
+ * Test-only catalog that extends [[DeltaCatalog]] and overrides [[loadCatalogTable]]
+ * to return [[InMemorySparkTable]] instead of SparkTable or DeltaTableV2.
+ *
+ * This allows testing DSv2 DML operations (INSERT, DELETE, UPDATE, MERGE) through
+ * Delta's catalog without requiring a fully functional SparkTable write implementation.
+ *
+ * Table creation still goes through the normal Delta path (creating real Delta tables
+ * on disk for schema resolution), but subsequent table loads return cached in-memory
+ * tables that support all V2 operations.
+ */
+class InMemoryDeltaCatalog extends DeltaCatalog {
+  override def loadCatalogTable(ident: Identifier, catalogTable: CatalogTable): Table = {
+    InMemoryDeltaCatalog.getOrCreateTable(ident, catalogTable, spark)
+  }
+}
+
+object InMemoryDeltaCatalog {
+  private val tableCache = new ConcurrentHashMap[String, InMemorySparkTable]()
+
+  def getOrCreateTable(
+      ident: Identifier,
+      catalogTable: CatalogTable,
+      spark: SparkSession): InMemorySparkTable = {
+    val tablePath = catalogTable.location.toString
+    tableCache.computeIfAbsent(tablePath, _ => {
+      val deltaTable = DeltaTableV2(
+        spark, new Path(catalogTable.location), catalogTable = Some(catalogTable))
+      val tableName = s"${ident.namespace().mkString(".")}.${ident.name()}"
+      val props = new java.util.HashMap[String, String](catalogTable.properties.asJava)
+      props.put("supports-deltas", "true")
+      new InMemorySparkTable(
+        tableName,
+        deltaTable.schema(),
+        Array.empty[Transform],
+        props)
+    })
+  }
+
+  def reset(): Unit = tableCache.clear()
+}
---
+++ spark/src/test/scala/org/apache/spark/sql/delta/catalog/InMemorySparkTable.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright (2025) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.delta.catalog
+
+import java.util
+
+import org.apache.spark.sql.connector.catalog.InMemoryRowLevelOperationTable
+import org.apache.spark.sql.connector.catalog.constraints.Constraint
+import org.apache.spark.sql.connector.expressions.Transform
+import org.apache.spark.sql.sources.Filter
+import org.apache.spark.sql.types.StructType
+
+/**
+ * In-memory DSv2 table used as a test stand-in for SparkTable (the Kernel-based Delta V2
+ * connector).
+ *
+ * Created by [[InMemoryDeltaCatalog]] when used as the session catalog in tests.
+ */
+class InMemorySparkTable(
+    name: String,
+    schema: StructType,
+    partitioning: Array[Transform],
+    properties: util.Map[String, String],
+    constraints: Array[Constraint] = Array.empty)
+  extends InMemoryRowLevelOperationTable(
+    name, schema, partitioning, properties, constraints) {
+
+  // Force DELETE to go through the SupportsRowLevelOperations path instead of
+  // the SupportsDeleteV2.deleteWhere path inherited from InMemoryTable, which
+  // only supports filtering by partition columns.
+  override def canDeleteWhere(filters: Array[Filter]): Boolean = false
+}
