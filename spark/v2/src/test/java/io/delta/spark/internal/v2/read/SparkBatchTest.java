5cb9071cbb48cb88416d0bb840d27b7fce424ca0
Yan Yan <yyanyyyy@gmail.com>
2026-04-28 17:45:51 +0000

    Address review: parametrize SparkBatchTest equals cases, fix copyright year
    - Combine the four equals=true cases (no filters, same single filter,
      partition-column reorder, data-column reorder) into one
      @ParameterizedTest. Keep testEqualsWithDifferentPushedFilters standalone
      since it asserts inequality.
    - Update copyright header from 2025 to 2026.

--- spark/v2/src/test/java/io/delta/spark/internal/v2/read/SparkBatchTest.java
+++ spark/v2/src/test/java/io/delta/spark/internal/v2/read/SparkBatchTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (2025) The Delta Lake Project Authors.
+ * Copyright (2026) The Delta Lake Project Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import io.delta.spark.internal.v2.DeltaV2TestBase;
 import io.delta.spark.internal.v2.catalog.SparkTable;
 import java.io.File;
+import java.util.stream.Stream;
 import org.apache.spark.sql.connector.catalog.Identifier;
 import org.apache.spark.sql.connector.read.Batch;
 import org.apache.spark.sql.sources.EqualTo;
@@ -30,6 +31,9 @@ import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class SparkBatchTest extends DeltaV2TestBase {
 
@@ -49,26 +53,41 @@ public class SparkBatchTest extends DeltaV2TestBase {
       new SparkTable(
           Identifier.of(new String[] {"spark_catalog", "default"}, tableName), tablePath, options);
 
-  @Test
-  public void testEqualsAndHashCode() {
-    SparkScanBuilder builder1 = (SparkScanBuilder) table.newScanBuilder(options);
-    Batch batch1 = ((SparkScan) builder1.build()).toBatch();
-
-    SparkScanBuilder builder2 = (SparkScanBuilder) table.newScanBuilder(options);
-    Batch batch2 = ((SparkScan) builder2.build()).toBatch();
-
-    assertEquals(batch1, batch2);
-    assertEquals(batch1.hashCode(), batch2.hashCode());
+  // Cases where two batches built from the same table must be equal. city/date are partition
+  // columns (exercise pushedToKernelFiltersSet); name/cnt are data columns and per
+  // ExpressionUtils.classifyFilter have isDataFilter=true, so they flow into
+  // SparkBatch.dataFilters and exercise the dataFiltersSet branch of equals/hashCode.
+  static Stream<Arguments> equalBatchCasesProvider() {
+    Filter cityEq = new EqualTo("city", "hz");
+    Filter dateEq = new EqualTo("date", "20180520");
+    Filter nameEq = new EqualTo("name", "x");
+    Filter cntGt = new GreaterThan("cnt", 10);
+    return Stream.of(
+        Arguments.of("no filters", new Filter[0], new Filter[0]),
+        Arguments.of("same single filter", new Filter[] {cityEq}, new Filter[] {cityEq}),
+        Arguments.of(
+            "partition filters in different order",
+            new Filter[] {cityEq, dateEq},
+            new Filter[] {dateEq, cityEq}),
+        Arguments.of(
+            "data filters in different order",
+            new Filter[] {nameEq, cntGt},
+            new Filter[] {cntGt, nameEq}));
   }
 
-  @Test
-  public void testEqualsWithSamePushedFilters() {
+  @ParameterizedTest(name = "{0}: batches should be equal")
+  @MethodSource("equalBatchCasesProvider")
+  public void testEqualsAndHashCode(String description, Filter[] filters1, Filter[] filters2) {
     SparkScanBuilder builder1 = (SparkScanBuilder) table.newScanBuilder(options);
-    builder1.pushFilters(new Filter[] {new EqualTo("city", "hz")});
+    if (filters1.length > 0) {
+      builder1.pushFilters(filters1);
+    }
     Batch batch1 = ((SparkScan) builder1.build()).toBatch();
 
     SparkScanBuilder builder2 = (SparkScanBuilder) table.newScanBuilder(options);
-    builder2.pushFilters(new Filter[] {new EqualTo("city", "hz")});
+    if (filters2.length > 0) {
+      builder2.pushFilters(filters2);
+    }
     Batch batch2 = ((SparkScan) builder2.build()).toBatch();
 
     assertEquals(batch1, batch2);
@@ -86,44 +105,5 @@ public class SparkBatchTest extends DeltaV2TestBase {
 
     assertNotEquals(batch1, batch2);
     assertNotEquals(batch1.hashCode(), batch2.hashCode());
-  }
-
-  @Test
-  public void testEqualsWithPushedFiltersInDifferentOrder() {
-    Filter cityEq = new EqualTo("city", "hz");
-    Filter dateEq = new EqualTo("date", "20180520");
-
-    SparkScanBuilder builder1 = (SparkScanBuilder) table.newScanBuilder(options);
-    builder1.pushFilters(new Filter[] {cityEq, dateEq});
-    Batch batch1 = ((SparkScan) builder1.build()).toBatch();
-
-    SparkScanBuilder builder2 = (SparkScanBuilder) table.newScanBuilder(options);
-    builder2.pushFilters(new Filter[] {dateEq, cityEq});
-    Batch batch2 = ((SparkScan) builder2.build()).toBatch();
-
-    assertEquals(batch1, batch2);
-    assertEquals(batch1.hashCode(), batch2.hashCode());
-  }
-
-  @Test
-  public void testEqualsWithDataFiltersInDifferentOrder() {
-    // city/date are partition columns, so the test above only exercises pushedToKernelFiltersSet.
-    // name and cnt are data columns (per the partitioned table schema), so per
-    // ExpressionUtils.classifyFilter these filters have isDataFilter=true and flow into
-    // SparkScan.dataFilters / SparkBatch.dataFilters, exercising the dataFiltersSet branch of
-    // equals/hashCode.
-    Filter nameEq = new EqualTo("name", "x");
-    Filter cntGt = new GreaterThan("cnt", 10);
-
-    SparkScanBuilder builder1 = (SparkScanBuilder) table.newScanBuilder(options);
-    builder1.pushFilters(new Filter[] {nameEq, cntGt});
-    Batch batch1 = ((SparkScan) builder1.build()).toBatch();
-
-    SparkScanBuilder builder2 = (SparkScanBuilder) table.newScanBuilder(options);
-    builder2.pushFilters(new Filter[] {cntGt, nameEq});
-    Batch batch2 = ((SparkScan) builder2.build()).toBatch();
-
-    assertEquals(batch1, batch2);
-    assertEquals(batch1.hashCode(), batch2.hashCode());
   }
 }
