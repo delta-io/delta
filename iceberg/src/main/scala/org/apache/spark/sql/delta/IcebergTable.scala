382e5dad7431ba2883c1c22fffe6ff1a04eaa82f
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2026-04-29 11:10:01 +0200

    improvements

--- iceberg/src/main/scala/org/apache/spark/sql/delta/IcebergTable.scala
+++ iceberg/src/main/scala/org/apache/spark/sql/delta/IcebergTable.scala
@@ -179,11 +179,7 @@ class IcebergTable(
     val mergedPartitionSchema = DeltaColumnMapping.setPhysicalNames(
       StructType(
         IcebergPartitionUtil.getPartitionFields(
-          tablePartitionSpec,
-          icebergTable.schema(),
-          castTimeType,
-          spark.sessionState.conf.caseSensitiveAnalysis
-        )
+          tablePartitionSpec, icebergTable.schema(), castTimeType)
       ),
       fieldPathToPhysicalName)
 
