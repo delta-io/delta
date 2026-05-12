de170a57ae70deec8c69573d5b9c9c1f16b416ea
Matthis Gördel <matthis.goerdel@databricks.com>
2026-05-12 08:30:26 +0000

    DeltaDropColumnSuite: use def instead of val for sparkConf

--- spark/src/test/scala/org/apache/spark/sql/delta/DeltaDropColumnSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/DeltaDropColumnSuite.scala
@@ -29,7 +29,7 @@ import org.apache.spark.sql.types.{ArrayType, IntegerType, MapType, StringType, 
 class DeltaDropColumnSuite extends QueryTest
   with DeltaArbitraryColumnNameSuiteBase {
 
-  override protected val sparkConf: SparkConf =
+  override protected def sparkConf: SparkConf =
     super.sparkConf.set(DeltaSQLConf.DELTA_ALTER_TABLE_DROP_COLUMN_ENABLED.key, "true")
 
   protected def dropTest(
