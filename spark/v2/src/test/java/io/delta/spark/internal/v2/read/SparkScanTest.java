06701fd9862b970a06f3b1c74d3deb70952ea7b8
Vishnu Chandrashekhar <vishnu.c@databricks.com>
2026-05-13 19:15:57 +0000

    Remove unused import java.util.Set in SparkScanTest

--- spark/v2/src/test/java/io/delta/spark/internal/v2/read/SparkScanTest.java
+++ spark/v2/src/test/java/io/delta/spark/internal/v2/read/SparkScanTest.java
@@ -15,7 +15,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.OptionalLong;
-import java.util.Set;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.spark.sql.catalyst.TableIdentifier;
 import org.apache.spark.sql.catalyst.catalog.CatalogColumnStat;
