19a07ae0fdbd814653c011559873e98c65d13a71
Matthis Gördel <matthis.goerdel@databricks.com>
2026-05-19 08:41:16 +0000

    foo

--- spark/src/test/scala/org/apache/spark/sql/delta/InMemoryTestTableMixin.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/InMemoryTestTableMixin.scala
@@ -71,10 +71,10 @@ case object DSv2DMLSchemaEvolution extends org.scalatest.Tag("DSv2DMLSchemaEvolu
  */
 trait InMemoryTestTableMixin extends SharedSparkSession with InMemoryTestTableMixinShims  {
 
-  override protected def sparkConf: SparkConf = super.sparkConf
+  override def sparkConf: SparkConf = super.sparkConf
     .set("spark.sql.catalog.spark_catalog", classOf[InMemoryDeltaCatalog].getName)
 
-  override protected def test
+  override def test
       (testName: String, testTags: org.scalatest.Tag*)
       (testFun: => Any)
       (implicit pos: org.scalactic.source.Position): Unit = {
