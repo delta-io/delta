5373f8660f595900d0eb9e75db82e6e73b6ed776
Cheng Ji <cheng.ji@databricks.com>
2026-05-28 16:29:03 +0000

    d

---
+++ storage/src/main/java/io/delta/storage/commit/GetCommitsFromLoadTableResponse.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (2021) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.delta.storage.commit;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import io.delta.storage.commit.uniform.UniformMetadata;
+
+public class GetCommitsFromLoadTableResponse extends GetCommitsResponse {
+
+  private final Optional<UniformMetadata> uniformMetadata;
+
+  public GetCommitsFromLoadTableResponse(
+      List<Commit> commits,
+      long latestTableVersion,
+      Optional<UniformMetadata> uniformMetadata) {
+    super(commits, latestTableVersion);
+    this.uniformMetadata = uniformMetadata;
+  }
+
+  public Optional<UniformMetadata> getUniformMetadata() {
+    return uniformMetadata;
+  }
+
+  @Override
+  public GetCommitsFromLoadTableResponse sortCommitsByVersion() {
+    List<Commit> sorted = new ArrayList<>(getCommits());
+    sorted.sort(Comparator.comparingLong(Commit::getVersion));
+    return new GetCommitsFromLoadTableResponse(sorted, getLatestTableVersion(), uniformMetadata);
+  }
+}
