2cda3e286f63efd11a0892b906169735ed82f29c
sotikoug83 <121517964+sotikoug83@users.noreply.github.com>
2026-07-17 15:35:05 -0700

    Wire up time travel in catalog and add tests (#7210)
    ## Description
    
    Wires DSv2 (Kernel-backed) time travel through the catalog so SQL
    `VERSION AS OF` / `TIMESTAMP AS OF` and DataFrame `versionAsOf` /
    `timestampAsOf` options resolve natively when the V2 connector is
    enabled.
    - `DeltaCatalog` now overrides `loadTable(ident, version)` and
    `loadTable(ident, timestamp)`.
    - Tests for all current entry points added in new
    `DeltaTimeTravelV2Suite`.
    - New `DeltaTimeTravelForceV2Suite` that reruns the existing
    `DeltaTimeTravelSuite` under the V2 connector.

--- spark/src/test/scala/org/apache/spark/sql/delta/DeltaHistoryManagerSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/DeltaHistoryManagerSuite.scala
@@ -48,8 +48,8 @@ import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.SharedSparkSession
 import org.apache.spark.util.Utils
 
-/** A set of tests which we can open source after Spark 3.0 is released. */
-trait DeltaTimeTravelTests extends QueryTest
+/** Helper methods for Delta time travel tests, without test registrations. */
+trait DeltaTimeTravelTestHelpers extends QueryTest
     with SharedSparkSession
     with GivenWhenThen
     with DeltaSQLCommandTest
@@ -157,6 +157,10 @@ trait DeltaTimeTravelTests extends QueryTest
   protected implicit def longToTimestampExpr(value: Long): String = {
     s"cast($value / 1000 as timestamp)"
   }
+}
+
+/** Delta time travel tests, built on the shared [[DeltaTimeTravelTestHelpers]]. */
+trait DeltaTimeTravelTests extends DeltaTimeTravelTestHelpers {
 
   import testImplicits._
 
