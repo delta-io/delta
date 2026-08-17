0b355f64172ca1f25692bddf0f6023d3e9e219cc
Chiin Luen Quah <259692855+chiinlquah@users.noreply.github.com>
2026-08-17 17:54:04 +0000

    Revert "[Kernel] Promote Snapshot/Scan/CommitRange accessors used by Delta Spark (#7269)"
    This reverts commit ed5cec69437fc98d09b28c2241a584d7c9ebb6f9.

--- kernel/kernel-api/src/main/java/io/delta/kernel/internal/commitrange/CommitRangeImpl.java
+++ kernel/kernel-api/src/main/java/io/delta/kernel/internal/commitrange/CommitRangeImpl.java
@@ -27,6 +27,7 @@ import io.delta.kernel.data.ColumnarBatch;
 import io.delta.kernel.engine.Engine;
 import io.delta.kernel.internal.DeltaLogActionUtils;
 import io.delta.kernel.internal.TableChangesUtils;
+import io.delta.kernel.internal.annotation.VisibleForTesting;
 import io.delta.kernel.internal.files.ParsedDeltaData;
 import io.delta.kernel.internal.fs.Path;
 import io.delta.kernel.utils.CloseableIterator;
@@ -89,7 +90,7 @@ public class CommitRangeImpl implements CommitRange {
     return endBoundaryOpt;
   }
 
-  @Override
+  @VisibleForTesting
   public List<FileStatus> getDeltaFiles() {
     return deltas.stream().map(ParsedDeltaData::getFileStatus).collect(Collectors.toList());
   }
